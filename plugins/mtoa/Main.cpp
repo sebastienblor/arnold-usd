9493bbcbfb588d1238961559b97b980ec9eb1060
Angel Jimenez <angel@solidangle.com>
2009-07-03 13:47:12 +0000

    Register/unregister renderer when plugin is loaded/unloaded.

--- plugins/mtoa/Main.cpp
+++ plugins/mtoa/Main.cpp
@@ -1,11 +1,14 @@
 
 #include "RenderCmd.h"
 #include <maya/MFnPlugin.h>
+#include <maya/MGlobal.h>
 
 __declspec(dllexport)
 MStatus initializePlugin(MObject object)
 {
    MFnPlugin plugin(object, "mtoa", "0.1", "Any");
+
+   MGlobal::executeCommand( MString("renderer arnold -rendererUIName \"Arnold Renderer\" -renderProcedure \"ArnoldRender\""));
 
    return plugin.registerCommand("ArnoldRender", CRenderCmd::creator, CRenderCmd::newSyntax);
 } // initializePlugin()
@@ -15,6 +18,8 @@ __declspec(dllexport)
 MStatus uninitializePlugin(MObject object)
 {
    MFnPlugin plugin(object);
+
+   MGlobal::executeCommand( MString("renderer arnold -unregisterRenderer"));
 
    return plugin.deregisterCommand("ArnoldRender");
 }  // uninitializePlugin()
