6046c818f66a173bc2b0717a60ce9d489a0c63e4
Angel Jimenez <angel@solidangle.com>
2009-07-23 12:08:18 +0000

    Connected Maya spot light with Arnold.

--- plugins/mtoa/maya_scene/Lights.cpp
+++ plugins/mtoa/maya_scene/Lights.cpp
@@ -1,6 +1,7 @@
 
 #include "MayaScene.h"
 
+#include <ai_constants.h>
 #include <ai_msg.h>
 #include <ai_nodes.h>
 
@@ -64,8 +65,7 @@ void CMayaScene::ExportLight(const MDagPath& dagPath)
 
       lightWorldMatrix = dagPath.inclusiveMatrix();
       vector           = lightWorldMatrix.translation(MSpace::kWorld);
-
-      AiNodeSetVec(light, "position", vector.x, vector.y, vector.z);
+      AiNodeSetArray(light, "position", AiArrayConvert(1, 1, AI_TYPE_POINT, &vector, TRUE));
 
       AiNodeSetBool(light, "cast_shadows", fnLight.useRayTraceShadows());
       AiNodeSetFlt(light, "radius", fnLight.shadowRadius());
@@ -76,6 +76,32 @@ void CMayaScene::ExportLight(const MDagPath& dagPath)
    }
    else if (dagPath.hasFn(MFn::kSpotLight))
    {
+      MFnSpotLight fnLight(dagPath);
+
+      light = AiNode("spot_light");
+
+      AiNodeSetStr(light, "name", fnDagNode.name().asChar());
+
+      color = fnLight.color();
+      AiNodeSetRGB(light, "color", color.r, color.g, color.b);
+      AiNodeSetFlt(light, "intensity", fnLight.intensity());
+
+      lightWorldMatrix = dagPath.inclusiveMatrix();
+      vector           = lightWorldMatrix.translation(MSpace::kWorld);
+      AiNodeSetArray(light, "position", AiArrayConvert(1, 1, AI_TYPE_POINT, &vector, TRUE));
+
+      vector += fnLight.lightDirection(0, MSpace::kWorld);
+      AiNodeSetArray(light, "look_at", AiArrayConvert(1, 1, AI_TYPE_POINT, &vector, TRUE));
+
+      AiNodeSetBool(light, "cast_shadows", fnLight.useRayTraceShadows());
+      AiNodeSetFlt(light, "radius", fnLight.shadowRadius());
+      AiNodeSetInt(light, "samples", fnDagNode.findPlug("shadowRays").asInt());
+
+      AiNodeSetFlt(light, "cone_angle", (fnLight.coneAngle() + fnLight.penumbraAngle()) * AI_RTOD);
+      AiNodeSetFlt(light, "penumbra_angle", fabsf(fnLight.penumbraAngle()) * AI_RTOD);
+
+      AiNodeSetBool(light, "affect_diffuse", fnLight.lightDiffuse());
+      AiNodeSetBool(light, "affect_specular", fnLight.lightSpecular());
    }
    else if (dagPath.hasFn(MFn::kAreaLight))
    {
