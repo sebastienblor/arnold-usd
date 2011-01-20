620b542a9aa270354ccc7fdd45d470924ee6ed25
Chad Dombrova <chadrik@gmail.com>
2011-01-20 03:21:41 +0000

    implement support for shave using the extension api

---
+++ contrib/extensions/shave/plugin/plugin.cpp
@@ -0,0 +1,18 @@
+#include "nodes/ArnoldNodeFactory.h"
+#include "ShaveAndHaircut.h"
+
+extern "C"
+{
+
+void initializePlugin(CExtension& plugin)
+{
+   plugin.RegisterDagTranslator("shaveHair", 0x1029b7, CShaveTranslator::creator,  CShaveTranslator::NodeInitializer, "shaveNode");
+}
+
+void uninitializePlugin(CExtension& plugin)
+{
+
+}
+
+}
+
