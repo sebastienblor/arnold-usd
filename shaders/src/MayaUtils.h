5173a997157067d55ed20884a6c0716e58c62d74
Chad Dombrova <chadrik@gmail.com>
2011-02-02 08:16:03 +0000

    mixing of inlining and externs seemed unnecessary and was breaking on linux

--- shaders/src/MayaUtils.h
+++ shaders/src/MayaUtils.h
@@ -3,20 +3,20 @@
 
 #include <ai.h>
 
-extern float Luminance(float v);
-extern float Luminance(const AtRGB &color);
-extern float Luminance(const AtRGBA &color);
+float Luminance(float v);
+float Luminance(const AtRGB &color);
+float Luminance(const AtRGBA &color);
 
-extern float Mix(float f0, float f1, float t);
-extern AtRGB Mix(const AtRGB &c0, const AtRGB &c1, float t);
-extern AtRGBA Mix(const AtRGBA &c0, const AtRGBA &c1, float t);
+float Mix(float f0, float f1, float t);
+AtRGB Mix(const AtRGB &c0, const AtRGB &c1, float t);
+AtRGBA Mix(const AtRGBA &c0, const AtRGBA &c1, float t);
 
-extern float MapValue(float v, float vmin, float vmax);
-extern float UnmapValue(float v, float vmin, float vmax);
+float MapValue(float v, float vmin, float vmax);
+float UnmapValue(float v, float vmin, float vmax);
 
-extern bool IsValidUV(float u, float v);
-extern float Integral(float t, float nedge);
-extern float Mod(float n, float d);
+bool IsValidUV(float u, float v);
+float Integral(float t, float nedge);
+float Mod(float n, float d);
 
 enum InterpolationType
 {
@@ -28,9 +28,9 @@ enum InterpolationType
 
 extern const char* InterpolationNames[];
 
-extern InterpolationType InterpolationNameToType(const char *n);
-extern void Interpolate(AtArray *p, AtArray *v, AtArray *it, float t, float &out);
-extern void Interpolate(AtArray *p, AtArray *v, AtArray *it, float t, AtRGB &out);
+InterpolationType InterpolationNameToType(const char *n);
+void Interpolate(AtArray *p, AtArray *v, AtArray *it, float t, float &out);
+void Interpolate(AtArray *p, AtArray *v, AtArray *it, float t, AtRGB &out);
 
 enum RampInterpolationType
 {
@@ -45,9 +45,9 @@ enum RampInterpolationType
 
 extern const char* RampInterpolationNames[];
 
-extern RampInterpolationType RampInterpolationNameToType(const char *n);
-extern void Ramp(AtArray *p, AtArray *v, float t, RampInterpolationType rit, float &out);
-extern void Ramp(AtArray *p, AtArray *v, float t, RampInterpolationType rit, AtRGB &out);
+RampInterpolationType RampInterpolationNameToType(const char *n);
+void Ramp(AtArray *p, AtArray *v, float t, RampInterpolationType rit, float &out);
+void Ramp(AtArray *p, AtArray *v, float t, RampInterpolationType rit, AtRGB &out);
 
 #ifndef LOG_05
 #  define LOG_05 -0.693147180559945  /* log(0.5) */
@@ -65,44 +65,44 @@ extern void Ramp(AtArray *p, AtArray *v, float t, RampInterpolationType rit, AtR
 
 void AddMayaColorBalanceParams(AtList *params);
    
-extern void MayaColorBalance(AtShaderGlobals* sg,
+void MayaColorBalance(AtShaderGlobals* sg,
                              AtNode* node,
                              AtInt p_start,
                              AtRGBA & result);
 
-extern void MayaDefaultColor(AtShaderGlobals* sg,
+void MayaDefaultColor(AtShaderGlobals* sg,
                              AtNode* node,
                              AtInt p_start,
                              AtRGBA & result);
 
-extern AtVector RGBtoHSV(AtRGB inRgb);
+AtVector RGBtoHSV(AtRGB inRgb);
 
-extern AtRGB HSVtoRGB(AtVector inHsv);
+AtRGB HSVtoRGB(AtVector inHsv);
 
-extern float FilteredPulseTrain(float edge, float period, float x, float dx);
+float FilteredPulseTrain(float edge, float period, float x, float dx);
 
-extern float SmoothStep(float e0, float e1, float x);
+float SmoothStep(float e0, float e1, float x);
 
-extern float Bias(float b, float x);
+float Bias(float b, float x);
 
-extern float fBm(AtShaderGlobals *sg, const AtPoint &p, float time, float initialAmplitude,
+float fBm(AtShaderGlobals *sg, const AtPoint &p, float time, float initialAmplitude,
                  int octaves[2], float initialLacunarity, float frequencyRatio, float ratio);
 
-extern float fTurbulence(AtShaderGlobals *sg, const AtPoint &point, float time, float lacunarity,
+float fTurbulence(AtShaderGlobals *sg, const AtPoint &point, float time, float lacunarity,
                          float frequencyRatio, int octaves[2], float ratio, float ripples[3]);
 
-extern AtPoint AnimatedCellNoise(const AtPoint &p, float tt);
+AtPoint AnimatedCellNoise(const AtPoint &p, float tt);
 
-extern int SuspendedParticles(const AtPoint &Pn, float time, float particleRadius, float jitter, float octave,
+int SuspendedParticles(const AtPoint &Pn, float time, float particleRadius, float jitter, float octave,
                               float &f1, AtPoint &pos1, float &f2, AtPoint &pos2, AtPoint (&particlePos)[27]);
 
-extern int SuspendedParticles2d(const AtPoint &Pn, float time, float particleRadius, float jitter, float octave, AtPoint (&particlePos)[27]);
+int SuspendedParticles2d(const AtPoint &Pn, float time, float particleRadius, float jitter, float octave, AtPoint (&particlePos)[27]);
 
-extern float ParticleDensity(int falloff, const AtPoint &particleCenter, const AtPoint &P, float radius);
+float ParticleDensity(int falloff, const AtPoint &particleCenter, const AtPoint &P, float radius);
 
-extern float BillowNoise(const AtPoint &p, float time, int dim, float radius, float sizeRand, float jitter,
+float BillowNoise(const AtPoint &p, float time, int dim, float radius, float sizeRand, float jitter,
                          int falloff, float spottyness, int octaves, float frequencyRatio, float ratio, float amplitude);
 
-extern float CosWaves(float posX, float posY, float posTime, int numWaves);
+float CosWaves(float posX, float posY, float posTime, int numWaves);
 
 #endif // MAYAUTILS_H
