7b7f86e567d643c1f76cb98c6b2febd7bd3352c8
Yannick Puech <yannick@solidangle.com>
2011-03-30 17:55:58 +0200

    remove duplicated Maya nodes IDs in ArnoldNodeIDs.h

--- plugins/mtoa/nodes/ArnoldNodeIDs.h
+++ plugins/mtoa/nodes/ArnoldNodeIDs.h
@@ -30,31 +30,4 @@
 // For automatic node generation
 #define ARNOLD_NODEID_AUTOGEN                   0x00115E00
 
-// Maya builtins ids
-#define MAYA_NODEID_SURFACE_SHADER              0x52535348
-#define MAYA_NODEID_LAMBERT                     0x524c414d
-#define MAYA_NODEID_FILE                        0x52544654
-#define MAYA_NODEID_PLACE2D_TEXTURE             0x52504c32
-#define MAYA_NODEID_BUMP2D                      0x5242554d
-#define MAYA_NODEID_BUMP3D                      0x52425533
-#define MAYA_NODEID_SAMPLER_INFO                0x5253494e
-#define MAYA_NODEID_PLUS_MINUS_AVERAGE          0x52504d41
-#define MAYA_NODEID_REMAP_VALUE                 0x524d564c
-#define MAYA_NODEID_REMAP_COLOR                 0x524d434c
-#define MAYA_NODEID_PROJECTION                  0x5250524a
-#define MAYA_NODEID_RAMP                        0x52545241
-#define MAYA_NODEID_LAYERED_TEXTURE             0x4c595254
-
-#define MAYA_NODEID_DIRECTIONAL_LIGHT           0x4449524c
-#define MAYA_NODEID_SPOT_LIGHT                  0x5350544c
-#define MAYA_NODEID_AMBIENT_LIGHT               0x414d424c
-#define MAYA_NODEID_AREA_LIGHT                  0x41524c54
-#define MAYA_NODEID_POINT_LIGHT                 0x504f4954
-
-#define MAYA_NODEID_MESH                        0x444d5348
-#define MAYA_NODEID_NURBS_SURFACE               0x4e535246
-
-#define MAYA_NODEID_CAMERA                      0x4443414d
-#define MAYA_NODEID_HAIR                        0x48535953
-
 #endif // ARNOLDNODEIDS_H
