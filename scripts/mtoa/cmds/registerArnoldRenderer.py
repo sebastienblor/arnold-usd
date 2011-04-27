ca09ec41cfe63f0465cfa7634f9d7df8f775541e
Olivier Renouard <olivier@solidangle.com>
2011-04-27 14:21:45 +0200

    imported patch python_clean.patch

--- scripts/mtoa/cmds/registerArnoldRenderer.py
+++ scripts/mtoa/cmds/registerArnoldRenderer.py
@@ -32,7 +32,6 @@ def renderSettingsTabLabel_melToUI(smel):
     # be localized. This procedure uses the first string
     # argument that is passed with the "-addGlobalsTab"
     # flag in the "renderer" command.
-    #
     result = smel
 
     if smel == 'Common':
@@ -70,34 +69,28 @@ def renderSettingsTabLabel_melToUI(smel):
 def addOneTabToGlobalsWindow(renderer, tabLabel, createProc):
     # Check to see if the unified render globals window existed.
     # If it does not exist, then we don't need to add any tab yet.
-    #
     if not cmds.window('unifiedRenderGlobalsWindow', exists=True):
         try:
             cmds.error(mel.eval('uiRes("m_unifiedRenderGlobalsWindow.kCannotAddTabs")'))
         except:
             pass
-
         return
 
     displayAllTabs = mel.eval('isDisplayingAllRendererTabs')
 
-    if not displayAllTabs:
-        # If the current renderer the renderer is not this
-        # renderer, then don't add the tab yet.
-        #
-        if utils.currentRenderer() != renderer:
-            return
+    # If the current renderer the renderer is not this
+    # renderer, then don't add the tab yet.
+    if not displayAllTabs and utils.currentRenderer() != renderer:
+        return
 
     cmds.setParent('unifiedRenderGlobalsWindow')
 
     # Hide the tabForm while updating.
-    #
     tabFormManagedStatus = cmds.formLayout('tabForm', q=True, manage=True)
     cmds.formLayout('tabForm', edit=True, manage=False)
     cmds.setParent('tabForm')
 
     # Set the correct tabLayout parent.
-    #
     tabLayoutName = ''
     if displayAllTabs:
         gMasterLayerRendererName = mel.eval('global string $gMasterLayerRendererName; $a_temps = $gMasterLayerRendererName')
@@ -111,26 +104,21 @@ def addOneTabToGlobalsWindow(renderer, tabLabel, createProc):
     # and the word "Tab" added to the end.
     # "masterLayer" will act as the renderer name if the tab
     # is in the master layer.
-    #
     tabName = mel.eval('rendererTabName("%s", "%s")'%(renderer, tabLabel))
 
     # if the tab-control does not exist, define it and add it
     # to the tabLayout
-    #
     if not cmds.layout(tabName, exists=True):
         cmds.setUITemplate('renderGlobalsTemplate', pushTemplate=True)
         cmds.setUITemplate('attributeEditorTemplate', pushTemplate=True)
 
         # Define the tab
-        #
         cmds.formLayout(tabName)
 
         # get the content of the tab from the createTabProc
-        #
 
         # Most create procs are now deferred till the tab is selected
         # These two are the default tabs, so we don't defer them
-
         createProcs = ['createMayaSoftwareCommonGlobalsTab',
                           'createMayaSoftwareGlobalsTab',
                           'createArnoldRendererCommonGlobalsTab',
@@ -140,25 +128,26 @@ def addOneTabToGlobalsWindow(renderer, tabLabel, createProc):
             mel.eval(createProc)
 
         # These end off the layouts of the information in the Tab
-        #
         cmds.setParent('..')
 
         cmds.setUITemplate(popTemplate=True)
         cmds.setUITemplate(popTemplate=True)
 
         # Add the tab to the tabLayout
-        #
         cmds.tabLayout(tabLayoutName,
                        edit=True,
                        tabLabel=(tabName, renderSettingsTabLabel_melToUI(tabLabel)))
 
     # Restore the old manage status for the tabForm.
-    #
     cmds.formLayout('tabForm', edit=True, manage=tabFormManagedStatus)
 
 def arnoldAddGlobalsTabs():
-    cmds.renderer('arnold', edit=True, addGlobalsTab=('Common', utils.pyToMelProc(createArnoldRendererCommonGlobalsTab, shortName=True), utils.pyToMelProc(updateArnoldRendererCommonGlobalsTab, shortName=True)))
-    cmds.renderer('arnold', edit=True, addGlobalsTab=('Arnold Renderer', utils.pyToMelProc(createArnoldRendererGlobalsTab, shortName=True), utils.pyToMelProc(updateArnoldRendererGlobalsTab, shortName=True)))
+    cmds.renderer('arnold', edit=True, addGlobalsTab=('Common',
+                                                      utils.pyToMelProc(createArnoldRendererCommonGlobalsTab, shortName=True),
+                                                      utils.pyToMelProc(updateArnoldRendererCommonGlobalsTab, shortName=True)))
+    cmds.renderer('arnold', edit=True, addGlobalsTab=('Arnold Renderer',
+                                                      utils.pyToMelProc(createArnoldRendererGlobalsTab, shortName=True),
+                                                      utils.pyToMelProc(updateArnoldRendererGlobalsTab, shortName=True)))
 
 
 def registerArnoldRenderer():
