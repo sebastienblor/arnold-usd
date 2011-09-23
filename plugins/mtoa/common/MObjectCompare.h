da92fd3c996c7568d25e67850aee69b4048add6d
Chad Dombrova <chadrik@gmail.com>
2011-09-23 12:00:53 -0700

    Add new class for using as the key in a std::multimap

--- plugins/mtoa/common/MObjectCompare.h
+++ plugins/mtoa/common/MObjectCompare.h
@@ -2,6 +2,8 @@
 #define MOBJECTCOMPARE_H
 
 #include <maya/MObjectHandle.h>
+#include <maya/MFnAttribute.h>
+#include <maya/MPlug.h>
 
 struct MObjectCompare
 {
@@ -9,6 +11,50 @@ struct MObjectCompare
    {
       return h1.hashCode() < h2.hashCode();
    }
+};
+
+class CNodeAttrHandle
+{
+public:
+   CNodeAttrHandle(const MObject& nodeObject, const MObject& attrObject) :
+         m_nodeHandle(nodeObject),
+         m_attrHandle(attrObject)
+   {}
+   CNodeAttrHandle(const MPlug& plug) :
+         m_nodeHandle(plug.node()),
+         m_attrHandle(plug.attribute())
+   {}
+   // match any attribute
+   CNodeAttrHandle(const MObject& nodeObject) :
+         m_nodeHandle(nodeObject)
+   {}
+   bool operator<(CNodeAttrHandle other) const
+   {
+      // check if same node
+      if (m_nodeHandle.hashCode() == other.m_nodeHandle.hashCode())
+      {
+         // check if same attribute
+         if (m_attrHandle.isValid() && other.m_attrHandle.isValid())
+            return (m_attrHandle.hashCode() < other.m_attrHandle.hashCode());
+         return false; // they are equal
+      }
+      return m_nodeHandle.hashCode() < other.m_nodeHandle.hashCode();
+   }
+   bool operator==(CNodeAttrHandle other) const
+   {
+      // check if same node
+      if (m_nodeHandle.object() == other.m_nodeHandle.object())
+      {
+         // check if same attribute
+         if (m_attrHandle.isValid() && other.m_attrHandle.isValid())
+            return (m_attrHandle.object() == other.m_attrHandle.object());
+         return true; // they are equal
+      }
+      return false;
+   }
+public :
+   const MObjectHandle m_nodeHandle;
+   const MObjectHandle m_attrHandle;
 };
 
 #endif // MOBJECTCOMPARE_H
