8706b424801c4d18b39d06115fa673f949671b0e
Olivier Renouard <olivier@solidangle.com>
2011-09-27 15:34:19 +0200

    Fix broken shave ui. Trac #183

--- contrib/extensions/shave/plugin/shave.py
+++ contrib/extensions/shave/plugin/shave.py
@@ -2,6 +2,15 @@ import maya.cmds as cmds
 from mtoa.ui.ae.customShapeAttributes import commonShapeAttributes, renderStatsAttributes
 from mtoa.ui.ae.shapeTemplate import registerUI
 
+def hairShaderNew(attrName):
+    cmds.setUITemplate('attributeEditorTemplate', pst=True)
+    cmds.attrNavigationControlGrp('aiHairShader', label="Hair Shader", at=attrName)
+    cmds.setUITemplate(ppt=True)
+
+def hairShaderReplace(attrName):
+    cmds.attrNavigationControlGrp('aiHairShader', edit=True, at=attrName)
+
+
 @registerUI("shaveHair")
 def builtin_hairSystem(ui):
     renderStatsAttributes(ui)
