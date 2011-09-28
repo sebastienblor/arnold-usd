6d1aeb6ac075832186160f73d41bf7de1c02c121
Borja Morales <borja@solidangle.com>
2011-09-28 14:28:31 +0200

    'module' object has no attribute 'abs' (ticket #480)

--- scripts/mtoa/ui/globals/common.py
+++ scripts/mtoa/ui/globals/common.py
@@ -1466,8 +1466,8 @@ def updateArnoldResolution(*args):
                     resDpi = gDefaultDpi
 
                 if width == resWidth and height == resHeight \
-                     and math.abs(aspect - resAspect) < 0.001 \
-                     and (resDpi==0 or math.abs(dpi - resDpi)) < 0.001:
+                     and math.fabs(aspect - resAspect) < 0.001 \
+                     and (resDpi==0 or math.fabs(dpi - resDpi)) < 0.001:
 
                     whichRes = numResolutionPresets + resItem + 2
                     break
@@ -1492,7 +1492,7 @@ def updateArnoldResolution(*args):
             resAspect = pm.getAttr(resNodeName + ".deviceAspectRatio")
 
             if width == resWidth and height == resHeight \
-                  and math.abs(aspect - resAspect) < 0.001:
+                  and math.fabs(aspect - resAspect) < 0.001:
 
                 # We add _2_ to $resItem below: 1 because we're
                 # skipping the first item (Custom) in the list, and 1
