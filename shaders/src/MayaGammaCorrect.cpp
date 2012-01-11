9f1037c3af25a09fee65d5c144aef231dcfee128
Chad Dombrova <chadrik@gmail.com>
2012-01-11 14:05:43 -0800

    When MayaGamma receives a negative input, return zero instead of NaN. Trac #662

--- shaders/src/MayaGammaCorrect.cpp
+++ shaders/src/MayaGammaCorrect.cpp
@@ -40,7 +40,19 @@ shader_evaluate
    AtColor value = AiShaderEvalParamRGB(p_value);
    AtColor gamma = AiShaderEvalParamRGB(p_gamma);
 
-   sg->out.RGB.r = (gamma.r == 0) ? value.r : pow(value.r, 1.0f / gamma.r);
-   sg->out.RGB.g = (gamma.g == 0) ? value.g : pow(value.g, 1.0f / gamma.g);
-   sg->out.RGB.b = (gamma.b == 0) ? value.b : pow(value.b, 1.0f / gamma.b);
+   if (value.r < AI_EPSILON)
+      sg->out.RGB.r = 0.0f;
+   else
+      sg->out.RGB.r = (gamma.r == 0.0f || gamma.r == 1.0f) ? value.r : pow(value.r, 1.0f / gamma.r);
+
+   if (value.g < AI_EPSILON)
+      sg->out.RGB.g = 0.0f;
+   else
+      sg->out.RGB.g = (gamma.g == 0.0f || gamma.g == 1.0f) ? value.g : pow(value.g, 1.0f / gamma.g);
+
+   if (value.b < AI_EPSILON)
+      sg->out.RGB.b = 0.0f;
+   else
+      sg->out.RGB.b = (gamma.r == 0.0f || gamma.b == 1.0f) ? value.b : pow(value.b, 1.0f / gamma.b);
+
 }
