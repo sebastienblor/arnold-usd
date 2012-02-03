99ba883f1d41b0af717f52eca14c98bbe31f3ffe
Pedro Gomez <pedro@solidangle.com>
2012-02-03 14:01:12 +0100

    Add file path tokens that are expanded during render. (Ticket #204)

--- shaders/src/MayaFile.cpp
+++ shaders/src/MayaFile.cpp
@@ -1,16 +1,33 @@
-#include <ai.h>
-
 #ifdef _MSC_VER
 #define _USE_MATH_DEFINES
 #endif
 #include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <map>
+
+#include <ai.h>
 
 #include "MayaUtils.h"
+
+#define MAX_FILENAME 500
 
 AI_SHADER_NODE_EXPORT_METHODS(MayaFileMtd);
 
 namespace
 {
+
+enum TokenModes
+{
+   SHAPE_NAME = 0,
+   SHAPE_PATH,
+   UDIM,
+   TILE,
+   USER_PARAM
+};
+
 
 enum MayaFileParams
 {
@@ -32,8 +49,18 @@ enum MayaFileParams
 
 };
 
+struct TokenData
+{
+   TokenModes mode;
+   unsigned int position;
+   void* extra;
+};
+
 typedef struct AtImageData
 {
+   char* path;
+   TokenData* tokens;
+   unsigned int ntokens;
    AtTextureHandle* texture_handle;
 } AtImageData;
 
@@ -59,23 +86,158 @@ node_parameters
 
 node_initialize
 {
-   AtImageData *idata = (AtImageData*) AiMalloc(sizeof(AtImageData));
-   idata->texture_handle = NULL;
-   AiNodeSetLocalData(node, idata);
 }
 
 node_update
 {
-   AtImageData *idata = (AtImageData*) AiNodeGetLocalData(node);
-   AiTextureHandleDestroy(idata->texture_handle);
-   idata->texture_handle = AiTextureHandleCreate(AiNodeGetStr(node, "filename"));
+   // cleanup
+   // since node_update may be called multiple times and a static memory allocation in node_init
+   // will not suffice, we need to free memory between updates
+   Finish(node);
+
+   const char *filename = params[p_filename].STR;
+
+   // calculate file tokens
+   std::vector<TokenData> tokens;
+
+   std::string fname(filename);
+   std::string newfname("");
+   std::map< std::string, std::string > processedTokens;
+   std::map< std::string, std::string >::iterator iter;
+   std::string::size_type lastPos = 0;
+   // Find first "non-delimiter".
+   std::string::size_type pos = 0;
+   std::string::size_type end = fname.length();
+   bool opened = false;
+   while ( pos < end && pos != std::string::npos &&
+         lastPos < end && lastPos != std::string::npos)
+   {
+      lastPos = pos;
+      // Find next "non-delimiter"
+      if (opened)
+      {
+         pos = fname.find(">", lastPos);
+         opened = false;
+         if (pos != std::string::npos)
+            pos = pos + 1;
+      }
+      else
+      {
+         pos = fname.find("<", lastPos);
+         opened = true;
+      }
+
+      // Found a token, add it to the vector.
+      const std::string token = fname.substr(lastPos, pos - lastPos);
+      std::string sub = fname.substr(lastPos, pos - lastPos);
+      //AiMsgInfo( "(%d, %d) found token '%s'", lastPos, pos, sub.c_str());
+      if (opened)
+      {
+         // a non-token file path part
+         newfname += sub;
+      }
+      else
+      {
+         // calculate the value of the map
+         if (sub == "<shapeName>")
+         {
+            TokenData data;
+            data.mode = SHAPE_NAME;
+            data.position = (unsigned int)lastPos;
+            data.extra = NULL;
+            tokens.push_back(data);
+         }
+         else if (sub == "<shapePath>")
+         {
+            TokenData data;
+            data.mode = SHAPE_PATH;
+            data.position = (unsigned int)lastPos;
+            data.extra = NULL;
+            tokens.push_back(data);
+         }
+         else if (sub.substr(0, 6) == "<attr:")
+         {
+            std::string attr = sub.substr(6, sub.length()-7);
+            TokenData data;
+            data.mode = USER_PARAM;
+            data.position = (unsigned int)lastPos;
+            data.extra = AiMalloc((unsigned long)attr.size());
+            strcpy((char*)data.extra, attr.c_str());
+            tokens.push_back(data);
+         }
+         else if (sub.substr(0, 5) == "<udim")
+         {
+            // mari/zbrush/cyslice-style udim patches: 1001
+            std::string::size_type len = sub.length();
+            // default dimension
+            int dim = 10;
+            // a specific dimension can be provided with <udim:5>
+            if (len > 7)
+            {
+               dim = atoi(sub.substr(6, len-7).c_str());
+               dim = dim <= 0 ? 1 : dim;
+            }
+            TokenData data;
+            data.mode = UDIM;
+            data.position = (unsigned int)lastPos;
+            //data.extra = NULL;
+            data.extra = AiMalloc(sizeof(int));
+            *((int*)data.extra) = dim;
+            tokens.push_back(data);
+         }
+         else if (sub == "<tile>" )
+         {
+            TokenData data;
+            data.mode = TILE;
+            data.position = (unsigned int)lastPos;
+            data.extra = NULL;
+            tokens.push_back(data);
+         }
+         else
+         {
+            AiMsgError("unknown token %s", sub.c_str());
+            newfname += sub;
+         }
+      }
+   }
+
+   AtImageData *idata = (AtImageData*) AiMalloc(sizeof(AtImageData));
+   idata->ntokens = (unsigned int)tokens.size();
+   if (tokens.size())
+   {
+      idata->tokens = (TokenData*) AiMalloc((unsigned long) (sizeof(TokenData) * tokens.size()));
+      std::copy(tokens.begin(), tokens.end(), idata->tokens);
+
+      idata->path = (char*) AiMalloc(newfname.size() + 1);
+      strcpy(idata->path, newfname.c_str());
+      idata->texture_handle = NULL;
+   }
+   else
+      idata->texture_handle = AiTextureHandleCreate(AiNodeGetStr(node, "filename"));
+   AiNodeSetLocalData(node, idata);
 }
 
 node_finish
 {
    AtImageData *idata = (AtImageData*) AiNodeGetLocalData(node);
-   AiTextureHandleDestroy(idata->texture_handle);
-   AiFree(AiNodeGetLocalData(node));
+   if (idata != NULL)
+   {
+      // Freeing ShaderData
+      if (idata->ntokens)
+      {
+         TokenData* token = idata->tokens;
+         for (unsigned int i=0; i < idata->ntokens; i++, token++)
+         {
+            if (token->extra != NULL)
+               AiFree(token->extra);
+         }
+         AiFree(idata->tokens);
+         AiFree(idata->path);
+      }
+      else
+         AiTextureHandleDestroy(idata->texture_handle);
+      AiFree(idata);
+   }
 }
 
 shader_evaluate
@@ -256,9 +418,112 @@ shader_evaluate
       AtTextureParams texparams;
       AiTextureParamsSetDefaults(&texparams);
       // setup filter?
+      bool success = true;
+      if (idata->ntokens > 0)
+      {
+         std::string newfname = "";
+         newfname.reserve(MAX_FILENAME);
+         std::string fname = idata->path;
+         //AiMsgInfo("FILE: # tokens: %d", idata->ntokens);
+         //AiMsgInfo("FILE: filename: %s", idata->path);
+         TokenData* token = idata->tokens;
+         unsigned int pos = 0;
+         for (unsigned int i=0; i < idata->ntokens; i++, token++)
+         {
+            newfname += fname.substr(pos, token->position - pos);
+            pos = token->position;
+            //AiMsgInfo("FILE: %d", (int)token->mode);
+            switch(token->mode)
+            {
+               // calculate the value of the map
+               case SHAPE_NAME:
+               {
+                  // short shape name
+                  AtNode* shape = sg->Op;
+                  std::string shapeName = AiNodeGetName(shape);
+                  size_t sep = shapeName.rfind('|');
+                  if (sep != std::string::npos)
+                     newfname += shapeName.substr(sep + 1);
+                  else
+                     newfname += shapeName;
+                  break;
+               }
+               case SHAPE_PATH:
+               {
+                  // full path with underscores for illegal characters
+                  AtNode* shape = sg->Op;
+                  std::string shapeName = AiNodeGetName(shape);
+                  std::string::size_type found=shapeName.find_first_of("|:");
+                  while (found != std::string::npos)
+                  {
+                     shapeName[found]='_';
+                     found=shapeName.find("|:", found + 1);
+                  }
+                  newfname += shapeName;
+                  break;
+               }
+               case USER_PARAM:
+               {
+                  // user attributes
+                  const char* value;
+                  if (AiUDataGetStr((const char*)token->extra, &value))
+                  {
+                     newfname += value;
+                  }
+                  else
+                  {
+                     // TODO: only warn once
+                     // AiMsgWarning("could not find user attribute %s for token %s", attr.c_str(), sub.c_str());
+                     success = false;
+                  }
+                  break;
+               }
+               case UDIM:
+               {
+                  // default dimension
+                  int* ptr = (int*)token->extra;
+                  int dim = *ptr;
+                  int row = static_cast<int>(floorf(inV));
+                  int col = static_cast<int>(floorf(inU));
+                  if ((col+1) > dim)
+                     AiMsgWarning("Invalid udim: U value %.04f is greater than maximum U dimension %d", inU, dim);
+                  else if (row < 0)
+                     AiMsgWarning("Invalid udim: U value %.04f is less than 0", inU);
+                  if (col < 0)
+                     AiMsgWarning("Invalid udim: V value %.04f is less than 0", inV);
+                  char buf[4];
+                  sprintf(buf, "%04d", ((row * dim) + col) + 1001);
+                  newfname += buf;
+                  break;
+               }
+               case TILE:
+               {
+                  // mudbox-style uv tiles: u1_v1
+                  int row = static_cast<int>(floorf(inV)) + 1;
+                  int col = static_cast<int>(floorf(inU)) + 1;
+                  char buf[7];
+                  sprintf(buf, "%d_%d", row, col);
+                  newfname += buf;
+                  break;
+               }
+            }// switch
+         }
 
-      sg->out.RGBA = AiTextureHandleAccess(sg, idata->texture_handle, &texparams, NULL);
-      MayaColorBalance(sg, node, p_defaultColor, sg->out.RGBA);
+         if (success)
+         {
+            newfname += fname.substr(pos, fname.length() - pos);
+            sg->out.RGBA = AiTextureAccess(sg, newfname.c_str(), &texparams, &success);
+         }
+         //AiMsgInfo("FILE: new name: %s", newfname.c_str());
+      }
+      else
+      {
+         sg->out.RGBA = AiTextureHandleAccess(sg, idata->texture_handle, &texparams, &success);
+      }
+      if (success)
+         MayaColorBalance(sg, node, p_defaultColor, sg->out.RGBA);
+      else
+         MayaDefaultColor(sg, node, p_defaultColor, sg->out.RGBA);
 
       // restore shader globals
       sg->u = inU;
