5d307e4dcedc569c61c3ae861062973027f21fb5
Borja Morales <borja@solidangle.com>
2012-02-20 17:01:03 +0100

    Expose reference time in the rendering options (ticket #720)

--- scripts/mtoa/ui/globals/arnold.py
+++ scripts/mtoa/ui/globals/arnold.py
@@ -199,7 +199,9 @@ def createArnoldRenderSettings():
     pm.attrControlGrp('os_binary_ass',
                    label='Binary Ass Export',
                    attribute='defaultArnoldRenderOptions.binaryAss')
-
+    
+    pm.separator()
+                    
     pm.attrControlGrp('os_outputAssBoundingBox',
                    label="Export BoundingBox",
                    attribute='defaultArnoldRenderOptions.outputAssBoundingBox')                   
@@ -575,6 +577,10 @@ def createArnoldMotionBlurSettings():
     pm.setUITemplate('attributeEditorTemplate', pushTemplate=True)
     pm.columnLayout(adjustableColumn=True)
 
+    pm.attrControlGrp('reference_time',
+                   label='Reference Time',
+                   attribute='defaultArnoldRenderOptions.reference_time')
+                   
     pm.checkBoxGrp('mb_enable',
                      cc=updateMotionBlurSettings,
                      label='Enable')
@@ -607,10 +613,9 @@ def createArnoldMotionBlurSettings():
 
     pm.attrControlGrp('mb_shader_enable',
                         label="Shaders",
-                        attribute='defaultArnoldRenderOptions.mb_shader_enable')
-
+                        attribute='defaultArnoldRenderOptions.mb_shader_enable')                        
+                        
     pm.separator()
-
 
     pm.floatSliderGrp('mb_shutter_size',
                       label="Shutter Size"
