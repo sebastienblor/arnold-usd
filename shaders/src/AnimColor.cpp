0f0ae10d0551c45c5ea82e1567882beb8bd724b7
Chad Dombrova <chadrik@gmail.com>
2012-03-07 09:53:37 -0800

    Use native AiArrayInterpolateRGBA in anim_color (Ticket #719)

--- shaders/src/AnimColor.cpp
+++ shaders/src/AnimColor.cpp
@@ -4,43 +4,6 @@
 namespace
 {
 
-// a modified version of AiArrayInterpolateFlt, from arnold core
-AtRGBA AiArrayInterpolateRGBA(const AtArray *array, float time, int idx)
-{
-   AtRGBA vector;
-
-   //AiAssert(array->type == AI_TYPE_VECTOR);
-
-   if (array->nkeys == 1 || time <= 0)
-      return ((AtRGBA *)array->data)[idx];
-
-   if (time >= 1)
-      return ((AtRGBA *)array->data)[(array->nkeys - 1) * array->nelements + idx];
-
-   if (array->nkeys == 2)
-   {
-      vector = AiRGBALerp(time,
-         ((AtRGBA *)array->data)[idx],
-         ((AtRGBA *)array->data)[array->nelements + idx]);
-   }
-   else
-   {
-      float t_nkm1 = time * (array->nkeys-1);
-      int   lokey  = FLOOR(t_nkm1);
-      int   hikey  = lokey + 1;
-      float t      = t_nkm1 - lokey;
-
-      //AiAssert(lokey >= 0 && lokey < array->nkeys);
-      //AiAssert(hikey >= 0 && hikey < array->nkeys);
-
-      vector = AiRGBALerp(t,
-         ((AtRGBA *)array->data)[lokey * array->nelements + idx],
-         ((AtRGBA *)array->data)[hikey * array->nelements + idx] );
-   }
-   return vector;
-}
-
-
 enum AnimColorParams
 {
    p_values
