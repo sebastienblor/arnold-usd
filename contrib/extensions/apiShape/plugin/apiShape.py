128cb135d5ec71f0154503ecff17efaa32b41a70
Olivier Renouard <olivier@solidangle.com>
2012-03-11 17:51:19 +0100

    apiShape UI fix

--- contrib/extensions/apiShape/plugin/apiShape.py
+++ contrib/extensions/apiShape/plugin/apiShape.py
@@ -1,9 +1,12 @@
-import maya.cmds as cmds
-from mtoa.ui.ae.customShapeAttributes import commonShapeAttributes, renderStatsAttributes
-from mtoa.ui.ae.templates import registerUI
+import pymel.core as pm
+import mtoa.ui.ae.templates as templates
+from mtoa.ui.ae.templates import AttributeTemplate, ShapeMixin, registerTranslatorUI
+from mtoa.ui.ae.shaderTemplate import ShaderMixin
 
-@registerUI("apiMesh")
-def builtin_apiShape(ui):
-    renderStatsAttributes(ui)
-    commonShapeAttributes(ui)
+class ApiShapeTemplate(AttributeTemplate, ShapeMixin, ShaderMixin):
+    def setup(self):
+        self.commonShapeAttributes()
+        self.addSeparator()
+        self.addAOVLayout()
 
+templates.registerTranslatorUI(ApiShapeTemplate, "apiMesh", "apiShape")
