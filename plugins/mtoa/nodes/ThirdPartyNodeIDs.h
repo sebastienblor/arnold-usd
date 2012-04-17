0befaa2bdc865e03b1794b4b1d8e424cefd96c8e
Borja Morales <borja@solidangle.com>
2012-04-17 14:11:54 +0200

    Create ThirdPartyNodeIDs.h (ticket #808)

---
+++ plugins/mtoa/nodes/ThirdPartyNodeIDs.h
@@ -0,0 +1,10 @@
+#ifndef THIRDPARTYNODEIDS_H
+#define THIRDPARTYNODEIDS_H
+
+// Arnold Third Party Shaders Are assigned the following 512 IDE block
+// 0x00116400 - 0x001165FF
+
+// Arnold Nodes in Maya
+#define THIRDPARTY_NODEID_AAOCEAN            0x00116400
+
+#endif // THIRDPARTYNODEIDS_H
