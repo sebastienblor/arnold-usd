65b36ccf34bb266a0cd0f886400199a2246b2ec6
Pedro Gomez <pedro@solidangle.com>
2012-06-13 15:27:51 +0200

    Added comments to explain better the new code (Ticket #874)

--- shaders/src/MayaFile.cpp
+++ shaders/src/MayaFile.cpp
@@ -19,6 +19,12 @@ AI_SHADER_NODE_EXPORT_METHODS(MayaFileMtd);
 namespace
 {
 
+// Modes: SHAPE_NAME, SHAPE_PATH, TILE and USER_PARAM has a variable token size,
+//  so they will require break the file path where they are found to be able to
+//  insert the evaluated token value in the correct place.
+// Mode UDIM has a fixed token size = 4. So This token will not break the file path
+//  where it is found, but it will be replaced by a 4 character mask that will be
+//  updated in the evaluation of the token to improve performance
 enum TokenModes
 {
    SHAPE_NAME = 0,
@@ -52,16 +58,21 @@ enum MayaFileParams
 struct TokenData
 {
    TokenModes mode;
-   int position;
-   int nextSize;
+   int position;    // Position on AtImageData.origName of next string chunk after token
+                    //  For UDIM token, as it does not break the current string chunk,
+                    //  it is the distance from the end of the current chunk to the begining
+                    //  of the UDIM mask as a negative number.
+
+   int nextSize;    // Size of next string chunk after token. For the UDIM token, it does
+                    //  not have any meaning so it will be 0
    void* extra;
 };
 
 typedef struct AtImageData
 {
-   char* origPath;
-   char** processPath;
-   unsigned int startPos;
+   char* origPath;        // Original file path without tokens
+   char** processPath;    // Current evaluated file path (one for each thread)
+   unsigned int startPos; // First position in the "processPath" where the first token will be added
    TokenData* tokens;
    unsigned int ntokens;
    AtTextureHandle* texture_handle;
@@ -146,23 +157,29 @@ node_update
          }
 
          // Found a token, add it to the vector.
-         //const std::string token = fname.substr(lastPos, pos - lastPos);
          std::string sub = fname.substr(lastPos, pos - lastPos);
-         //AiMsgInfo( "(%d, %d) found token '%s'", lastPos, pos, sub.c_str());
          if (opened)
          {
-            // a non-token file path part
+            // a non-token file path part. "sub" has a chunk of text without tokens.
             newfname += sub;
+            // If we are at the end of the file path and there is a token that broke the file path chunk, update its "nextSize" attribute
             if (pos == std::string::npos && prevToken >= 0)
                tokens[prevToken].nextSize = (int)newfname.size() - tokens[prevToken].position;
+            // If we have not found a token that break the first file path text, increase the size of the "firstBreak" value
             if (!breakFound)
                firstBreak = (int)newfname.size();
+            // If last token was a UDIM
             if (tokens.size() > 0 && tokens.back().mode == UDIM)
+               // Update it to be the position of the begining of the UDIM mask
+               //  relative to the end of the string chunk. Example:
+               //   text####text
+               //       |
+               //       \------- = "-8"
                tokens.back().position = tokens.back().position - (int) newfname.size();
          }
          else
          {
-            // calculate the value of the map
+            // process the tokens
             if (sub == "<shapeName>")
             {
                TokenData data;
@@ -170,9 +187,11 @@ node_update
                data.position = (int) newfname.size();
                data.extra = NULL;
                data.nextSize = 0;
+               // If a previous token broke the file path chunk, update its "nextSize" attribute
                if (prevToken >= 0)
                   tokens[prevToken].nextSize = (int) newfname.size() - tokens[prevToken].position;
                tokens.push_back(data);
+               // Set this token as the last that broke the text chunk
                prevToken = (int) tokens.size()-1;
                breakFound = true;
             }
@@ -183,9 +202,11 @@ node_update
                data.position = (int) newfname.size();
                data.extra = NULL;
                data.nextSize = 0;
+               // If a previous token broke the file path chunk, update its "nextSize" attribute
                if (prevToken >= 0)
                   tokens[prevToken].nextSize = (int) newfname.size() - tokens[prevToken].position;
                tokens.push_back(data);
+               // Set this token as the last that broke the text chunk
                prevToken = (int) tokens.size()-1;
                breakFound = true;
             }
@@ -198,9 +219,11 @@ node_update
                data.extra = AiMalloc((unsigned long)attr.size());
                strcpy((char*)data.extra, attr.c_str());
                data.nextSize = 0;
+               // If a previous token broke the file path chunk, update its "nextSize" attribute
                if (prevToken >= 0)
                   tokens[prevToken].nextSize = (int) newfname.size() - tokens[prevToken].position;
                tokens.push_back(data);
+               // Set this token as the last that broke the text chunk
                prevToken = (int) tokens.size()-1;
                breakFound = true;
             }
@@ -223,11 +246,14 @@ node_update
                }
                TokenData data;
                data.mode = UDIM;
+               // Position will be updated when the end of the text chunk is reached
                data.position = (int) newfname.size();
                data.extra = AiMalloc(sizeof(int));
                *((int*)data.extra) = dim;
 
                tokens.push_back(data);
+               // This token does not break the text chunk, we only add a 4 characters
+               //  mask to be replaced at evaluation time
                newfname += "1001";
                // Just in case file name ends here
                if (!breakFound)
@@ -240,9 +266,11 @@ node_update
                data.position = (int) newfname.size();
                data.extra = NULL;
                data.nextSize = 0;
+               // If a previous token broke the file path chunk, update its "nextSize" attribute
                if (prevToken >= 0)
                   tokens[prevToken].nextSize = (int) newfname.size() - tokens[prevToken].position;
                tokens.push_back(data);
+               // Set this token as the last that broke the text chunk
                prevToken = (int) tokens.size()-1;
                breakFound = true;
             }
@@ -263,6 +291,7 @@ node_update
          idata->origPath = (char*) AiMalloc((unsigned long)newfname.size() + 1);
          strcpy(idata->origPath, newfname.c_str());
 
+         // For each thread, create a processPath with the first text chunk already copied to it.
          AtNode* nodeOpt = AiUniverseGetOptions();
          int threads = AiNodeGetInt(nodeOpt, "threads");
          idata->processPath = (char**) AiMalloc(sizeof(char*) * threads);
@@ -519,8 +548,10 @@ shader_evaluate
                      memcpy(&(idata->processPath[sg->tid][pos]),shapeName.c_str(),shapeName.size());
                      pos += (unsigned int) shapeName.size();
                   }
+                  // Copy next text chunk to the "processPath"
                   memcpy(&(idata->processPath[sg->tid][pos]),&(idata->origPath[token->position]),token->nextSize);
                   pos += token->nextSize;
+                  // Set the end of the string
                   idata->processPath[sg->tid][pos] = 0;
                   break;
                }
@@ -537,8 +568,10 @@ shader_evaluate
                   }
                   memcpy(&(idata->processPath[sg->tid][pos]),shapeName.c_str(),shapeName.size());
                   pos += (unsigned int) shapeName.size();
+                  // Copy next text chunk to the "processPath"
                   memcpy(&(idata->processPath[sg->tid][pos]),&(idata->origPath[token->position]),token->nextSize);
                   pos += token->nextSize;
+                  // Set the end of the string
                   idata->processPath[sg->tid][pos] = 0;
                   break;
                }
@@ -551,8 +584,10 @@ shader_evaluate
                      int len = (int) strlen(value);
                      memcpy(&(idata->processPath[sg->tid][pos]),value,len);
                      pos += (unsigned int) len;
+                     // Copy next text chunk to the "processPath"
                      memcpy(&(idata->processPath[sg->tid][pos]),&(idata->origPath[token->position]),token->nextSize);
                      pos += token->nextSize;
+                     // Set the end of the string
                      idata->processPath[sg->tid][pos] = 0;
                   }
                   else
@@ -572,6 +607,8 @@ shader_evaluate
                   int col = static_cast<int>(floorf(inU));
                   
                   int mariCode = ((row * dim) + col) + 1001;
+
+                  // Update the UDIM mask in the "processPath"
                   idata->processPath[sg->tid][pos + token->position+3] = (mariCode%10) + '0';
                   mariCode /= 10;
                   idata->processPath[sg->tid][pos + token->position+2] = (mariCode%10) + '0';
@@ -595,8 +632,10 @@ shader_evaluate
                   int len = (int) strlen(buf);
                   memcpy(&(idata->processPath[sg->tid][pos]),buf,len);
                   pos += len;
+                  // Copy next text chunk to the "processPath"
                   memcpy(&(idata->processPath[sg->tid][pos]),&(idata->origPath[token->position]),token->nextSize);
                   pos += token->nextSize;
+                  // Set the end of the string
                   idata->processPath[sg->tid][pos] = 0;
                   break;
                }
