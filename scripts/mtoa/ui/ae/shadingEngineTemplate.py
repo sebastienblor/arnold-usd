5993e9117ca2c884512144c1c36eedd9690c7258
Pal Mezei <pal@solidangle.com>
2012-06-21 17:19:44 +0200

    Fixing unicode handling on linux related to Ticket #891

--- scripts/mtoa/ui/ae/shadingEngineTemplate.py
+++ scripts/mtoa/ui/ae/shadingEngineTemplate.py
@@ -206,3 +206,10 @@ class ShadingEngineTemplate(templates.AttributeEditorTemplate):
 
 templates.registerAETemplate(ShadingEngineTemplate, "shadingEngine")
 
+class DisplacementShaderTemplate(templates.AttributeEditorTemplate):
+    def setup(self):
+        self.addControl("aiDisplacementPadding", label="Displacement Padding")
+        self.addControl("aiDisplacementZeroValue", label="Displacement Zero Value")
+
+templates.registerAETemplate(DisplacementShaderTemplate, "displacementShader")
+
--- scripts/mtoa/utils.py
+++ scripts/mtoa/utils.py
@@ -9,6 +9,7 @@ import os
 import shlex
 import sys
 import ctypes
+import string
 
 def even(num):
     return bool(num % 2)
@@ -355,6 +356,13 @@ def getFileName(pathType, tokens, path='<Scene>', frame=None, fileType='images',
         if not os.path.exists(dir):
             os.makedirs(dir)
     return result
+
+def convertToUnicode(s):
+    try:
+        s = s.encode('utf-8')
+    except UnicodeDecodeError:
+        pass
+    return s
 
 def getEnvironmentVariable(name):
     '''
