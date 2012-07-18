9a90c885a08cd03faf2818168c589ccf7cd0ec8a
Pal Mezei <pal@solidangle.com>
2012-07-18 11:55:14 +0200

    Creating new branch for Ticket #923 .

---
+++ plugins/mtoa/session/ArnoldLightLinks.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   ArnoldLightLinks.h
+ * Author: palmezei
+ *
+ * Created on July 18, 2012, 11:48 AM
+ */
+
+#ifndef ARNOLDLIGHTLINKS_H
+#define	ARNOLDLIGHTLINKS_H
+
+
+
+#endif	/* ARNOLDLIGHTLINKS_H */
+
