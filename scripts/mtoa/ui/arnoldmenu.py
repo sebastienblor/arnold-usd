72b86dab783f293c7f0cc8420809275e0862da48
Pal Mezei <pal@solidangle.com>
2012-08-02 13:04:37 +0200

    Templating the original mesh with the mesh_light. (Ticket #913)

--- scripts/mtoa/ui/arnoldmenu.py
+++ scripts/mtoa/ui/arnoldmenu.py
@@ -35,6 +35,7 @@ def doCreateMeshLight():
     cmds.xform(lLocator, worldSpace=True, matrix=wm)
     cmds.connectAttr('%s.outMesh' % shs[0], '%s.inputMesh' % lShape, force=True)
     cmds.setAttr('%s.aiTranslator' % lShape, 'mesh', type='string')
+    cmds.setAttr('%s.template' % shs[0], 1)
    
 def createArnoldMenu():
     # Add an Arnold menu in Maya main window
