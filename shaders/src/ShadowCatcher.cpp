93a3518c1cc3436d569337d1e055a3bd079c1e4d
Pal Mezei <pal@solidangle.com>
2012-08-08 16:22:16 +0200

    Added surfaceShader to the shadowCatcher, and changed the classification.

--- shaders/src/ShadowCatcher.cpp
+++ shaders/src/ShadowCatcher.cpp
@@ -5,6 +5,7 @@ AI_SHADER_NODE_EXPORT_METHODS(ShadowCatcherMtd);
 enum ShadowCatcherParams
 {
    p_catch_shadows = 0,
+   p_surface_shader,
    p_shadow_color,
    p_enable_transparency,
    p_shadow_transparency,
@@ -17,10 +18,11 @@ node_parameters
 {
    AiMetaDataSetStr(mds, NULL, "maya.name", "aiShadowCatcher");
    AiMetaDataSetInt(mds, NULL, "maya.id", 0x00115D19);
-   AiMetaDataSetStr(mds, NULL, "maya.classification", "shader/utility");
+   AiMetaDataSetStr(mds, NULL, "maya.classification", "shader/surface");
    AiMetaDataSetBool(mds, NULL, "maya.swatch", false);
    
    AiParameterBOOL("catchShadows", true);
+   AiParameterRGBA("surfaceShader", 0.0f, 0.0f, 0.0f, 0.0f);
    AiParameterRGB("shadowColor", 0.0f, 0.0f, 0.0f);
    AiParameterBOOL("enableTransparency", false);
    AiParameterRGB("shadowTransparency", 0.0f, 0.0f, 0.0f);
@@ -43,17 +45,26 @@ node_finish
 
 shader_evaluate
 {
+   if (!(sg->Rt & AI_RAY_CAMERA))
+   {
+      sg->out.RGBA = AiShaderEvalParamRGBA(p_surface_shader);
+      return;
+   }
+           
    AtRGB result = AI_RGB_BLACK;
    AtRGB resultOpacity = AI_RGB_WHITE;
    float resultAlpha = 0.0f;
       
    if (AiShaderEvalParamBool(p_catch_shadows))
-   {
-      const AtRGB matte = AiLightsGetShadowMatte(sg);
-      result = AiShaderEvalParamRGB(p_shadow_color) * matte;
-      resultAlpha = (matte.r + matte.g + matte.b) / 3.0f;
+   {      
+      
+      AtRGB matte = AI_RGB_BLACK;
+      matte = AiLightsGetShadowMatte(sg);
+      resultAlpha = (matte.r + matte.g + matte.b) / 3.0f;      
       if (AiShaderEvalParamBool(p_enable_transparency))
          resultOpacity = matte * (1.0f - AiShaderEvalParamRGB(p_shadow_transparency));
+      matte *= AiShaderEvalParamRGB(p_shadow_color);
+      result = matte;
    }
    
    if (AiShaderEvalParamBool(p_catch_diffuse))
