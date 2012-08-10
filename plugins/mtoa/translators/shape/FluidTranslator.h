12a7611057e7aa3df82d4a96f1d3af38c82b1688
Pal Mezei <pal@solidangle.com>
2012-08-10 20:50:44 +0200

    Changing back to CDagTranslator instead of CNodeTranslator.

--- plugins/mtoa/translators/shape/FluidTranslator.h
+++ plugins/mtoa/translators/shape/FluidTranslator.h
@@ -3,11 +3,12 @@
 
 #include "translators/NodeTranslator.h"
 
-class CFluidTranslator : public CNodeTranslator{
+class CFluidTranslator : public CDagTranslator{
 public:
    static void NodeInitializer(CAbTranslator context);
    static void* creator() {return new CFluidTranslator();}
    virtual AtNode* CreateArnoldNodes();   
+   bool RequiresMotionData() {return false;}
 protected:
    virtual void Export(AtNode* fluid);
 };
