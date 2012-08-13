e12897ec8b074f15e385ccb2c4bec9581c5aed92
Pal Mezei <pal@solidangle.com>
2012-08-13 12:03:47 +0200

    Some cc to the volume export.

--- plugins/mtoa/translators/shape/FluidTranslator.cpp
+++ plugins/mtoa/translators/shape/FluidTranslator.cpp
@@ -12,6 +12,23 @@ AtNode* CFluidTranslator::CreateArnoldNodes()
    return AddArnoldNode("standard");
 }
 
+void ExportFloatGrid(AtNode* fluid, float* values, const char* paramName, unsigned int numVoxels)
+{
+   if (values == 0)
+      return;
+   AtArray* array = AiArrayAllocate(numVoxels, 1, AI_TYPE_FLOAT);
+   for (unsigned int i = 0; i < numVoxels; ++i)
+   {
+      const float cVoxel = values[i];
+      if (cVoxel < AI_EPSILON)
+         AiArraySetFlt(array, i, 0.f);
+      else
+         AiArraySetFlt(array, i, cVoxel);
+   }
+   AiNodeDeclare(fluid, paramName, "constant ARRAY FLOAT");
+   AiNodeSetArray(fluid, paramName, array);
+}
+
 void CFluidTranslator::Export(AtNode* fluid)
 {
    MFnFluid mayaFluid(GetMayaObject());
@@ -22,15 +39,47 @@ void CFluidTranslator::Export(AtNode* fluid)
    mayaFluid.getResolution(xRes, yRes, zRes);
    mayaFluid.getDimensions(xDim, yDim, zDim);
    
-   std::cerr << xRes << " " << yRes << " " << zRes << std::endl;
-   
    const unsigned int numVoxels = xRes * yRes * zRes;
    
-   float* density = mayaFluid.density();
-   if (density != 0)
+   MFnFluid::FluidMethod fluidMethod;
+   MFnFluid::FluidGradient fluidGradient;
+   
+   mayaFluid.getDensityMode(fluidMethod, fluidGradient);
+   
+   if (fluidMethod != MFnFluid::kZero)
+      ExportFloatGrid(fluid, mayaFluid.density(), "density", numVoxels);
+   
+   mayaFluid.getFuelMode(fluidMethod, fluidGradient);
+   
+   if (fluidMethod != MFnFluid::kZero)
+      ExportFloatGrid(fluid, mayaFluid.fuel(), "fuel", numVoxels);
+   
+   mayaFluid.getTemperatureMode(fluidMethod, fluidGradient);
+   
+   if (fluidMethod != MFnFluid::kZero)
+      ExportFloatGrid(fluid, mayaFluid.temperature(), "temperature", numVoxels);
+   
+   MFnFluid::ColorMethod colorMethod;
+   
+   mayaFluid.getColorMode(colorMethod);
+   
+   if (colorMethod != MFnFluid::kUseShadingColor)
    {
-      AtArray* densityArray = AiArrayConvert(numVoxels, 1, AI_TYPE_FLOAT, density);
-      AiNodeDeclare(fluid, "density", "constant ARRAY FLOAT");
-      AiNodeSetArray(fluid, "density", densityArray);
+      float* r; float* g; float* b;
+      mayaFluid.getColors(r, g, b);
+      if (r != 0 && g != 0 && b != 0)
+      {
+         AtArray* array = AiArrayAllocate(numVoxels, 1, AI_TYPE_RGB);
+         for (unsigned int i = 0; i < numVoxels; ++i)
+         {
+            AtColor cColor = {r[i], g[i], b[i]};
+            cColor.r = cColor.r < AI_EPSILON ? 0.f : cColor.r;
+            cColor.g = cColor.g < AI_EPSILON ? 0.f : cColor.g;
+            cColor.b = cColor.b < AI_EPSILON ? 0.f : cColor.b;
+            AiArraySetRGB(array, i, cColor);
+         }
+         AiNodeDeclare(fluid, "colors", "constant ARRAY RGB");
+         AiNodeSetArray(fluid, "colors", array);
+      }
    }
 }
