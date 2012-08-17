9a8c0a34203b1a1139a1b0b47ab6343b6c86bfb6
Pal Mezei <pal@solidangle.com>
2012-08-17 18:20:18 +0200

    A lot of fixes for the light Linking export.

--- plugins/mtoa/session/ArnoldLightLinks.cpp
+++ plugins/mtoa/session/ArnoldLightLinks.cpp
@@ -43,7 +43,10 @@ void CArnoldLightLinks::ParseLightLinks()
    AiNodeIteratorDestroy(niter);
    m_numArnoldLights = m_arnoldLights.size();
    if (m_numArnoldLights)
+   {
       m_linkedLights.resize(m_numArnoldLights);
+      m_linkedShadows.resize(m_numArnoldLights);
+   }
 }
 
 void CArnoldLightLinks::AppendNodesToList(MFnDependencyNode& linkedNodes, std::vector<AtNode*>& nodeList, 
@@ -65,15 +68,31 @@ void CArnoldLightLinks::AppendNodesToList(MFnDependencyNode& linkedNodes, std::v
          if (!status)
             continue;
          std::map<std::string, AtNode*>::iterator it = m_arnoldLights.find(linkedLight.name().asChar());
+         if (it == m_arnoldLights.end())
+            it = m_arnoldLights.find(dgPath.fullPathName().asChar() + 1); //if the shapeName is not unique we are using the full path name
          if (it != m_arnoldLights.end())
-            nodeList[numLinkedNodes++] = it->second;
+         {
+            std::vector<AtNode*>::iterator itEnd = nodeList.begin() + numLinkedNodes;
+            if (std::find(nodeList.begin(), itEnd, it->second) == itEnd)
+               nodeList[numLinkedNodes++] = it->second;
+         }
       }
    }
    else
    {
       std::map<std::string, AtNode*>::iterator it = m_arnoldLights.find(linkedNodes.name().asChar());
+      if (it == m_arnoldLights.end())
+      {
+         MDagPath dgPath;
+         MDagPath::getAPathTo(linkedNodes.object(), dgPath);
+         it = m_arnoldLights.find(dgPath.fullPathName().asChar() + 1); //if the shapeName is not unique we are using the full path name
+      }
       if (it != m_arnoldLights.end())
-         nodeList[numLinkedNodes++] = it->second;
+      {
+         std::vector<AtNode*>::iterator itEnd = nodeList.begin() + numLinkedNodes;
+         if (std::find(nodeList.begin(), itEnd, it->second) == itEnd)
+            nodeList[numLinkedNodes++] = it->second;
+      }
    }
 }
 
@@ -82,7 +101,8 @@ void CArnoldLightLinks::HandleLightLinker(MPlug& conn,
         NodeLinkMode& lightLinkMode, NodeLinkMode& shadowLinkMode)
 {
    MPlug parentPlug = conn.parent();
-   const char* plugName = parentPlug.partialName(false, false, false, false, false, true).asChar();
+   std::string plugName = parentPlug.partialName(false, false, false, false, false, true).asChar();
+   // sometimes this buffer is invalidated
 
    static MPlugArray conns2;
    parentPlug.child(0).connectedTo(conns2, true, false);
@@ -92,13 +112,13 @@ void CArnoldLightLinks::HandleLightLinker(MPlug& conn,
 
    if (m_lightMode == MTOA_LIGHTLINK_MAYA)
    {
-      if ((lightLinkMode == MTOA_NODELINK_LINK) && (strncmp(plugName, "link", 4) == 0)) // linking the light
+      if ((lightLinkMode == MTOA_NODELINK_LINK) && (strncmp(plugName.c_str(), "link", 4) == 0)) // linking the light
       {
          // check for this being a light set
          MFnDependencyNode linkedLights(conns2[0].node());
          AppendNodesToList(linkedLights, m_linkedLights, numLinkedLights);
       }
-      else if (strncmp(plugName, "ignore", 6) == 0) // ignoring the light
+      else if (strncmp(plugName.c_str(), "ignore", 6) == 0) // ignoring the light
       {
          lightLinkMode = MTOA_NODELINK_IGNORE;
          MFnDependencyNode linkedLights(conns2[0].node());
@@ -108,13 +128,13 @@ void CArnoldLightLinks::HandleLightLinker(MPlug& conn,
 
    if (m_shadowMode == MTOA_SHADOWLINK_MAYA)
    {
-      if ((shadowLinkMode == MTOA_NODELINK_LINK) && (strncmp(plugName, "shadowLink", 10) == 0)) // linking the shadow
+      if ((shadowLinkMode == MTOA_NODELINK_LINK) && (strncmp(plugName.c_str(), "shadowLink", 10) == 0)) // linking the shadow
       {
          // check for this being a light set
          MFnDependencyNode linkedShadows(conns2[0].node());
          AppendNodesToList(linkedShadows, m_linkedShadows, numLinkedShadows);
       }
-      else if (strncmp(plugName, "shadowIgnore", 12) == 0) // ignoring the shadow
+      else if (strncmp(plugName.c_str(), "shadowIgnore", 12) == 0) // ignoring the shadow
       {
          shadowLinkMode = MTOA_NODELINK_IGNORE;
          MFnDependencyNode linkedShadows(conns2[0].node());
@@ -159,9 +179,22 @@ void CArnoldLightLinks::ExportLightLinking(AtNode* shape, MFnDependencyNode& dNo
    instObjGroupsPlug = instObjGroupsPlug.elementByLogicalIndex(0);
    static MPlugArray conns;
    instObjGroupsPlug.connectedTo(conns, false, true);
-   unsigned int numConnections = conns.length();   
-   for (unsigned int i = 0; i < numConnections; ++i)
+   unsigned int numConnections = conns.length();
+   // check for the per face assignment light linking
+   // only check for the first one
+   // because we don't support per face light linking
+   if (numConnections == 0)
    {
+      instObjGroupsPlug = instObjGroupsPlug.child(0, &status);
+      if (status)
+      {
+         instObjGroupsPlug = instObjGroupsPlug.elementByLogicalIndex(0);
+         instObjGroupsPlug.connectedTo(conns, false, true);
+         numConnections = conns.length();
+      }
+   }
+   for (unsigned int i = 0; i < numConnections; ++i)
+   {      
       MPlug conn = conns[i];
       if (conn.node().hasFn(MFn::kShadingEngine))
       {
