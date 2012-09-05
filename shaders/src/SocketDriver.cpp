55646985ebca5a1fad9a9f6eea293e16d81bc18a
Pal Mezei <pal@solidangle.com>
2012-09-05 17:07:33 +0200

    Sending RGB and RGBA aov data properly through sockets.

--- shaders/src/SocketDriver.cpp
+++ shaders/src/SocketDriver.cpp
@@ -4,6 +4,9 @@
 #include <cstring>
 #include <sys/socket.h>
 #include <netdb.h> 
+#include <unistd.h>
+#include <map>
+#include <string>
 
 AI_DRIVER_NODE_EXPORT_METHODS(SocketDriverMtd);
 
@@ -14,11 +17,22 @@ node_parameters
 
 struct SocketDriverData{
    int socketFd;
+   std::map<std::string, int> aovMap;
+   
+   static void* operator new(size_t size)
+   {
+      return AiMalloc(size);
+   }
+   
+   static void operator delete(void* p)
+   {
+      AiFree(p);
+   }
 };
 
 node_initialize
 {
-   SocketDriverData* data = (SocketDriverData*)AiMalloc(sizeof(SocketDriverData));
+   SocketDriverData* data = new SocketDriverData;
    AiDriverInitialize(node, false, data);
 }
 
@@ -44,6 +58,26 @@ driver_extension
    return 0;
 }
 
+void SendSocket(int socketFd, const void* data, int dataSize)
+{
+   int rem = dataSize;
+   const char* d = (const char*)data;
+   while(rem)
+   {
+      int curr = send(socketFd, d, MIN(1024, rem), 0); // without this there might be some problems (especially on windows)
+      if (curr == -1)
+         return;
+      rem = rem - curr;
+      d += curr;
+   }
+}
+
+template <typename T>
+void SendSocket(int socketFd, const T& data)
+{
+   SendSocket(socketFd, &data, sizeof(T));
+}
+
 driver_open
 {
    int status;
@@ -58,7 +92,7 @@ driver_open
    status = getaddrinfo("localhost", "4242", &host_info, &host_info_list);
    if (status != 0)
    {
-      std::cerr << "getaddrinfo error " << gai_strerror(status) << std::endl;
+      AiMsgError("Getaddrinfo error %s", gai_strerror(status));
       return;
    }
    
@@ -66,14 +100,14 @@ driver_open
    
    if (socketFd == -1)
    {
-      std::cerr << "socket creation error" << std::endl;
+      AiMsgError("socket creation error");
       return;
    }
    
    status = connect(socketFd, host_info_list->ai_addr, host_info_list->ai_addrlen);
    if (status == -1)
    {
-      std::cerr << "error connecting to the server" << std::endl;
+      AiMsgError("error connecting to the server");
       return;
    }
    
@@ -84,7 +118,30 @@ driver_open
    ws[0] = display_window.maxx - display_window.minx + 1;
    ws[1] = display_window.maxy - display_window.miny + 1;
    
-   send(socketFd, (char*)ws, sizeof(int) * 2, 0);
+   int evt = 1;
+   
+   SendSocket(socketFd, evt);
+   SendSocket(socketFd, ws, sizeof(int) * 2);
+   
+   const char* outputName;
+   int pixelType;
+   
+   while (AiOutputIteratorGetNext(iterator, &outputName, &pixelType, 0))
+   {
+      const int outputNameLength = strlen(outputName);
+      if (outputNameLength < 1)
+         continue;
+      
+      int aovID = data->aovMap.size();
+      data->aovMap[outputName] = aovID;
+      
+      evt = 2;
+      SendSocket(socketFd, evt);
+      SendSocket(socketFd, aovID);
+      SendSocket(socketFd, pixelType);
+      SendSocket(socketFd, outputNameLength);
+      SendSocket(socketFd, outputName, outputNameLength);
+   }
 }
 
 driver_prepare_bucket
@@ -94,15 +151,37 @@ driver_prepare_bucket
 
 driver_write_bucket
 {
+   SocketDriverData* data = (SocketDriverData*)AiDriverGetLocalData(node);
    
+   const char* outputName;
+   int pixelType;
+   const void* bucketData;
+   
+   while (AiOutputIteratorGetNext(iterator, &outputName, &pixelType, &bucketData))
+   {
+      std::map<std::string, int>::iterator it = data->aovMap.find(outputName);
+      if (it == data->aovMap.end())
+         continue;
+      int evt = 3; // aov data
+      SendSocket(data->socketFd, evt);
+      SendSocket(data->socketFd, it->second);
+      SendSocket(data->socketFd, bucket_xo);
+      SendSocket(data->socketFd, bucket_yo);
+      SendSocket(data->socketFd, bucket_size_x);
+      SendSocket(data->socketFd, bucket_size_y);
+      int pixelSize = pixelType == AI_TYPE_RGB ? sizeof(AtRGB) : sizeof(AtRGBA);
+      SendSocket(data->socketFd, bucketData, pixelSize * bucket_size_x * bucket_size_y);
+   }
 }
 
 driver_close
 {
-   
+   SocketDriverData* data = (SocketDriverData*)AiDriverGetLocalData(node);
+   if (data->socketFd != -1)
+      close(data->socketFd);
 }
 
 node_finish
 {
-   
+   delete (SocketDriverData*)AiDriverGetLocalData(node);
 }
