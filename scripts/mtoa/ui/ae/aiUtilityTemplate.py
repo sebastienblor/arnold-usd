59a1bda3ec53955cce1b085fa805e7e9bfce5129
Pal Mezei <pal@solidangle.com>
2012-10-02 19:39:10 +0200

    Matchin the order of the enums in aiUtility.color_mode with the xsi order. (Ticket #1080)

--- scripts/mtoa/ui/ae/aiUtilityTemplate.py
+++ scripts/mtoa/ui/ae/aiUtilityTemplate.py
@@ -5,14 +5,13 @@ from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
 def aiUtilityCreateColorMode(attr):
     cmds.setUITemplate('attributeEditorPresetsTemplate', pushTemplate=True)
     cmds.attrEnumOptionMenuGrp('AIUtilityColorMode', attribute=attr, label="Color Mode", 
-                               enumeratedItem=[(0, 'Color'), (1, 'Geometric Normal'), (2, 'Un-bumped Normal'), (3, 'Normal'),
+                               enumeratedItem=[(0, 'Color'), (3, 'Normal'), (1, 'Geometric Normal'), (2, 'Un-bumped Normal'), (22, 'Bump Difference'),
                                                (4, 'Barycentric Coords'), (5, 'UV Coords'), (6, 'U Coords'), (7, 'V Coords'),
                                                (8, 'U Surface Derivative (dPdu)'), (9, 'V Surface Derivative (dPdv)'),
                                                (10, 'Shading Point (Relative to BBox)'), (11, 'Primitive ID'),
                                                (12, 'Triangle Wireframe'), (13, 'Polygon Wireframe'), (15, 'Object'),
                                                (16, 'Subdivision Edge Length'), (17, 'Floatgrid'), (18, 'Reflection Lines'),
-                                               (19, 'Bad UVs'), (20, 'Number of Lights'), (21, 'Object ID'),
-                                               (22, 'Bump Difference')])
+                                               (19, 'Bad UVs'), (20, 'Number of Lights'), (21, 'Object ID')])
     cmds.setUITemplate(popTemplate=True)
 
 def aiUtilitySetColorMode(attr):
