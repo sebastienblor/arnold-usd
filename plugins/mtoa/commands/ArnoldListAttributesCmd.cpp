ad7e56235368599a6b9f4702bf19acfefb698ae9
Pal Mezei <pal@solidangle.com>
2012-10-16 19:26:48 +0200

    Added listing attributes, with some debug code.

--- plugins/mtoa/commands/ArnoldListAttributesCmd.cpp
+++ plugins/mtoa/commands/ArnoldListAttributesCmd.cpp
@@ -7,6 +7,7 @@
 #include <maya/MDagPath.h>
 #include <maya/MFnDependencyNode.h>
 #include <maya/MFnAttribute.h>
+#include <maya/MStringArray.h>
 
 #include <map>
 #include <string>
@@ -24,6 +25,45 @@ CArnoldListAttributesCmd::~CArnoldListAttributesCmd()
 void* CArnoldListAttributesCmd::creator()
 {
    return new CArnoldListAttributesCmd();
+}
+
+static void GetAttributesFromNode(const MFnDependencyNode& node, std::map<std::string, std::string>& attributeList, std::string* fullName = 0)
+{
+   const unsigned int attributeCount = node.attributeCount();
+   for (unsigned int i = 0; i < attributeCount; ++i)
+   {      
+      MFnAttribute attr(node.attribute(i));
+      if (attr.isHidden())
+         continue;
+      if (!attr.isWritable())
+         continue;
+      const std::string attrName = attr.name().asChar();
+      std::map<std::string, std::string>::iterator it = attributeList.find(attrName);
+      if (it == attributeList.end())
+      {
+         if (fullName)
+            attributeList[attrName] = *fullName + std::string(".") + attrName;
+         else
+            attributeList[attrName] = std::string(node.name().asChar()) + std::string(".") + attrName;
+      }
+   }
+}
+
+static void GetAttributesFromDgPath(const MDagPath& dg, std::map<std::string, std::string>& attributeList)
+{   
+   if (dg.isInstanced())
+      if (dg.instanceNumber() != 0)
+         return;
+   std::string fullName = dg.fullPathName().asChar();
+   MFnDependencyNode node(dg.node());
+   GetAttributesFromNode(node, attributeList, &fullName);
+   unsigned int childCount = dg.childCount();
+   for (unsigned int i = 0; i < childCount; ++i)
+   {
+      MDagPath dg2 = dg;
+      dg2.push(dg.child(i));
+      GetAttributesFromDgPath(dg2, attributeList);
+   }
 }
 
 MStatus CArnoldListAttributesCmd::doIt(const MArgList& args)
@@ -53,6 +93,37 @@ MStatus CArnoldListAttributesCmd::doIt(const MArgList& args)
       status.perror("Cannot create MFnSet!");
       return status;
    }
+   slist.clear();
+   set.getMembers(slist, true);
+   
+   unsigned int numSetMembers = slist.length();
+   
+   std::map<std::string, std::string> attributeMap;
+   
+   for (unsigned int i = 0; i < numSetMembers; ++i)
+   {
+      MDagPath dg;
+      status = slist.getDagPath(i, dg);
+      if (status)
+         GetAttributesFromDgPath(dg, attributeMap);
+      else
+      {
+         MObject nodeObject;
+         status = slist.getDependNode(i, nodeObject);
+         if (status)
+            GetAttributesFromNode(nodeObject, attributeMap);
+      }
+   }
+   
+   MStringArray resultArray;
+   
+   resultArray.setLength((unsigned int)attributeMap.size());
+   
+   unsigned int id = 0;
+   for (std::map<std::string, std::string>::const_iterator it = attributeMap.begin(); it != attributeMap.end(); ++it)
+      resultArray[id++] = it->second.c_str();
+   
+   setResult(resultArray);
    
    return MS::kSuccess;
 }
