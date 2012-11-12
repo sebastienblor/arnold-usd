29a86c96dc641c86615335b547bcfab1f3c2f7ef
Pal Mezei <pal@solidangle.com>
2012-11-12 18:39:44 +0100

    Adding a skeleton driver for reporting the progress.

---
+++ plugins/mtoa/render/BatchProgressDriver.cpp
@@ -0,0 +1,50 @@
+#include <ai.h>
+
+#include <maya/MGlobal.h>
+
+AI_DRIVER_NODE_EXPORT_METHODS(batch_progress_driver_mtd);
+
+node_parameters
+{
+   
+}
+
+node_update
+{
+   
+}
+
+driver_supports_pixel_type
+{
+   return true;
+}
+
+driver_extension
+{
+   return NULL;
+}
+
+driver_open
+{
+   
+}
+
+driver_prepare_bucket
+{
+   
+}
+
+driver_write_bucket
+{
+
+}
+
+driver_close
+{
+   
+}
+
+node_finish
+{
+   
+}
