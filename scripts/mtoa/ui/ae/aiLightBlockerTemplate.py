8c91c0afa8d693a3db513a462b38a9d21e294d3f
Pal Mezei <pal@solidangle.com>
2012-12-07 19:59:02 +0100

    Rewriting aiLightBlocker template. (Ticket #805)

--- scripts/mtoa/ui/ae/aiLightBlockerTemplate.py
+++ scripts/mtoa/ui/ae/aiLightBlockerTemplate.py
@@ -1,28 +1,24 @@
-import maya.cmds as cmds
-import maya.mel as mel
-from mtoa.ui.ae.aiSwatchDisplay import aiSwatchDisplay
+import pymel.core as pm
+from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
 
-def AEaiLightBlockerTemplate(nodeName):
-
-    aiSwatchDisplay(nodeName)
-
-    cmds.editorTemplate(beginScrollLayout=True)
-
-    cmds.editorTemplate(beginLayout="Light Blocker Attributes", collapse=False)
-
-    cmds.editorTemplate("geometry_type", addControl=True, label="Geometry Type")
-    cmds.editorTemplate("density", addControl=True, label="Density")
-
-    cmds.editorTemplate(addSeparator=True)
-
-    cmds.editorTemplate("ramp_axis", addControl=True, label="Ramp Axis")
-    cmds.editorTemplate("ramp", addControl=True, label="Ramp")
-    cmds.editorTemplate("height_edge", addControl=True, label="Height Edge")
-    cmds.editorTemplate("width_edge", addControl=True, label="Width Edge")
-    cmds.editorTemplate("roundness", addControl=True, label="Roundness")
-
-    cmds.editorTemplate(endLayout=True)
-
-    cmds.editorTemplate(addExtraControls=True)
-    cmds.editorTemplate(endScrollLayout=True)
+class AEaiLightBlockerTemplate(ShaderAETemplate):
+    def setup(self):
+        self.addSwatch()
+        self.beginScrollLayout()
+        
+        self.beginLayout('Light Blocker Attributes', collapse=False)
+        self.addControl('geometry_type', label='Geometry Type')
+        self.addControl('density', label='Density')
+        self.addSeparator()
+        self.addControl('ramp_axis', label='Ramp Axis')
+        self.addControl('ramp', label='Ramp')
+        self.addControl('height_edge', label='Height Edge')
+        self.addControl('width_edge', label='Width Edge')
+        self.addControl('roundness', label='Roundness')
+        self.endLayout()
+        
+        pm.mel.AEdependNodeTemplate(self.nodeName)
+        
+        self.addExtraControls()
+        self.endScrollLayout()
 
