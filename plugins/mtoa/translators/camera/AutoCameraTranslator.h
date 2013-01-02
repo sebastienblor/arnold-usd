4558180fbd161cc338864fbbacc405acc857efd3
Pal Mezei <pal@solidangle.com>
2013-01-02 19:32:39 +0100

    Adding files for an auto camera translator node.

---
+++ plugins/mtoa/translators/camera/AutoCameraTranslator.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   AutoCameraTranslator.h
+ * Author: palmezei
+ *
+ * Created on January 2, 2013, 7:31 PM
+ */
+
+#ifndef AUTOCAMERATRANSLATOR_H
+#define	AUTOCAMERATRANSLATOR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* AUTOCAMERATRANSLATOR_H */
+
