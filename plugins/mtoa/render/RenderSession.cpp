561966d5e56426c5fb8a828b5fec1d92649af634
Pal Mezei <pal@solidangle.com>
2013-01-31 19:58:37 +0100

    Removing debug print.

--- plugins/mtoa/render/RenderSession.cpp
+++ plugins/mtoa/render/RenderSession.cpp
@@ -498,7 +498,6 @@ void CRenderSession::DoAssWrite(MString customFileName, const bool compressed)
 
       // FIXME : problem this is actually double filtering files
       // (Once at export to AiUniverse and once at file write from it)
-      std::cerr << "----------------" << m_renderOptions.expandProcedurals() << std::endl;
       AiASSWrite(fileName.asChar(), m_renderOptions.outputAssMask(), m_renderOptions.expandProcedurals());
    }
 }
