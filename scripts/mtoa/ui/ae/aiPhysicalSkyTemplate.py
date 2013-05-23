9e44c574dfa78a72c3034281e396d4b73f70c440
Pedro Gomez <pedro@solidangle.com>
2013-05-23 16:36:28 +0200

    Create a layout for the physical sky shader (Ticket #924)

---
+++ scripts/mtoa/ui/ae/aiPhysicalSkyTemplate.py
@@ -0,0 +1,29 @@
+import pymel.core as pm
+import mtoa.utils as utils
+import mtoa.ui.ae.utils as aeUtils
+import maya.cmds as cmds
+from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
+
+class AEaiPhysicalSkyTemplate(ShaderAETemplate):
+    def setup(self):
+        self.addSwatch()
+        self.beginScrollLayout()
+        
+        self.beginLayout("Physical Sky Attributes", collapse=False)
+        
+        self.addControl("turbidity", label="Turbidity")
+        self.addControl("ground_albedo", label="Ground Albedo")
+        self.addControl("sky_tint", label="Sky Tint")
+        
+        self.addControl("elevation", label="Elevation")
+        self.addControl("azimuth", label="Azimuth")
+        self.addControl("intensity", label="Intensity")
+        self.addControl("visible_solar_disc", label="Visible Solar Disc")
+        self.addControl("sun_tint", label="Sun Tint")
+        
+        self.endLayout()
+        
+        pm.mel.AEdependNodeTemplate(self.nodeName)
+        self.addExtraControls()
+        self.endScrollLayout()
+
