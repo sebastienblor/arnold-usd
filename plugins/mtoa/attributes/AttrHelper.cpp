558e0d7af76cb6499a5aeb96395d3b51a1ceff03
Pal Mezei <pal@solidangle.com>
2013-07-08 19:32:42 +0200

    Fixing missing attr_prefix. (Ticket #1053)

--- plugins/mtoa/attributes/AttrHelper.cpp
+++ plugins/mtoa/attributes/AttrHelper.cpp
@@ -1188,6 +1188,8 @@ void CBaseAttrHelper::ReadPrefixMetadata()
       const char* tmp;
       if (AiMetaDataGetStr(m_nodeEntry, NULL, "maya.attr_prefix", &tmp))
          m_prefix = tmp;
+      else
+         m_prefix = "";
    }
 }
 
