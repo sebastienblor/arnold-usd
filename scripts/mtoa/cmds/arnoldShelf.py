57e3e967baa02a9a96140ffbfcfc9180c19e0279
Pal Mezei <pal@solidangle.com>
2013-07-22 15:22:38 +0200

    Making some simple cosmetic changes to the shelf.

--- scripts/mtoa/cmds/arnoldShelf.py
+++ scripts/mtoa/cmds/arnoldShelf.py
@@ -5,8 +5,8 @@ import mtoa.utils as mutils
 import mtoa.ui.globals.settings as settings
 
 def removeArnoldShelf():
-   if cmds.shelfLayout('MtoA', exists=True):
-      cmds.deleteUI('MtoA')
+   if cmds.shelfLayout('Arnold', exists=True):
+      cmds.deleteUI('Arnold')
 
 def createPhysicalSky():
    if cmds.objExists('defaultArnoldRenderOptions'):
@@ -17,15 +17,14 @@ def createPhysicalSky():
 def createArnoldShelf():
    removeArnoldShelf()
    shelfTab = maya.mel.eval('global string $gShelfTopLevel;')
-   maya.mel.eval('global string $mtoaShelf;')
-   maya.mel.eval('$mtoaShelf = `shelfLayout -cellWidth 32 -cellHeight 32 -p $gShelfTopLevel MtoA`;')
-   cmds.shelfButton(command=lambda *args: cmds.arnoldFlushCache(textures=True), annotation='Flush Texture Cache', image='FlushTextureShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: cmds.arnoldFlushCache(skydome=True), annotation='Flush Background Cache', image='FlushBackgroundShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: cmds.arnoldFlushCache(quads=True), annotation='Flush Quad Caches', image='FlushQuadLightShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: cmds.arnoldFlushCache(flushall=True), annotation='Flush All Caches', image='FlushAllCachesShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: arnoldmenu.arnoldTxManager(), annotation='TX Manager', image='TXManagerShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: mutils.createLocator('aiAreaLight', asLight=True), annotation='Create Area Light', image='AreaLightShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: mutils.createLocator('aiSkyDomeLight', asLight=True), annotation='Create SkyDome Light', image='SkydomeLightShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: createPhysicalSky(), annotation='Create Physical Sky', image='PhysicalSkyShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: arnoldmenu.doCreateMeshLight(), annotation='Create Mesh Light', image='MeshLightShelf.png', style='iconOnly')
-   cmds.shelfButton(command=lambda *args: mutils.createLocator('aiPhotometricLight', asLight=True), annotation='Create Photometric Light', image='PhotometricLightShelf.png', style='iconOnly')
+   maya.mel.eval('global string $arnoldShelf;')
+   maya.mel.eval('$arnoldShelf = `shelfLayout -cellWidth 32 -cellHeight 32 -p $gShelfTopLevel Arnold`;')
+   cmds.shelfButton(label='Flush Texture Cache', command=lambda *args: cmds.arnoldFlushCache(textures=True), annotation='Flush Texture Cache', image='FlushTextureShelf.png', style='iconOnly')
+   cmds.shelfButton(label='Flush Background Cache', command=lambda *args: cmds.arnoldFlushCache(skydome=True), annotation='Flush Background Cache', image='FlushBackgroundShelf.png', style='iconOnly')
+   cmds.shelfButton(label='Flush Quad Caches', command=lambda *args: cmds.arnoldFlushCache(quads=True), annotation='Flush Quad Caches', image='FlushQuadLightShelf.png', style='iconOnly')
+   cmds.shelfButton(label='Flush All Caches', command=lambda *args: cmds.arnoldFlushCache(flushall=True), annotation='Flush All Caches', image='FlushAllCachesShelf.png', style='iconOnly')
+   cmds.shelfButton(label='TX Manager', command=lambda *args: arnoldmenu.arnoldTxManager(), annotation='TX Manager', image='TXManagerShelf.png', style='iconOnly')
+   cmds.shelfButton(label='Create Area Light', command=lambda *args: mutils.createLocator('aiAreaLight', asLight=True), annotation='Create Area Light', image='AreaLightShelf.png', style='iconOnly')
+   cmds.shelfButton(label='Create SkyDome Light', command=lambda *args: mutils.createLocator('aiSkyDomeLight', asLight=True), annotation='Create SkyDome Light', image='SkydomeLightShelf.png', style='iconOnly')
+   cmds.shelfButton(label='Create Physical Sky', command=lambda *args: createPhysicalSky(), annotation='Create Physical Sky', image='PhysicalSkyShelf.png', style='iconOnly')
+   cmds.shelfButton(label='Create Mesh Light', command=lambda *args: arnoldmenu.doCreateMeshLight(), annotation='Create Mesh Light', image='MeshLightShelf.png', style='iconOnly')
