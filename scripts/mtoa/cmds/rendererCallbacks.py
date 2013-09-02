a80360c0e176acf71df57ab5bc071f5f04f6ba4f
Pedro Gomez <pedro@solidangle.com>
2013-09-02 13:46:54 +0200

    Initial XGen Extension implementation

--- scripts/mtoa/cmds/rendererCallbacks.py
+++ scripts/mtoa/cmds/rendererCallbacks.py
@@ -3,6 +3,9 @@ import maya.mel as mel
 import mtoa.utils as utils
 from mtoa.ui.nodeTreeLister import aiHyperShadeCreateMenu_BuildMenu, createArnoldNodesTreeLister_Content
 import mtoa.ui.ae.templates as templates
+import xgenm as xg
+import ctypes
+import types
 
 def aiHyperShadePanelBuildCreateMenuCallback() :
     if cmds.pluginInfo('mtoa', query=True, loaded=True) :
@@ -34,6 +37,108 @@ def aiNodeCanBeUsedAsMaterialCallback(nodeId, nodeOwner ) :
         return 1
     else:
         return 0
+        
+def castSelf(selfid):
+    # Can't pass self as an object.
+    # It's cast to id(self) by the caller
+    # and we convert it back to a python object here
+    if isinstance(selfid,str):
+        return ctypes.cast( int(selfid), ctypes.py_object ).value
+    else:
+        return selfid
+      
+def aiExportFrame( self, frame, objFilename ):
+    '''Export a single mental ray archive frame.'''
+    cmds.currentTime( frame )
+
+    print "##### aiExportFrame ",objFilename
+    cmds.arnoldExportAss(f=objFilename+".ass", s=True)
+    '''cmds.Mayatomr( 	mi=True, exportFilter=7020488, 
+                active=True, binary=True, compression=9, 
+                fragmentExport=True, fragmentChildDag =True, passContributionMaps =True, 
+                assembly=True, assemblyRootName="obj", exportPathNames="n", 
+                file=objFilename + ".mi" )'''
+    print "##### aiExportFrame Exported"
+    self.log( "assExport " + objFilename + ".ass")
+
+def aiExportAppendFile( self, assFilename, material, obj, lod ):
+    lodList = self.tweakLodAppend( self.curFiles, lod  )
+    for l in lodList:
+        self.addArchiveFile( "ass", assFilename, material, "", l, 3 )
+        
+def aiExport( self, objs, filename, lod, materialNS ):
+    print "##### aiExport ",objs," ", filename, " ", lod, " ", materialNS, "\n"
+    filename = self.nestFilenameInDirectory( filename, "ass" )
+    
+    lastProgress = self.progress
+    self.splitProgress( len(objs) )
+    
+    self.log( "assExport " + filename + lod )
+    
+    # force units to centimeters when exporting mi.
+    #prevUnits = cmds.currentUnit( query=True, linear=True, fullName=True )
+    #cmds.currentUnit( linear="centimeter" )
+    
+    prevTime = cmds.currentTime( query=True )
+
+    for obj in objs:
+        objFilename = filename + "_" + obj + lod
+        cmds.select( obj, r=True )
+
+        filenames = []
+        # Choose to export single file or a sequence.
+        frameToken = ""
+        if self.startFrame != self.endFrame:
+            print "##### aiExport Multiframe"
+            frameToken =".${FRAME}"
+
+            dummyFrameFile = open( objFilename + frameToken + ".ass.gz", "wt" )
+            dummyFrameFile.write( "STARTFRAME=%4.4d\nENDFRAME=%4.4d\n" % (int( self.startFrame), int(self.endFrame) ) )
+            dummyFrameFile.close()
+
+            for curFrame in range( int(self.startFrame), int(self.endFrame)+ 1 ):
+                aiExportFrame( self, curFrame, objFilename + ".%4.4d" % int(curFrame) )
+        else:
+            print "##### aiExport 1 frame"
+            aiExportFrame( self, self.startFrame, objFilename )
+        
+        if self.curFiles != None:
+            print "##### aiExport curFiles ",self.curFiles
+            materials = self.getSGsFromObj( obj )
+            if materials and len(materials)>0 :
+                assFilename = objFilename + frameToken + ".ass.gz"
+                aiExportAppendFile( self, assFilename, materialNS+materials[0], obj, lod )
+        self.incProgress()
+    
+    #cmds.currentUnit( linear=prevUnits )
+    cmds.currentTime( prevTime )
+    
+    self.progress = lastProgress
+    print "##### aiExport END"
+        
+def xgaiArchiveExport(selfid) :
+    print "##### xgaiArchiveExport ",selfid
+    self = castSelf(selfid)
+    aiExport( self, self.invokeArgs[0], self.invokeArgs[1], self.invokeArgs[2], self.invokeArgs[3] )
+    
+def xgaiArchiveExportInfo(selfid) :
+    print "##### xgaiArchiveExportInfo ",selfid
+    self = castSelf(selfid)
+    self.archiveDirs.append( "ass" )
+    self.archiveLODBeforeExt.append( ".${FRAME}.ass" )
+    self.archiveLODBeforeExt.append( ".${FRAME}.ass.gz" )
+    self.archiveLODBeforeExt.append( ".ass" )
+    self.archiveLODBeforeExt.append( ".ass.gz" )
+    
+def xgaiArchiveExportInit(selfid) :
+    print "##### xgaiArchiveExportInit ",selfid
+    
+xg.registerCallback( "ArchiveExport", "mtoa.cmds.rendererCallbacks.xgaiArchiveExport" )
+xg.registerCallback( "ArchiveExportInfo", "mtoa.cmds.rendererCallbacks.xgaiArchiveExportInfo" )
+xg.registerCallback( "ArchiveExportInit", "mtoa.cmds.rendererCallbacks.xgaiArchiveExportInit" )
+'''cmds.callbacks(addCallback=aiArchiveExport,
+               hook="ArchiveExport",
+               owner="arnold")'''
 
 # Add the callbacks
 def registerCallbacks():
@@ -50,6 +155,7 @@ def registerCallbacks():
     cmds.callbacks(addCallback=aiCreateRenderNodeSelectNodeCategoriesCallback,
                    hook="createRenderNodeSelectNodeCategories",
                    owner="arnold")
+               
 
     # FIXME: Maya doc is wrong
     #cmds.callbacks(addCallback=aiRenderNodeClassificationCallback,
