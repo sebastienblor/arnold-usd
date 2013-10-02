04b750e02451351eecae923e615a6290c9b14b87
Pal Mezei <pal@solidangle.com>
2013-10-02 20:14:18 +0200

    Adding some more code.

--- shaders/src/MayaFluidTexture2D.cpp
+++ shaders/src/MayaFluidTexture2D.cpp
@@ -4,6 +4,31 @@
 
 AI_SHADER_NODE_EXPORT_METHODS(MayaFluidTexture2DMtd);
 
+struct MayaFluidTexture2D {
+   CMayaFluidData<false>* fluidData;
+
+   static void* operator new(size_t size)
+   {
+      return AiMalloc(size);
+   }
+   
+   static void operator delete(void* p)
+   {
+      AiFree(p);
+   }
+
+   MayaFluidTexture2D() : fluidData(0)
+   {
+
+   }
+
+   ~MayaFluidTexture2D()
+   {
+      if (fluidData)
+         delete fluidData;
+   }
+};
+
 node_parameters
 {
    InitializeFluidShaderParameters(params, false);
@@ -13,17 +38,25 @@ node_parameters
 
 node_initialize
 {
+   AiNodeSetLocalData(node, new MayaFluidTexture2D());
 }
 
 node_update
 {
+   MayaFluidTexture2D* data = reinterpret_cast<MayaFluidTexture2D*>(AiNodeGetLocalData(node));
+   if (data->fluidData)
+      delete data->fluidData;
+   data->fluidData = new CMayaFluidData<false>(node);
 }
 
 node_finish
 {
+   delete reinterpret_cast<MayaFluidTexture2D*>(AiNodeGetLocalData(node));
 }
 
 shader_evaluate
 {
-   sg->out.RGB = AI_RGB_RED;
+   MayaFluidTexture2D* data = reinterpret_cast<MayaFluidTexture2D*>(AiNodeGetLocalData(node));
+   AtVector lpt = {sg->u, sg->v};
+   sg->out.RGB = data->fluidData->readDensity(lpt, FT_LINEAR);
 }
