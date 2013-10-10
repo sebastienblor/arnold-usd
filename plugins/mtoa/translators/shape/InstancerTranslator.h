6c064b7760fae8bd2ec23b05cb22303214aad7db
Pal Mezei <pal@solidangle.com>
2013-10-10 16:06:36 +0200

    Fixing auto search and replace errors. (Ticket #1545)

--- plugins/mtoa/translators/shape/InstancerTranslator.h
+++ plugins/mtoa/translators/shape/InstancerTranslator.h
@@ -38,14 +38,14 @@ public:
    static void NodeInitializer(CAbTranslator context);
    virtual void Update(AtNode* anode);
    void Export(AtNode* anode);
-   void ExportMotion(AtNode* anode, unsigned intstep);
-   virtual void UpdateMotion(AtNode* anode, unsigned intstep);
+   void ExportMotion(AtNode* anode, unsinged int step);
+   virtual void UpdateMotion(AtNode* anode, unsinged int step);
 
 
 protected:
    int ComputeMasterVisibility(const MDagPath& masterDagPath) const;
    virtual void ExportInstancer(AtNode* instancer, bool update);
-   virtual void ExportInstances(AtNode* instancer, unsigned intstep);
+   virtual void ExportInstances(AtNode* instancer, unsinged int step);
 
 
 protected:
