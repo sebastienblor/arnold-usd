9e9136e77bc6742638f6c26d5f2ed94e98d70670
Pal Mezei <pal@solidangle.com>
2013-12-09 18:01:25 +0100

    Cleaning up aiLightDecay's template code.

--- scripts/mtoa/ui/ae/aiLightDecayTemplate.py
+++ scripts/mtoa/ui/ae/aiLightDecayTemplate.py
@@ -1,15 +1,6 @@
 import mtoa.ui.ae.utils as aeUtils
 import pymel.core as pm
 from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
-'''
-def checkNearAtten(nodeName):
-    aeUtils.arnoldDimControlIfFalse(nodeName, 'near_start', 'use_near_atten')
-    aeUtils.arnoldDimControlIfFalse(nodeName, 'near_end', 'use_near_atten')
-
-def checkFarAtten(nodeName):
-    aeUtils.arnoldDimControlIfFalse(nodeName, 'far_start', 'use_far_atten')
-    aeUtils.arnoldDimControlIfFalse(nodeName, 'far_end', 'use_far_atten')
-'''
 
 class AEaiLightDecayTemplate(ShaderAETemplate):
     def checkNearAtten(self, nodeName):
@@ -34,14 +25,12 @@ class AEaiLightDecayTemplate(ShaderAETemplate):
         
         self.beginLayout('Attenuation', collapse=False)
 
-        #cmds.editorTemplate("use_near_atten", aeCallback(checkNearAtten), addControl=True, label="Use Near Attenuation")
         self.addControl('use_near_atten', changeCommand=self.checkNearAtten, label='Use Near Attenuation')
         self.addControl('near_start', label='Near Start')
         self.addControl('near_end', label='Near End')
 
         self.addSeparator()
         
-        #cmds.editorTemplate("use_far_atten", aeCallback(checkNearAtten), addControl=True, label="Use Far Attenuation")        
         self.addControl('use_far_atten', changeCommand=self.checkFarAtten, label='Use Far Attenuation')
         self.addControl('far_start', label='Far Start')
         self.addControl('far_end', label='Far End')
