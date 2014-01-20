0579de3726f31080fd6f7be384587509221bdaa5
Pal Mezei <pal@solidangle.com>
2014-01-20 19:01:42 +0100

    Dumping all the fragment shaders into a hardcoded dir.

--- plugins/mtoa/viewport2/ArnoldVP2Command.hpp
+++ plugins/mtoa/viewport2/ArnoldVP2Command.hpp
@@ -4,20 +4,41 @@
 #include <maya/MString.h>
 #include <maya/MFragmentManager.h>
 #include <maya/MArgList.h>
+#include <maya/MString.h>
+#include <maya/MStringArray.h>
 
 class CArnoldVP2Command : public MPxCommand{
 public:
-	static void* creator();
+    static void* creator();
 
-	virtual MStatus doIt(const MArgList& args);
+    virtual MStatus doIt(const MArgList& args);
 };
 
 void* CArnoldVP2Command::creator()
 {
-	return new CArnoldVP2Command();
+    return new CArnoldVP2Command();
 }
+
+#include <fstream>
+#include <string>
 
 MStatus CArnoldVP2Command::doIt(const MArgList& args)
 {
-	return MS::kSuccess;
+    MStringArray sArray;
+    MHWRender::MRenderer* theRenderer = MHWRender::MRenderer::theRenderer();
+    if (theRenderer == 0)
+        return MS::kFailure;
+    MHWRender::MFragmentManager* fmg = theRenderer->getFragmentManager();
+    if (fmg == 0)
+        return MS::kFailure;
+    fmg->fragmentList(sArray);
+    for (unsigned int i = 0; i < sArray.length(); ++i)
+    {
+        std::string fragmentName = sArray[i].asChar();
+        std::fstream fs((std::string("/work/mayaFragments") + fragmentName).c_str(), std::ios::out);
+        MString fragmentString;
+        fmg->getFragmentXML(fragmentName.c_str(), fragmentString);
+        fs << fragmentString.asChar();
+    }
+    return MS::kSuccess;
 }
