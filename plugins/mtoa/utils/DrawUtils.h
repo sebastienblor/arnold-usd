da4a11202933589c5fad204bbbc4450f4fcb79f0
Pal Mezei <pal@solidangle.com>
2014-02-26 17:15:55 +0100

    Removing glu functions from light blocker node.

--- plugins/mtoa/utils/DrawUtils.h
+++ plugins/mtoa/utils/DrawUtils.h
@@ -58,7 +58,7 @@ public:
 
 class CCylinderPrimitive : public CLinePrimitiveData{
 public:
-   CCylinderPrimitive(GLsizei resolution = 20)
+   CCylinderPrimitive(float radius = 1.0f, float height = 1.0f, GLsizei resolution = 20)
    {
       vertices.resize(resolution * 6);
       const GLsizei indexDiff = resolution * 3;
@@ -66,13 +66,15 @@ public:
       {
          const float d = AI_PITIMES2 * (float(i) / float(resolution));
          float* v = &vertices[i * 3];
-         v[0] = cosf(d);
-         v[1] = 1.0f;
-         v[2] =  sinf(d);
+         const float x = cosf(d) * radius;
+         const float z = sinf(d) * radius;
+         v[0] = x;
+         v[1] = height;
+         v[2] = z;
          v = &vertices[i * 3 + indexDiff];
-         v[0] = cosf(d);
-         v[1] = -1.0f;
-         v[2] = sinf(d);
+         v[0] = x;
+         v[1] = -height;
+         v[2] = z;
       }
       indices.resize(resolution * 6);
       const GLsizei res2 = resolution * 2;
@@ -87,6 +89,63 @@ public:
          const GLsizei i2o = i2 + resolution * 4;
          indices[i2o] = i;
          indices[i2o + 1] = i + resolution;
+      }
+   }
+};
+
+class CSpherePrimitive : public CLinePrimitiveData{
+public:
+   CSpherePrimitive(float radius = 1.0f, GLsizei resolution = 16)
+   {
+      // 0 is the bottom point
+      // 1 is the top point
+      vertices.resize((resolution * resolution + 2) * 6);
+      vertices[0] = 0.0f;
+      vertices[1] = -radius;
+      vertices[2] = 0.0f;
+      vertices[3] = 0.0f;
+      vertices[4] = radius;
+      vertices[5] = 0.0f;
+      GLsizei vid = 5;
+      for (GLsizei yy = 0; yy < resolution; ++yy)
+      {
+         const float dy = AI_PI * float(yy) / float(resolution) - AI_PIOVER2;
+         const float y = sinf(dy) * radius;
+         const float pr = cosf(dy) * radius;
+         for (GLsizei xx = 0; xx < resolution; ++xx)
+         {
+            const float dx = AI_PITIMES2 * float(xx) / float(resolution);
+            vertices[++vid] = cosf(dx) * pr;
+            vertices[++vid] = y;
+            vertices[++vid] = sinf(dx) * pr;
+         }
+      }
+      indices.resize(resolution * resolution * 2 + // for horizontal lines
+         (resolution + 1) * resolution * 2); // for vertical lines
+      // fill horizontal lines
+      GLsizei id = 0;
+      for (GLsizei yy = 0; yy < resolution; ++yy)
+      {
+         const GLsizei wy = 2 + yy * resolution;
+         for (GLsizei xx = 0; xx < resolution; ++xx)
+         {
+            indices[id++] = wy + xx;
+            indices[id++] = wy + (xx + 1) % resolution;
+         }
+      }
+
+      for (GLsizei xx = 0; xx < resolution; ++xx)
+      {
+         const GLsizei xx2 = 2 + xx;
+         indices[id++] = 0;         
+         indices[id++] = xx2;
+         for (GLsizei yy = 0; yy < (resolution - 1); ++yy)
+         {
+            indices[id++] = xx2 + yy * resolution;
+            indices[id++] = xx2 + (yy + 1) * resolution;
+         }
+         indices[id++] = xx2 + (resolution - 1) * resolution;
+         indices[id++] = 1;
       }
    }
 };
