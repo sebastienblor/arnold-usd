e7230bc52bd512d10c861d103fef0c1675e9403a
Pedro Gomez <pedro@solidangle.com>
2014-06-04 13:23:22 +0200

    Fix XGen MtoA loading order issue

--- contrib/extensions/xgen/plugin/xgenTranslator.py
+++ contrib/extensions/xgen/plugin/xgenTranslator.py
@@ -30,8 +30,15 @@ templates.registerTranslatorUI(xgmDescriptionTemplate, "xgmDescription", "xgenTr
 # these  are used to build the  "renderer"  callbacks to slot arnold  settings into the  xgen GUI.   the values of these controls 
 # are stored on  each description node and can be parsed from there by the translator.
 # this is mainly to make  arnold fit into the   XGen workflow better. 
-xg.deregisterCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldRefresh" )
-xg.registerCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldUI" )
-xg.registerCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldRefresh" )
-xg.xgGlobal.DescriptionEditor.refresh("Full")
-xg.deregisterCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldUI" )
+
+if xg.xgGlobal.DescriptionEditor is not None:
+    # XGen has already been loaded
+    xg.deregisterCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldRefresh" )
+    xg.registerCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldUI" )
+    xg.registerCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldRefresh" )
+    xg.xgGlobal.DescriptionEditor.refresh("Full")
+    xg.deregisterCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldUI" )
+else:
+    # XGen has still not been loaded
+    xg.registerCallback( "RenderAPIRendererTabUIInit", "xgenArnoldUI.xgArnoldUI" )
+    xg.registerCallback( "RenderAPIRendererTabUIRefresh", "xgenArnoldUI.xgArnoldRefresh" )
