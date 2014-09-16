298dc7a2d167a8d84377e968c20d2331504d46ea
Pal Mezei <pal@solidangle.com>
2014-09-16 15:30:40 +0200

    Fixing spacing stuff.

--- plugins/mtoa/viewport2/standInBBox.hlsl
+++ plugins/mtoa/viewport2/standInBBox.hlsl
@@ -7,8 +7,8 @@ cbuffer ConstantBuffer : register(b0)
 }
 
 void mainVS(
-	float4 vertex : POSITION,
-	out float4 position: SV_POSITION)
+    float4 vertex : POSITION,
+    out float4 position: SV_POSITION)
 { 
     position = mul(vertex * scale + offset, wvp);
 }
