94b8e43cbbafe9d07c23da79d52bd7c51ca66345
Pal Mezei <pal@solidangle.com>
2014-11-18 21:41:37 +0100

    Adding rough support for meshlight material writing onto its own aov. (Ticket #1945)

--- shaders/src/MeshLightMaterial.cpp
+++ shaders/src/MeshLightMaterial.cpp
@@ -4,18 +4,21 @@ AI_SHADER_NODE_EXPORT_METHODS(MeshLightMaterialMtd);
 
 enum MeshLightMaterialParams{
    p_color = 0,
-   p_color_multiplier
+   p_color_multiplier,
+   p_aov_meshlight_beauty
 };
 
 node_parameters
 {
    AiParameterRGB("color", 0.f, 0.f, 0.f);
    AiParameterRGB("color_multiplier", 0.0f, 0.0f, 0.0f);
+   AiParameterStr("aov_meshlight_beauty", "meshlight_beauty");
    
    AiMetaDataSetBool(mds, "color", "always_linear", true); // no gamma correction
    AiMetaDataSetStr(mds, NULL, "maya.name", "aiMeshLightMaterial");
    AiMetaDataSetInt(mds, NULL, "maya.id", 0x00115D1B);
-   AiMetaDataSetBool(mds, NULL, "maya.hide", true);
+   //AiMetaDataSetBool(mds, NULL, "maya.hide", true);
+   AiMetaDataSetStr(mds, NULL, "maya.classification", "shader/surface");
 }
 
 node_initialize
@@ -25,7 +28,7 @@ node_initialize
 
 node_update
 {
-   
+   AiAOVRegister(AiNodeGetStr(node, "aov_meshlight_beauty"), AI_TYPE_RGB, AI_AOV_BLEND_NONE);
 }
 
 node_finish
@@ -49,4 +52,7 @@ shader_evaluate
 
    sg->out.RGB = AiShaderEvalParamRGB(p_color) * AiShaderEvalParamRGB(p_color_multiplier);
    sg->out.RGBA.a = 1.f;
+
+   if (sg->Rt & AI_RAY_CAMERA)
+      AiAOVSetRGB(sg, AiShaderEvalParamStr(p_aov_meshlight_beauty), sg->out.RGB);
 }
