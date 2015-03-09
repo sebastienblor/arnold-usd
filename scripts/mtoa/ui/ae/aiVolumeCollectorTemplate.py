98de7a020612b4b387510b6486d147ef6c77c245
Pedro Gomez <pedro@solidangle.com>
2015-03-09 19:01:03 +0100

    Update the volume_collector and the vdb plugins to the HtoA 1.3.0 version (Ticket #2025)

--- scripts/mtoa/ui/ae/aiVolumeCollectorTemplate.py
+++ scripts/mtoa/ui/ae/aiVolumeCollectorTemplate.py
@@ -2,10 +2,47 @@ import pymel.core as pm
 from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
 
 class AEaiVolumeCollectorTemplate(ShaderAETemplate):
+    def scatteringSource(self, nodeName):
+        source = self.nodeAttr('scattering_source')
+        sourceVal = pm.getAttr(source)
+        print sourceVal
+        if(sourceVal == 0):
+            pm.editorTemplate(dimControl=(nodeName, "scattering", False))
+            pm.editorTemplate(dimControl=(nodeName, "scatteringChannel", True))
+        else:
+            pm.editorTemplate(dimControl=(nodeName, "scattering", True))
+            pm.editorTemplate(dimControl=(nodeName, "scatteringChannel", False))
+            
+    def attenuationSource(self, nodeName):
+        source = self.nodeAttr('attenuation_source')
+        sourceVal = pm.getAttr(source)
+        print sourceVal
+        if(sourceVal == 0):
+            pm.editorTemplate(dimControl=(nodeName, "attenuation", False))
+            pm.editorTemplate(dimControl=(nodeName, "attenuationChannel", True))
+        elif(sourceVal == 1):
+            pm.editorTemplate(dimControl=(nodeName, "attenuation", True))
+            pm.editorTemplate(dimControl=(nodeName, "attenuationChannel", False))
+        else:
+            pm.editorTemplate(dimControl=(nodeName, "attenuation", True))
+            pm.editorTemplate(dimControl=(nodeName, "attenuationChannel", True))
+            
+    def emissionSource(self, nodeName):
+        source = self.nodeAttr('emission_source')
+        sourceVal = pm.getAttr(source)
+        print sourceVal
+        if(sourceVal == 0):
+            pm.editorTemplate(dimControl=(nodeName, "emission", False))
+            pm.editorTemplate(dimControl=(nodeName, "emissionChannel", True))
+        else:
+            pm.editorTemplate(dimControl=(nodeName, "emission", True))
+            pm.editorTemplate(dimControl=(nodeName, "emissionChannel", False))
+            
     def setup(self):
         self.beginScrollLayout()
         
         self.beginLayout('Scattering', collapse=False)
+        self.addControl('scattering_source',changeCommand=self.scatteringSource)
         self.addControl('scattering', label='Scattering')
         self.addControl('scattering_channel')
         self.addControl('scattering_color')
@@ -14,15 +51,16 @@ class AEaiVolumeCollectorTemplate(ShaderAETemplate):
         self.endLayout()
         
         self.beginLayout('Attenuation', collapse=False)
-        self.addControl('attenuation_mode', label='Attenuation Mode')
-        self.addControl('attenuation_use_scattering')
+        self.addControl('attenuation_source',changeCommand=self.attenuationSource)
         self.addControl('attenuation')
         self.addControl('attenuation_channel')
         self.addControl('attenuation_color')
         self.addControl('attenuation_intensity')
+        self.addControl('attenuation_mode', label='Attenuation Mode')
         self.endLayout()
         
         self.beginLayout('Emission', collapse=False)
+        self.addControl('emission_source',changeCommand=self.emissionSource)
         self.addControl('emission', label='Emission')
         self.addControl('emission_channel')
         self.addControl('emission_color')
