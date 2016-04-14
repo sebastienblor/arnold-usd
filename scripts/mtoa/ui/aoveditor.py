f434a82b840436cd295602fef8ef0fc18c1b6a86
Jonathan Feldstein <jonathan.feldstein@autodesk.com>
2016-04-14 10:03:01 -0400

    Cleaned up version of AOV callbacks.

--- scripts/mtoa/ui/aoveditor.py
+++ scripts/mtoa/ui/aoveditor.py
@@ -26,6 +26,9 @@ def _uiName(tag):
 global _updating
 _updating = False
 
+global _tabEditor
+_tabEditor = None
+
 AOV_CALLBACK_ATTRS = ('type', 'defaultValue')
 
 class AOVBrowser(object):
@@ -772,7 +775,8 @@ def createArnoldAOVTab():
 
     cmds.columnLayout('arnoldTabColumn', adjustableColumn=True)
 
-    ed = ArnoldAOVEditor(aovNode)
+    global _tabEditor
+    _tabEditor = ArnoldAOVEditor(aovNode)
 
     cmds.formLayout(parentForm,
                edit=True,
@@ -788,11 +792,16 @@ def createArnoldAOVTab():
     pm.setUITemplate('attributeEditorTemplate', popTemplate=True)
 
     cmds.setParent(parentForm)
-    pm.evalDeferred(ed.fixOptionMenus)
-    ed.setEnabledState()
-    pm.scriptJob(attributeChange = (aovNode.node.aovMode, ed.setEnabledState), parent=ed.mainCol)
+    pm.evalDeferred(_tabEditor.fixOptionMenus)
+    _tabEditor.setEnabledState()
+    pm.scriptJob(attributeChange = (aovNode.node.aovMode, _tabEditor.setEnabledState), parent=_tabEditor.mainCol)
 
     #cmds.setUITemplate(popTemplate=True)
 
 def updateArnoldAOVTab():
     pass
+
+def refreshArnoldAOVTab():
+    global _tabEditor
+    if _tabEditor:
+        _tabEditor.refresh()
