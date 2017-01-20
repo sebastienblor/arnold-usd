a3a84392508a04740f7b06b6bd644c5499b12681
sebastien.ortega <sebastien@solidangle.com>
2017-01-20 12:14:10 +0100

    Get the openvdb bounding box from the vdb file (#2264)

--- scripts/mtoa/volume_vdb.py
+++ scripts/mtoa/volume_vdb.py
@@ -33,3 +33,31 @@ def GetChannelNames(filename):
     channel_names = [AiArrayGetStr(channel_array, i) for i in xrange(channel_array.contents.nelements)]
     return channel_names
 
+
+
+def GetChannelBounds(filename, channel_names):
+    if not dso:
+        return []
+    
+    # space separated names is acceptable
+    if isinstance(channel_names, basestring):
+        channel_names = channel_names.split()
+
+    if len(channel_names) == 0:
+        channel_names = GetChannelNames(filename)
+
+    # expand list with clusters that match the beginning of the grid name
+    all_channel_array = dso.channelNames(filename)
+    all_channel_names = [AiArrayGetStr(all_channel_array, i) for i in xrange(all_channel_array.contents.nelements)]
+
+    for chan in channel_names:
+        channel_names.extend(filter(lambda x: x != chan and x.startswith(chan), all_channel_names))
+    
+    channel_array = AiArrayAllocate(len(channel_names), 1, AI_TYPE_STRING)
+    for i in xrange(channel_array.contents.nelements):
+        AiArraySetStr(channel_array, i, channel_names[i])
+    
+    bbox = dso.channelBBox(filename, channel_array)
+
+    return [bbox.min.x, bbox.min.y, bbox.min.z, bbox.max.x, bbox.max.y, bbox.max.z]
+
