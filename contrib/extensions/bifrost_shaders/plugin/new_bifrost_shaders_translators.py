ac21467bd63f2980bcee5daabec158ec7c38278a
sebastien.ortega <sebastien@solidangle.com>
2017-03-08 19:36:34 +0100

    Removing deprecated bifrost_shaders

--- contrib/extensions/bifrost_shaders/plugin/new_bifrost_shaders_translators.py
+++
@@ -1,9 +0,0 @@
-import mtoa.ui.ae.templates as templates
-from mtoa.ui.ae.templates import AttributeTemplate
-
-class BifrostHydroTemplate(AttributeTemplate):
-    def setup(self):
-        self.addControl("aiRRDepth", label = "Russian Roulette Depth");
-        self.addControl("aiRoughnessDepthScale", label = "Roughness Depth Scale");
-
-templates.registerAETemplate(BifrostHydroTemplate, 'bifrostHydro')
