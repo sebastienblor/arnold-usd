37f953b6568eccbf457e911f44cf5f00f38e38f7
sebastien.ortega <sebastien@solidangle.com>
2017-08-09 12:10:25 +0200

    Updating test 232 images

--- testsuite/test_0232/data/post_script.py
+++ testsuite/test_0232/data/post_script.py
@@ -3,26 +3,40 @@ import sys
 test_root_path = sys.argv[1]
 oiiotool_path  = sys.argv[2]
 
-index = 0
-for root, dirs, files in os.walk(test_root_path):
-	for img in files:
-		if not img.endswith('.exr'):
-			continue
-		if not 'testrender' in img:
-			continue
-		
-		outImg = "testrender"
-		outImg += str(index)
-		outImg += ".tif"
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderTorusShape.exr"), os.path.join(test_root_path, "testrender1.tif"))
+os.system(cmd)
 
-		inputImg = os.path.join(test_root_path, img)
-		outImg = os.path.join(test_root_path, outImg)
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderTorus1Shape.exr"), os.path.join(test_root_path, "testrender2.tif"))
+os.system(cmd)
 
-		if 'Aov' in img:
-			cmd = ('%s %s -ch "R,G,B" -o %s') % (oiiotool_path, inputImg, outImg)
-		else:
-			cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, inputImg, outImg)
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderSphereShape.exr"), os.path.join(test_root_path, "testrender3.tif"))
+os.system(cmd)
 
-		os.system(cmd)
-		index = index+1
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderSphere1Shape.exr"), os.path.join(test_root_path, "testrender4.tif"))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderSphere2Shape.exr"), os.path.join(test_root_path, "testrender5.tif"))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderAovShape.exr"), os.path.join(test_root_path, "testrender6.tif"))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderAovShape.diffuse_direct.exr"), os.path.join(test_root_path, "testrender7.tif"))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderAovShape.emission.exr"), os.path.join(test_root_path, "testrender8.tif"))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B" -o %s') % (oiiotool_path, os.path.join(test_root_path, "testrenderAovShape.specular_direct.exr"), os.path.join(test_root_path, "testrender9.tif"))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "aiStandardSurface1SG_testrenderTorus2Shape_1001.exr"), os.path.join(test_root_path, "testrender10.tif"))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "aiStandardSurface1SG_testrenderTorus3Shape_1001.exr"), os.path.join(test_root_path, "testrender11.tif"))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "aiStandardSurface1SG_testrenderTorus3Shape_1012.exr"), os.path.join(test_root_path, "testrender12.tif"))
+os.system(cmd)
+
 
