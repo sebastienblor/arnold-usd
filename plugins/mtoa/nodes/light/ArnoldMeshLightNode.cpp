sebastienblor/arnold-usd

commit e93d7bea69c662f5d7065ea891bc512af4662ce6

Mesh light attributes were created twice (#2093)

plugins/mtoa/nodes/light/ArnoldMeshLightNode.cpp
@@ -317,7 +317,6 @@ MStatus CArnoldMeshLightNode::initialize()
    nAttr.setStorable(true);
    nAttr.setKeyable(true);
    nAttr.setMin(0);
-   nAttr.setSoftMin(0);
    nAttr.setSoftMax(10);
    nAttr.setChannelBox(true);
    addAttribute(s_intensity);

plugins/mtoa/translators/light/LightTranslators.cpp
@@ -634,3 +634,13 @@ void CMeshLightNewTranslator::NodeChanged(MObject& node, MPlug& plug)
    
    CMeshLightTranslator::NodeChanged(node, plug);
 }
+
+void CMeshLightNewTranslator::NodeInitializer(CAbTranslator context)
+{
+   CExtensionAttrHelper helper(context.maya, "mesh_light");
+   // common attributes
+   MakeCommonAttributes(helper);
+   helper.MakeInput("shadow_color");
+   helper.MakeInput("cast_shadows");
+   helper.MakeInput("cast_volumetric_shadows");
+}

plugins/mtoa/translators/light/LightTranslators.h
@@ -186,6 +186,8 @@ class CMeshLightNewTranslator : public CMeshLightTranslator
 public:
    CMeshLightNewTranslator() : CMeshLightTranslator() {}
    virtual MObject GetMeshObject() const;
+   static void NodeInitializer(CAbTranslator context);
+   
 
    static void* creator()
    {

commit 6389234217b28e46a76ce5106d7e5788b687d6b7

Fixing regression introduced in #2093

plugins/mtoa/nodes/light/ArnoldMeshLightNode.cpp
@@ -321,7 +321,7 @@ MStatus CArnoldMeshLightNode::initialize()
    nAttr.setChannelBox(true);
    addAttribute(s_intensity);
 
-   s_lightVisible = nAttr.create("lightVisible", "lightVisible", MFnNumericData::kBoolean, 0);
+   s_lightVisible = nAttr.create("lightVisible", "light_visible", MFnNumericData::kBoolean, 0);
    nAttr.setHidden(false);
    nAttr.setKeyable(true);
    nAttr.setChannelBox(false);