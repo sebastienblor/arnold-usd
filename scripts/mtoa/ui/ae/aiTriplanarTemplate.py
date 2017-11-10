6332ad16d3e0e75e06c0c8d7235964b13385edef
sebastien.ortega <sebastien@solidangle.com>
2017-11-10 11:05:00 +0100

    Exposing cell attributes in triplanar

--- scripts/mtoa/ui/ae/aiTriplanarTemplate.py
+++ scripts/mtoa/ui/ae/aiTriplanarTemplate.py
@@ -2,6 +2,14 @@ import pymel.core as pm
 from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
 
 class AEaiTriplanarTemplate(ShaderAETemplate):
+    def updateCell(self, nodeName):
+        cellAttr = '%s.%s' % (nodeName, 'cell')
+        cellValue = pm.getAttr(cellAttr)
+        
+        pm.editorTemplate(dimControl=(nodeName, 'cellRotate', not cellValue))
+        pm.editorTemplate(dimControl=(nodeName, 'cellBlend', not cellValue))
+            
+
     def setup(self):
         self.addSwatch()
         self.beginScrollLayout()
@@ -17,7 +25,13 @@ class AEaiTriplanarTemplate(ShaderAETemplate):
         self.addControl("rotate", label="Rotate")
         self.addControl("offset", label="Offset")
         self.endLayout()
-        
+
+        self.beginLayout("Cell", collapse=False)
+        self.addControl("cell", changeCommand=self.updateCell, label="Enable Cell")
+        self.addControl("cellRotate", label="Rotate")
+        self.addControl("cellBlend", label="Blend")
+        self.endLayout()
+                
         pm.mel.AEdependNodeTemplate(self.nodeName)
 
         self.addExtraControls()
