a123587e45e99ce6db6df8debda4952b412b9254
sebastien.ortega <sebastien@solidangle.com>
2018-02-22 14:16:25 +0100

    Better filtering on which ARV menus appear in viewport rendering (#3343)

--- plugins/mtoa/render/display/renderview_mtoa.cpp
+++ plugins/mtoa/render/display/renderview_mtoa.cpp
@@ -501,7 +501,7 @@ void CRenderViewMtoA::OpenMtoAViewportRendererOptions()
    }
    workspaceCmd += " \"ArnoldViewportRendererOptions\""; // name of the workspace, to get it back later
 
-   std::string menusFilter = "AOVs;Crop Region;Refresh Render;Update Full Scene;Abort Render";
+   std::string menusFilter = "Crop Region;AOVs;Refresh Render;Update Full Scene;Abort Render;Log;Save UI Threads;Debug Shading;Isolate Selection;Lock Selection";
    CRenderViewInterface::OpenOptionsWindow(200, 50, menusFilter.c_str(), MQtUtil::mainWindow());
 
    QMainWindow *optWin = GetOptionsWindow();
