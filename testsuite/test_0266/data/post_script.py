e7a4fe2cce52e3c243c09949573cf101402a86d6
sebastien.ortega <sebastien@solidangle.com>
2018-06-01 13:44:02 +0200

    Adding test for #3459

---
+++ testsuite/test_0266/data/post_script.py
@@ -0,0 +1,12 @@
+import sys
+
+test_root_path = sys.argv[1]
+oiiotool_path  = sys.argv[2]
+mtoa_path = sys.argv[3]
+
+cmd = '{} -i {} -o {} -v 1'.format(os.path.join(mtoa_path, 'bin', 'noice'), os.path.join(test_root_path, 'testrender.exr'), os.path.join(test_root_path, 'denoised.exr'))
+os.system(cmd)
+
+
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "denoised.exr"), os.path.join(test_root_path, "testrender.tif"))
+os.system(cmd)
