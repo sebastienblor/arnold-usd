996fffb19783eb7acfe81efa438c9f9232134c07
sebastien.ortega <sebastien@solidangle.com>
2018-06-04 09:58:39 +0200

    Adding test 267 for noice with separate files

---
+++ testsuite/test_0267/data/post_script.py
@@ -0,0 +1,14 @@
+import sys
+
+test_root_path = sys.argv[1]
+oiiotool_path  = sys.argv[2]
+mtoa_path = sys.argv[3]
+
+cmd = '{} -i {} {} {} {} {} -o {} -v 1'.format(os.path.join(mtoa_path, 'bin', 'noice'), os.path.join(test_root_path, 'original_beauty.exr'), os.path.join(test_root_path, 'original_diffuse_albedo_noice.exr'), os.path.join(test_root_path, 'original_Z_noice.exr'), os.path.join(test_root_path, 'original_N_noice.exr'), os.path.join(test_root_path, 'original_variance.exr'), os.path.join(test_root_path, 'denoised.exr'))
+os.system(cmd)
+
+cmd = ('%s %s -ch "R,G,B,A" -o %s') % (oiiotool_path, os.path.join(test_root_path, "denoised.exr"), os.path.join(test_root_path, "testrender.tif"))
+os.system(cmd)
+
+
+
