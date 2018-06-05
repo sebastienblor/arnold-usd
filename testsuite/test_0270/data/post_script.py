14b6aa1fd28ea3eac3ba37b9d432d0a660eb73f0
sebastien.ortega <sebastien@solidangle.com>
2018-06-05 11:27:29 +0200

    Add test 270 for #2727

---
+++ testsuite/test_0270/data/post_script.py
@@ -0,0 +1,14 @@
+test_root_path = sys.argv[1]
+oiiotool_path  = sys.argv[2]
+mtoa_path = sys.argv[3]
+
+cmd = '{} -dp -dw {}'.format(os.path.join(mtoa_path, 'bin', 'kick'), os.path.join(test_root_path, 'test_camera1.ass'))
+os.system(cmd)
+
+cmd = '{} -dp -dw {}'.format(os.path.join(mtoa_path, 'bin', 'kick'), os.path.join(test_root_path, 'test_camera2.ass'))
+os.system(cmd)
+
+
+
+
+
