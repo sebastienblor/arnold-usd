ad93872a5736e7ff7073743dc9855fa1ab397d4a
sebastien.ortega <sebastien@solidangle.com>
2018-06-12 11:10:46 +0200

    Updating test 223 after maketx change

--- testsuite/test_0223/data/test.py
+++ testsuite/test_0223/data/test.py
@@ -9,15 +9,17 @@ mtoa_root_path    = sys.argv[2]
 makeTxPath        = os.path.join(mtoa_root_path, 'bin', 'maketx')
 synColor_path     = os.path.join(maya_root_path, 'synColor')
 
+''' We no longer need that, since the syncolor color_manager node points directly to the installed syncolor folder
 # Create a syncolorConfig file with the path to maya syncolor native catalogs
 synColorConfig = '<?xml version="1.0" encoding="UTF-8"?> <SynColorConfig version="2.0"> <AutoConfigure graphicsMonitor="false" /> <TransformsHome dir="%s" /> <SharedHome dir="." /> <ReferenceTable> <Ref alias="OutputToSceneBridge" path="misc/identity.ctf" basePath="Autodesk" /> <Ref alias="SceneToOutputBridge" path="RRT+ODT/ACES_to_CIE-XYZ_v0.1.1.ctf" basePath="Autodesk" /> <Ref alias="broadcastMonitor" path="display/broadcast/CIE-XYZ_to_HD-video.ctf" basePath="Autodesk" /> <Ref alias="defaultLook" path="misc/identity.ctf" basePath="Autodesk" /> <Ref alias="graphicsMonitor" path="interchange/sRGB/CIE-XYZ_to_sRGB.ctf" basePath="Autodesk" /> </ReferenceTable> </SynColorConfig>' \
   % (synColor_path)
 
 with open('synColorConfig.xml','w') as f:
     f.write(synColorConfig)
+'''
 
-cmd = '%s --nomipmap --colorengine "syncolor" --colorconfig synColorConfig.xml --colorconvert "Sony SLog2" "ACEScg" texture.jpg -o testrender.tif' \
-    % (makeTxPath)
+cmd = '%s --nomipmap --colorengine "syncolor" --colorconfig "%s" --colorconvert "Sony SLog2" "ACEScg" texture.jpg -o testrender.tif' \
+    % (makeTxPath, synColor_path)
 
 print cmd
 
