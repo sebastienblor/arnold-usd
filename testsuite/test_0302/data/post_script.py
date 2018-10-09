f768744fd8b0959138da167e19329bf0044f6fa7
sebastien.ortega <sebastien@solidangle.com>
2018-10-09 21:10:02 +0200

    Adding test_302 for #3612

---
+++ testsuite/test_0302/data/post_script.py
@@ -0,0 +1,18 @@
+import sys
+
+oiiotool_path = sys.argv[2]
+
+#cmd = ('%s -v --info %s') % (oiiotool_path, 'testrender_diffuse_direct.exr')
+#print cmd
+#os.system(cmd)
+print 'Extracting channels from multi-layer images'
+cmd = ('%s %s -ch "%s,%s,%s" -o %s') % (oiiotool_path, 'testrender.exr', 
+	'R', 'G', 'B', 'testrender_beauty.tif')
+os.system(cmd)
+cmd = ('%s %s -ch "%s,%s,%s" -o %s') % (oiiotool_path, 'testrender.exr', 
+	'N.X', 'N.Y', 'N.Z', 'testrender_n.tif')
+os.system(cmd)
+
+cmd = ('%s %s -ch "%s,%s,%s" -o %s') % (oiiotool_path, 'testrender.exr', 
+	'N_1.X', 'N_1.Y', 'N_1.Z', 'testrender_n_1.tif')
+os.system(cmd)
