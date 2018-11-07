sebastienblor/arnold-usd

commit 6246d71ababbbb8b4b31b7596f04ccd4e17e3b5a

One more piece of code when correct master instance needs to be considered (#3633)

plugins/mtoa/session/ArnoldSession.cpp
@@ -1813,26 +1813,32 @@ UPDATE_BEGIN:
                MDagPath shapePath = path;
                shapePath.extendToShape();
 
-               int instanceNum = shapePath.instanceNumber();
-
-               if (instanceNum > 0)
+               if (shapePath.isInstanced())
                {
-                  dagTr = ExportDagPath(shapePath);
-                  if (dagTr)
-                  {
-                     name = shapePath.partialPathName();
-                     if (mtoa_translation_info)
-                        MtoaDebugLog("[mtoa] Exported new node: "+ name);
+                  MDagPath srcParent;
+                  MFnDagNode(MFnDagNode(shapePath.node()).parent(0)).getPath(srcParent);
+                  srcParent.push(dagPath.node());
 
-                     newDag = true;
-                     
-                     translatorsToUpdate.push_back(dagTr);
-                     if (motionBlur && (!(exportMotion && mbRequiresFrameChange)) && dagTr->RequiresMotionData())
+                  // If this isn't the master instance
+                  if (srcParent != shapePath)
+                  {
+                     dagTr = ExportDagPath(shapePath);
+                     if (dagTr)
                      {
-                        // Find out if we need to call ExportMotion for each motion step
-                        // or if a single Export is enough. 
-                        exportMotion = true;
-                        mbRequiresFrameChange = true;
+                        name = shapePath.partialPathName();
+                        if (mtoa_translation_info)
+                           MtoaDebugLog("[mtoa] Exported new node: "+ name);
+
+                        newDag = true;
+                        
+                        translatorsToUpdate.push_back(dagTr);
+                        if (motionBlur && (!(exportMotion && mbRequiresFrameChange)) && dagTr->RequiresMotionData())
+                        {
+                           // Find out if we need to call ExportMotion for each motion step
+                           // or if a single Export is enough. 
+                           exportMotion = true;
+                           mbRequiresFrameChange = true;
+                        }
                      }
                   }
                }

commit 5bee115dd6e84179a0057b5ed5859a0b2b142b09

Fix build issue introduced in previous commit (#3633)

plugins/mtoa/session/ArnoldSession.cpp
@@ -1817,10 +1817,10 @@ UPDATE_BEGIN:
                {
                   MDagPath srcParent;
                   MFnDagNode(MFnDagNode(shapePath.node()).parent(0)).getPath(srcParent);
-                  srcParent.push(dagPath.node());
+                  srcParent.push(shapePath.node());
 
                   // If this isn't the master instance
-                  if (srcParent != shapePath)
+                  if (!(srcParent == shapePath))
                   {
                      dagTr = ExportDagPath(shapePath);
                      if (dagTr)