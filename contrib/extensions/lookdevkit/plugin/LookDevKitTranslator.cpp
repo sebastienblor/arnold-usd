04beb78566e13a9e8813b7d155e8917220f1f13e
sebastien.ortega <sebastien@solidangle.com>
2019-03-05 09:22:30 +0100

    Convert ldkColorCorrect

--- contrib/extensions/lookdevkit/plugin/LookDevKitTranslator.cpp
+++ contrib/extensions/lookdevkit/plugin/LookDevKitTranslator.cpp
@@ -2,7 +2,7 @@
 #include "../shaders/utils.h"
 #include "extension/Extension.h"
 #include <cassert>
-
+#include "utils/MayaUtils.h"
 #include <maya/MFnNumericData.h>
 
 void CLookDevKitTranslator::ExportRGBAChannels(AtNode *shader, const char *arnoldParam, const char *rgbParam, const char *alphaParam)
@@ -70,6 +70,30 @@ AtNode* CLookDevKitTranslator::CreateArnoldNodes()
       nodeType = MString("rgb_to_float");
    else if (nodeType == MString("floatMath"))
       nodeType = MString("rgb_to_float");
+   else if (nodeType == MString("colorCorrect"))
+   {      
+      AtNode *cc = AddArnoldNode("color_correct");
+      // FIXME : also alphaGamma, alphaClamp
+      AtNode *cc2 = (IsFloatAttrDefault(FindMayaPlug("alphaGain"), 1.f) && IsFloatAttrDefault(FindMayaPlug("alphaOffset"), 0.f)) ? 
+                        NULL : AddArnoldNode("color_correct", "cc2");
+      AtNode *clamp = (IsBoolAttrDefault(FindMayaPlug("colClamp"), false)) ? NULL : AddArnoldNode("clamp", "clamp");
+      AtNode *gamma = (IsRGBAttrDefault(FindMayaPlug("colGamma"), 1.f, 1.f, 1.f)) ? NULL : AddArnoldNode("pow", "gamma");
+      AtNode *premult = (IsBoolAttrDefault(FindMayaPlug("premultiplyResult"), false)) ? NULL : AddArnoldNode("divide", "premult");
+      AtNode *shuffleAlpha = (premult || (IsFloatAttrDefault(FindMayaPlug("alphaGamma"), 1.f) && 
+                                         IsBoolAttrDefault(FindMayaPlug("alphaClamp"), false))) ? NULL : AddArnoldNode("shuffle", "shuffleA");
+      if (premult)
+         return premult;
+      if (shuffleAlpha)
+         return shuffleAlpha;
+      if (cc2)
+         return cc2;
+      if (clamp)
+         return clamp;
+      if (gamma)
+         return gamma;
+
+      return cc;
+   }
    else
    {
 	   MString prefix = nodeType.substringW(0, 0);
@@ -443,8 +467,206 @@ void CLookDevKitTranslator::Export(AtNode* shader)
       ProcessParameter(subtract, "input2.r", AI_TYPE_FLOAT, "mask");
       AiNodeLink(subtract, "input", shader);
       AiNodeSetStr(shader, "mode", "r");
+   } else if (nodeType == MString("colorCorrect"))
+   {
+      AtNode *cc = shader;
+      AtNode *cc2 = GetArnoldNode("cc2");
+      AtNode *clamp = GetArnoldNode("clamp");
+      AtNode *gamma = GetArnoldNode("gamma");
+      AtNode *premult = GetArnoldNode("premult");
+      AtNode *shuffleA = GetArnoldNode("shuffleA");
+      AtNode *upstream = cc;
+      AtNode *unpremultInput = NULL;
+      AtNode *shuffle = NULL;
+      // unpremultiplyInput
+      AiNodeUnlink(cc, "input");
+      ProcessParameter(cc, "input", AI_TYPE_RGBA, "inColor");
+      
+      // this is the node to be used for the alpha
+      AtNode *colorSource = AiNodeGetLink(cc, "input");
+      AtNode *alphaSource = colorSource;
+
+      MPlugArray connections;
+      MPlug alphaPlug = FindMayaPlug("inAlpha");
+      alphaPlug.connectedTo(connections, true, false);
+      // override the node used for the alpha
+      if (connections.length() > 0)
+         alphaSource = ExportConnectedNode(connections[0]);
+      
+      if (alphaSource && !IsBoolAttrDefault(FindMayaPlug("unpremultiplyInput"), false))
+      {
+         unpremultInput = GetArnoldNode("unpremult");
+         if (unpremultInput == NULL)
+            unpremultInput = AddArnoldNode("multiply", "unpremult");
+
+         ProcessParameter(unpremultInput, "input1", AI_TYPE_RGB, "inColor");
+         // here I need to know what is my alpha !!!
+         // in order to link it
+         if (alphaSource != colorSource)
+         {
+            AiNodeLink(alphaSource, "input2.r", unpremultInput);
+            AiNodeLink(alphaSource, "input2.g", unpremultInput);
+            AiNodeLink(alphaSource, "input2.b", unpremultInput);
+         } else
+         {
+            AiNodeLinkOutput(alphaSource, "a", unpremultInput, "input2.r");
+            AiNodeLinkOutput(alphaSource, "a", unpremultInput, "input2.g");
+            AiNodeLinkOutput(alphaSource, "a", unpremultInput, "input2.b");
+         }
+      } 
+
+      if (alphaSource && (alphaSource != colorSource))
+      {
+         // alpha comes from a different node, need to combine this
+         AtNode *shuffle = GetArnoldNode("shuffle");
+         if (shuffle == NULL)
+            shuffle = AddArnoldNode("shuffle", "shuffle");
+
+         AiNodeLink(shuffle, "input", cc);
+         if (unpremultInput)
+            AiNodeLink(unpremultInput, "color", shuffle);
+         else
+            ProcessParameter(shuffle, "color", AI_TYPE_RGB, "inColor");
+         ProcessParameter(shuffle, "alpha", AI_TYPE_FLOAT, "inAlpha");
+      } else if (unpremultInput)
+         AiNodeLink(unpremultInput, "input", cc);
+      
+         
+      MPlug huePlug = FindMayaPlug("hueShift");
+      connections.clear();
+      huePlug.connectedTo(connections, true, false);
+      if (connections.length() > 0)
+      {
+         AtNode* linkedNode = ExportConnectedNode(connections[0]);
+         AtNode* multiply = GetArnoldNode("mult_hue");
+         if (multiply == NULL)
+            multiply = AddArnoldNode("multiply", "mult_hue");
+
+         AiNodeLink(linkedNode, "input1", multiply); 
+         float hueFactor = 1.f / 360.f;
+         AiNodeSetRGB(multiply, "input2", hueFactor, hueFactor, hueFactor);  
+         AiNodeLink(multiply, "hue_shift", cc);
+         
+      } else
+      {
+         AiNodeUnlink(cc, "hue_shift");
+         AiNodeSetFlt(cc, "hue_shift", huePlug.asFloat() / 360.f);
+      }
+      ProcessParameter(cc, "saturation", AI_TYPE_FLOAT, "satGain");
+      AiNodeSetFlt(cc, "contrast_pivot", 0.f);
+      ProcessParameter(cc, "contrast", AI_TYPE_FLOAT, "valGain");
+      ProcessParameter(cc, "multiply", AI_TYPE_RGB, "colGain");
+      ProcessParameter(cc, "add", AI_TYPE_RGB, "colOffset");
+      
+      if (gamma)
+      {
+         AiNodeLink(cc, "base", gamma);
+         MPlug plug = FindMayaPlug("colGamma");
+         MPlugArray connections;
+         plug.connectedTo(connections, true, false);
+         if (connections.length() > 0)
+         {
+            AtNode* linkedNode = ExportConnectedNode(connections[0]);
+            // there is a link on the gamma attribute. We need to insert an invert shader in the middle
+            AtNode* invert = GetArnoldNode("invert");
+            if (invert == NULL)
+               invert = AddArnoldNode("reciprocal", "invert");
+
+            AiNodeLink(linkedNode, "input", invert);   
+            AiNodeLink(invert, "exponent", gamma);
+            
+         } else
+         {
+            // no connection on the gamma attribute, just set the exponent as 1/gamma
+            AiNodeSetRGB(gamma, "exponent", 1.f / AiMax(AI_EPSILON, plug.child(0).asFloat()), 
+                                             1.f / AiMax(AI_EPSILON, plug.child(1).asFloat()), 
+                                             1.f / AiMax(AI_EPSILON, plug.child(2).asFloat()));
+         }
+         upstream = gamma;         
+      }
+      if (clamp && FindMayaPlug("colClamp").asBool())
+      {
+         AiNodeLink(upstream, "input", clamp);
+         AiNodeSetStr(clamp, "mode", "color");
+         ProcessParameter(clamp, "min_color", AI_TYPE_RGB, "colClampMin");
+         ProcessParameter(clamp, "max_color", AI_TYPE_RGB, "colClampMax");
+         upstream = clamp;
+      }
+      if (cc2)
+      {
+         AiNodeLink(upstream, "input", cc2);
+         ProcessParameter(cc2, "alpha_multiply", AI_TYPE_FLOAT, "alphaGain");
+         ProcessParameter(cc2, "alpha_add", AI_TYPE_FLOAT, "alphaOffset");
+         upstream = cc2;
+      }
+      AtNode *alphaUpstream = upstream;
+      if (!IsFloatAttrDefault(FindMayaPlug("alphaGamma"), 1.f))
+      {
+         AtNode *alphaGamma = GetArnoldNode("gammaA");
+         if (alphaGamma == NULL)
+            alphaGamma = AddArnoldNode("color_correct", "gammaA");
+         AiNodeLinkOutput(upstream, "a", alphaGamma, "input");
+         ProcessParameter(alphaGamma, "gamma", AI_TYPE_FLOAT, "alphaGamma");
+         alphaUpstream = alphaGamma;
+      }
+      if (!IsBoolAttrDefault(FindMayaPlug("alphaClamp"), false))
+      {
+         AtNode *alphaClamp = GetArnoldNode("clampA");
+         if (alphaClamp == NULL)
+            alphaClamp = AddArnoldNode("clamp", "clampA");
+         if(alphaUpstream == upstream)
+            AiNodeLinkOutput(alphaUpstream, "a", alphaClamp, "input");
+         else
+            AiNodeLink(alphaUpstream, "input", alphaClamp);
+            
+         ProcessParameter(alphaClamp, "min", AI_TYPE_FLOAT, "alphaClampMin");
+         ProcessParameter(alphaClamp, "max", AI_TYPE_FLOAT, "alphaClampMax");
+         alphaUpstream = alphaClamp;
+      }
+
+      if (premult)
+      {
+         AiNodeLink(upstream, "input1", premult);
+
+         if (alphaUpstream == upstream)
+         {
+            AiNodeLinkOutput(alphaUpstream, "a", premult, "input2.r");
+            AiNodeLinkOutput(alphaUpstream, "a", premult, "input2.g");
+            AiNodeLinkOutput(alphaUpstream, "a", premult, "input2.b");
+         } else
+            AiNodeLink(alphaUpstream, "input2", premult);
+
+      } else if (shuffleA)
+      {
+         AiNodeLink(upstream, "color", shuffleA);
+         if (alphaUpstream == upstream)
+            AiNodeLinkOutput(alphaUpstream, "a", shuffleA, "alpha");
+         else
+            AiNodeLink(alphaUpstream, "alpha", shuffleA);
+      }
    } else // default behaviour
       CNodeTranslator::Export(shader);
 }
 
 
+void CLookDevKitTranslator::NodeChanged(MObject& node, MPlug& plug)
+{
+   MString nodeType = MFnDependencyNode(GetMayaObject()).typeName();
+   if (nodeType == MString("colorCorrect"))
+   {
+      MString plugName = plug.partialName(false, false, false, false, false, true);
+      if ((plugName == "alphaGamma" || plugName == "alphaClamp") && GetArnoldNode("shuffleA") == NULL && GetArnoldNode("premult") == NULL)
+         SetUpdateMode(AI_RECREATE_NODE);
+      else if ((plugName == "alphaGain"||plugName == "alphaOffset") && GetArnoldNode("cc2") == NULL)
+         SetUpdateMode(AI_RECREATE_NODE);
+      else if (plugName ==  "colClamp" && GetArnoldNode("clamp") == NULL)
+         SetUpdateMode(AI_RECREATE_NODE);
+      else if (plugName ==  "colGamma" && GetArnoldNode("gamma") == NULL)
+         SetUpdateMode(AI_RECREATE_NODE);
+      else if (plugName ==  "premultiplyResult" && GetArnoldNode("premult") == NULL)
+         SetUpdateMode(AI_RECREATE_NODE);
+      else if ((plugName ==  "alphaGamma" || plugName == "alphaClamp") && GetArnoldNode("premult") == NULL)
+         SetUpdateMode(AI_RECREATE_NODE);
+   }
+   CShaderTranslator::NodeChanged(node, plug);
+}
