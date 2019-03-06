ce4e56915426bd07589956f5a128f9d9fa4a01da
sebastien.ortega <sebastien@solidangle.com>
2019-03-06 11:17:20 +0100

    Deprecating XGenHairMapping

--- contrib/extensions/xgenSpline/plugin/XGenHairMappingTranslator.cpp
+++ contrib/extensions/xgenSpline/plugin/XGenHairMappingTranslator.cpp
@@ -3,13 +3,34 @@
 
 AtNode* CXgHairMappingTranslator::CreateArnoldNodes()
 {
-    return ProcessAOVOutput(AddArnoldNode("XGenHairMapping"));
+   AddArnoldNode("state_float", "bv");
+   return AddArnoldNode("layer_rgba");
 }
 
 void CXgHairMappingTranslator::Export(AtNode* shader)
 {
-    ProcessParameter(shader, "coverage", AI_TYPE_VECTOR2);
-    ProcessParameter(shader, "translateFrame", AI_TYPE_VECTOR2);
-    ProcessParameter(shader, "repeatUV", AI_TYPE_VECTOR2);
-    ProcessParameter(shader, "offsetUV", AI_TYPE_VECTOR2, "offset");
+   AtNode *bv = GetArnoldNode("bv");
+   AiNodeSetStr(bv, "variable", "bv");
+   AiNodeSetRGBA(shader, "input1", 0.f, 0.f, 0.f, 1.f);
+   AiNodeLink(bv, "input1.g", shader);
+
+   MPlug translateFramePlug = FindMayaPlug("translateFrame");
+   AiNodeSetRGBA(shader, "input2", translateFramePlug.child(0).asFloat(), translateFramePlug.child(1).asFloat(), 0.f, 1.f);
+   AiNodeSetStr(shader, "operation2", "minus");
+   AiNodeSetStr(shader, "alpha_operation2", "preserve");
+
+   MPlug coveragePlug = FindMayaPlug("coverage");
+   AiNodeSetRGBA(shader, "input3", coveragePlug.child(0).asFloat(), coveragePlug.child(1).asFloat(), 1.f, 1.f);
+   AiNodeSetStr(shader, "operation3", "divide");
+   AiNodeSetStr(shader, "alpha_operation3", "preserve");
+
+   MPlug repeatUVPlug = FindMayaPlug("repeatUV");
+   AiNodeSetRGBA(shader, "input4", repeatUVPlug.child(0).asFloat(), repeatUVPlug.child(1).asFloat(), 1.f, 1.f);
+   AiNodeSetStr(shader, "operation4", "multiply");
+   AiNodeSetStr(shader, "alpha_operation4", "preserve");
+
+   MPlug offsetUVPlug = FindMayaPlug("offset");
+   AiNodeSetRGBA(shader, "input5", offsetUVPlug.child(0).asFloat(), offsetUVPlug.child(1).asFloat(), 0.f, 1.f);
+   AiNodeSetStr(shader, "operation5", "plus");
+   AiNodeSetStr(shader, "alpha_operation5", "preserve");
 }
