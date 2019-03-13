1677a8457ba6302b90550c607713e007ecdac11f
sebastien.ortega <sebastien@solidangle.com>
2019-03-13 14:16:30 +0100

    Exposing more parameters in camera projection AE

--- scripts/mtoa/ui/ae/aiCameraProjectionTemplate.py
+++ scripts/mtoa/ui/ae/aiCameraProjectionTemplate.py
@@ -2,6 +2,7 @@ import maya.mel
 from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
 
 class AEaiCameraProjectionTemplate(ShaderAETemplate):
+
     def setup(self):
         self.beginScrollLayout()
         self.addControl('projectionColor')
@@ -11,6 +12,9 @@ class AEaiCameraProjectionTemplate(ShaderAETemplate):
         self.addControl('frontFacing')
         self.addControl('backFacing')
         self.addControl('useShadingNormal')
+        self.addControl('coordSpace')
+        self.addControl('prefName')
+        self.addControl('P')
         self.endLayout()
 
         maya.mel.eval('AEdependNodeTemplate '+self.nodeName)
