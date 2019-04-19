80090163ee772470efdc158235f29fff5c937428
Mike Farnsworth <mike@solidangle.com>
2019-04-19 14:47:38 -0600

    Hack data out of BifData, and pass serialized and pointer bifShapes along (mtoa#3780)

--- contrib/extensions/bifrostGraph/plugin/BifShapeTranslator.cpp
+++ contrib/extensions/bifrostGraph/plugin/BifShapeTranslator.cpp
@@ -1,6 +1,7 @@
+#include <sstream>
+
 #include "BifShapeTranslator.h"
 
-#include <bifData.h>
 #include <maya/MFnDependencyNode.h>
 #include <maya/MFnDagNode.h>
 #include <maya/MProfiler.h>
@@ -8,6 +9,79 @@
 #include <maya/MPlug.h>
 #include <maya/MDataHandle.h>
 #include <maya/MFnPluginData.h>
+
+#define BIFDATA_HACK 1
+
+
+/*
+ * We reproduce this class because there is no other way to get to the Amino
+ * array of objects currently.  When an MPlug is provided on the bifShape, we
+ * will switch to using that instead to get serialized data to hand over to
+ * arnold_bifrost.
+ *
+ * This must match the layout of the real BifData in maya-bifrostboard-plugin.
+ */
+#ifdef BIFDATA_HACK
+
+#include <maya/MPxGeometryData.h>
+#include <maya/MTypeId.h>
+#include <maya/MString.h>
+
+#include <bifrost_object/bifrost_object_i.h>
+#include <geometry/bifrost_serialization.h>
+
+#include <stdOpLib/Cpp/AminoArray.h>
+
+
+const MTypeId BifDataId(0x00081122);
+const MString BifDataTypeName( "bifData" );
+
+class BifData : public MPxData
+{
+public:
+    enum class BifDataModel { NotSet, BifrostExp, };
+
+	//////////////////////////////////////////////////////////////////
+	//
+	// Overrides from MPxData
+	//
+	//////////////////////////////////////////////////////////////////
+
+	BifData() : fDataModel(BifDataModel::NotSet) { }
+	~BifData() override { }
+
+	MStatus			readASCII( const MArgList& argList, unsigned& idx ) override { return MS::kSuccess; }
+	MStatus			readBinary( istream& in, unsigned length ) override { return MS::kSuccess; }
+	MStatus			writeASCII( ostream& out ) override { return MS::kSuccess; }
+	MStatus			writeBinary( ostream& out ) override { return MS::kSuccess; }
+
+	MTypeId         typeId() const override { return BifDataId; }
+	MString         name() const override { return BifDataTypeName; }
+
+	bool empty() const 
+    { 
+        switch (fDataModel)
+        {
+        case BifDataModel::BifrostExp:
+            return fBifrostExpObjects.empty();
+        default:
+            return true;
+        }
+    }
+
+    Amino::Array<Amino::Ptr<const BifrostExp::Object> >& bifrostObjects()
+    {
+        fDataModel = BifDataModel::BifrostExp;
+        return fBifrostExpObjects;
+    }
+
+private:
+    BifDataModel fDataModel;
+    Amino::Array<Amino::Ptr<const BifrostExp::Object> > fBifrostExpObjects;
+};
+
+#endif // BIFDATA_HACK
+
 
 static bool s_bifrostBoardSupported = false;
 
@@ -24,6 +98,11 @@ void CBifShapeTranslator::NodeInitializer(CAbTranslator context)
    // bifrost_board procedural parameters
    helper.MakeInput("filename");
    helper.MakeInput("compound");
+   CAttrData fastBifHackData;
+   fastBifHackData.defaultValue.BOOL() = false;
+   fastBifHackData.name = "aiFastBifHack";
+   fastBifHackData.shortName = "aiFastBifHack";
+   helper.MakeInputBoolean(fastBifHackData);
 
    // procedural namespace parameter
    helper.MakeInput("namespace");
@@ -46,39 +125,82 @@ AtNode* CBifShapeTranslator::CreateArnoldNodes()
 
 void CBifShapeTranslator::Export( AtNode *shape )
 {
-
-   // // test code taken from bifSubSCeneOverride.h in maya-bifrostboard-plugin
-   // MFnDagNode dagNode(m_dagPath.node()); // get the bifShape node
-   // MPlug viewport = FindMayaPlug("viewport");
-   // MDataHandle handle;
-   // plug.getValue(handle);
-   // MFnPluginData fnData(handle.data());
-   // BifData* data = static_cast<BifData*>(fnData.data(&status));
-   // if (data)
-   //   {
-   //       if (!data->empty()) // only one type of data (BifrostExp::Object) so if not empty it must be it!
-   //       {
-   //           unsigned int index = 1;
-   //           for (auto iter = data->bifrostObjects().begin(); iter != data->bifrostObjects().end(); ++iter, ++index)
-   //           {
-   //               Amino::Ptr< const BifrostExp::Object > bifrostObj = *iter;
-   //           }
-   //       }
-   //    }
-
-   // export BifShape parameters
-   MPlug filenamePlug = FindMayaPlug("aiFilename");
-   if (!filenamePlug.isNull())
+#ifdef BIFDATA_HACK
+   MStatus status = MS::kSuccess;
+   MPlug viewport = FindMayaPlug("viewport");
+   MDataHandle handle;
+   viewport.getValue(handle);
+   MFnPluginData fnData(handle.data());
+   BifData* data = static_cast<BifData*>(fnData.data(&status));
+   if (data && !data->empty())
    {
-      MString filename = filenamePlug.asString();
-      AiNodeSetStr(shape, "filename", filename.asChar());
+      bool fastHack = false;
+      MPlug fastHackPlug = FindMayaPlug("aiFastBifHack");
+      if (!fastHackPlug.isNull())
+      {
+         AiMsgWarning("[bifShapeTranslator] %s: using fast hack (passing pointers -- veeerrry dangerous!)", AiNodeGetName(shape));
+         fastHack = fastHackPlug.asBool();
+      }
+      
+      if (fastHack)
+      {
+         // Pass pointers to bifrost objects directly
+         AtArray *inputsArray = AiArrayAllocate(static_cast<uint32_t>(data->bifrostObjects().size()), 1, AI_TYPE_STRING);
+         // We can get away with one entry, because they're all the same and arnold_bifrost figures this out
+         AtArray *interpsArray = AiArray(1, 1, AI_TYPE_STRING, "pointer");
+         uint32_t i = 0;
+         for (auto iter = data->bifrostObjects().begin(); iter != data->bifrostObjects().end(); ++iter, ++i)
+         {
+            std::ostringstream inputNamer;
+            inputNamer << "input" << i;
+            inputNamer.flush();
+            std::string inputName = inputNamer.str();
+            std::string bifrostInputName = std::string("bifrost:") + inputName;
+            AiNodeDeclare(shape, bifrostInputName.c_str(), "constant POINTER");
+            // INTENTIONAL LEAK: Dangle a reference to the object and keep it around, otherwise we crash!
+            Amino::Ptr<BifrostExp::Object> *mutObj = new Amino::Ptr<BifrostExp::Object>(data->bifrostObjects()[i].toMutable());
+            AiNodeSetPtr(shape, bifrostInputName.c_str(), mutObj->get());
+            AiArraySetStr(inputsArray, i, AtString(inputName.c_str()));
+         }
+         AiNodeSetArray(shape, "input_names", inputsArray);
+         AiNodeSetArray(shape, "input_interpretations", interpsArray);
+      }
+      else
+      {
+         // Serialize to disk, tell the procedural about it
+         std::string filename = "/tmp/bifrost-";
+         filename += AiNodeGetName(shape);
+         filename += ".bob";
+         if (!BifrostExp::IO::serialize(data->bifrostObjects(), Amino::String(filename.c_str())))
+         {
+            AiMsgError("[bifShapeTranslator] Could not serialized bifrost objects to %s", filename.c_str());
+            return;
+         }
+         AtArray *inputsArray = AiArray(1, 1, AI_TYPE_STRING, "input0");
+         AtArray *interpsArray = AiArray(1, 1, AI_TYPE_STRING, "file");
+         AiNodeDeclare(shape, "bifrost:input0", "constant STRING");
+         AiNodeSetStr(shape, "bifrost:input0", filename.c_str());
+         AiNodeSetArray(shape, "input_names", inputsArray);
+         AiNodeSetArray(shape, "input_interpretations", interpsArray);
+      }
    }
-
-   MPlug geomPlug = FindMayaPlug("aiCompound");
-   if (!geomPlug.isNull())
+   else
+#endif // BIFDATA_HACK
    {
-      MString geomPath = geomPlug.asString();
-      AiNodeSetStr(shape, "compound", geomPath.asChar());
+      // export BifShape parameters
+      MPlug filenamePlug = FindMayaPlug("aiFilename");
+      if (!filenamePlug.isNull())
+      {
+         MString filename = filenamePlug.asString();
+         AiNodeSetStr(shape, "filename", filename.asChar());
+      }
+
+      MPlug geomPlug = FindMayaPlug("aiCompound");
+      if (!geomPlug.isNull())
+      {
+         MString geomPath = geomPlug.asString();
+         AiNodeSetStr(shape, "compound", geomPath.asChar());
+      }
    }
 
    if (RequiresShaderExport())
