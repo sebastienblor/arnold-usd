99844975bab8d82c6e1848ab0a0f231231fbae74
sebastien.ortega <sebastien@solidangle.com>
2019-10-17 15:35:45 +0200

    No longer recreate a custom procedural when the operators input changes

--- plugins/mtoa/translators/shape/ProceduralTranslator.cpp
+++ plugins/mtoa/translators/shape/ProceduralTranslator.cpp
@@ -279,8 +279,9 @@ void CProceduralTranslator::NodeChanged(MObject& node, MPlug& plug)
       plugName == "MinBoundingBox0" || plugName == "MinBoundingBox1" || plugName == "MinBoundingBox2" || 
       plugName == "MaxBoundingBox0" || plugName == "MaxBoundingBox1" || plugName == "MaxBoundingBox2") return;
 
+   // do *not* recreate the node if it's an operators input
    MGlobal::displayWarning(plugName.substringW(0, 9));
-   if (!(IsTransformPlug(plug) || (plugName.length() > 10 && plugName.substringW(0, 9) == MString("operators["))))
+   if (!(IsTransformPlug(plug) && (plugName.length() <= 10 || plugName.substringW(0, 9) != MString("operators["))))
       SetUpdateMode(AI_RECREATE_NODE);
    
    CShapeTranslator::NodeChanged(node, plug);
