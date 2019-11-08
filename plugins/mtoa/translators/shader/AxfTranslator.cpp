e70e6eb575a4deaddaa36b2af2bb8e9bc02889fe
Krishnan Ramachandran <krishnan.chunangad.ramachandran@autodesk.com>
2019-11-08 15:22:01 +0000

    [FB-4036] : Lambert is a more inexpensive default

--- plugins/mtoa/translators/shader/AxfTranslator.cpp
+++ plugins/mtoa/translators/shader/AxfTranslator.cpp
@@ -13,13 +13,13 @@ AtNode* CArnoldAxfShaderTranslator::CreateArnoldNodes()
    if (axf_path.length()==0 )
    {
       AiMsgWarning("[mtoa] [translator %s] No Axf File Path Provided to node : %s", GetTranslatorName().asChar(), maya_node_name.asChar());
-      return AddArnoldNode("standard_surface");
+      return AddArnoldNode("lambert");
    }
    
    if (tex_path.length()==0)
    {
       AiMsgError("[mtoa] [translator %s] No texture path provided to Node : %s", GetTranslatorName().asChar(), maya_node_name.asChar());
-      return AddArnoldNode("standard_surface");
+      return AddArnoldNode("lambert");
    }
 
    AxFtoASessionStart();
