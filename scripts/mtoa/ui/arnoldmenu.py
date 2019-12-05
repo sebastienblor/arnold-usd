fce4e8363bdef778100e03fd87b49e6461025783
Ashley Handscomb Retallack <ashley@solidangle.com>
2019-12-05 12:07:12 +0000

    updated URLs for Licensing Help and Purchase Subscription

--- scripts/mtoa/ui/arnoldmenu.py
+++ scripts/mtoa/ui/arnoldmenu.py
@@ -454,9 +454,9 @@ def createArnoldMenu():
         cmds.menuItem('ArnoldLicenseManager', label='License Manager', parent='ArnoldLicensingMenu',
                     c=lambda *args: arnoldLicenseManager())
         cmds.menuItem('ArnoldLicensingHelp',  label='Licensing Help', parent='ArnoldLicensingMenu', 
-                    c=lambda *args: cmds.launch(webPage='https://docs.arnoldrenderer.com/display/A5AILIC/Licensing+Home'))
+                    c=lambda *args: cmds.launch(webPage='https://docs.arnoldrenderer.com/x/TYcfBg'))
         cmds.menuItem('ArnoldSuscribe',  label='Purchase Subscription', parent='ArnoldLicensingMenu', 
-                    c=lambda *args: cmds.launch(webPage='https://www.solidangle.com/arnold/buy/'))
+                    c=lambda *args: cmds.launch(webPage='https://www.autodesk.com/products/arnold/subscribe/'))
         
         '''
         cmds.menuItem('ArnoldConnectLicenseServer', label='Connect to License Server', parent='ArnoldLicensingMenu',
