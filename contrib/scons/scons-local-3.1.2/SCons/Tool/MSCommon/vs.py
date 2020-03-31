bc771c6a5469b18f57bd327bb8169ed6876110a5
sebastien.ortega <sebastien@solidangle.com>
2020-03-31 11:54:08 +0200

    Fix in scons 3.1.2 scripts

--- contrib/scons/scons-local-3.1.2/SCons/Tool/MSCommon/vs.py
+++ contrib/scons/scons-local-3.1.2/SCons/Tool/MSCommon/vs.py
@@ -199,17 +199,6 @@ class VisualStudio(object):
 
 SupportedVSList = [
     # Visual Studio 2019
-    VisualStudio('15.9',
-                 vc_version='15.9',
-                 sdk_version='10.0A',
-                 hkeys=[],
-                 common_tools_var='VS160COMNTOOLS',
-                 executable_path=r'Common7\IDE\devenv.com',
-                 batch_file_path=r'VC\Auxiliary\Build\vsvars32.bat',
-                 supported_arch=['x86', 'amd64', "arm"],
-                 ),
-
-    # Visual Studio 2019
     VisualStudio('14.2',
                  vc_version='14.2',
                  sdk_version='10.0A',
