5e0c4a5c66166834bd4d0c92fa7e64e655bc2992
Krishnan Chunangad Ramachandran <krishnan.chunangad.ramachandran@autodesk.com>
2020-04-22 19:33:52 +0100

    Fixing intend

--- tools/python/utils/mtoa_build_tools.py
+++ tools/python/utils/mtoa_build_tools.py
@@ -76,7 +76,7 @@ def make_module(env, target, source):
       os.makedirs(os.path.dirname(source[0]))
    f = open(source[0], 'w' )
    # Maya got problems with double digit versions
-    f.write('+ mtoa %s %s\n' % (get_mtoa_version(3), target[0]))
+   f.write('+ mtoa %s %s\n' % (get_mtoa_version(3), target[0]))
    #f.write('+ mtoa %s %s\n' % ('any', target[0]))
    f.write('PATH +:= bin\n')
    f.write('MAYA_CUSTOM_TEMPLATE_PATH +:= scripts/mtoa/ui/templates\n')
