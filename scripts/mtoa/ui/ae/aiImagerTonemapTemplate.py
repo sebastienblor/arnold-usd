4f5caaadfea8a693c28aef265add975c17b8ba1a
Sebastien Blaineau-Ortega <sebastien.blor@gmail.com>
2020-09-29 18:36:09 +0200

    Update the imager nodes ui

--- scripts/mtoa/ui/ae/aiImagerTonemapTemplate.py
+++ scripts/mtoa/ui/ae/aiImagerTonemapTemplate.py
@@ -1,16 +1,28 @@
+import maya.cmds as cmds
 import maya.mel
 from mtoa.ui.ae.templates import TranslatorControl
 from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
 
 class AEaiImagerTonemapTemplate(ShaderAETemplate):
 
+    def updateParamsVisibility(self, nodeName):
+        modeAttr = '%s.%s' % (nodeName, 'mode')
+        modeValue = cmds.getAttr(modeAttr)
+        cmds.editorTemplate(dimControl=(nodeName, 'filmicToeStrength', modeValue != 0))
+        cmds.editorTemplate(dimControl=(nodeName, 'filmicToeLength', modeValue != 0))
+        cmds.editorTemplate(dimControl=(nodeName, 'filmicShoulderStrength', modeValue != 0))
+        cmds.editorTemplate(dimControl=(nodeName, 'filmicShoulderLength', modeValue != 0))
+        cmds.editorTemplate(dimControl=(nodeName, 'filmicShoulderAngle', modeValue != 0))
+        cmds.editorTemplate(dimControl=(nodeName, 'reinhardHighlights', modeValue != 1))
+        cmds.editorTemplate(dimControl=(nodeName, 'reinhardShadows', modeValue != 1))
+    
     def setup(self):
         #mel.eval('AEswatchDisplay "%s"' % nodeName)
 
         self.beginScrollLayout()
         self.addControl('enable', label='Enable')
         self.addSeparator()
-        self.addControl('mode', label='Mode')
+        self.addControl('mode', label='Mode', changeCommand=self.updateParamsVisibility)
         self.beginLayout("Filmic", collapse=False)
         self.addControl('filmicToeStrength', label='Toe Strength')
         self.addControl('filmicToeLength', label='Toe Length')
