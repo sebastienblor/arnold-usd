b11a01daab53690e81805c5f19b942290a88d491
Ashley Handscomb Retallack <ashley@solidangle.com>
2020-11-20 12:59:02 +0000

    #4394 updated imagerLensEffects to use the ImagerUI class directly

--- scripts/mtoa/ui/ae/aiImagerLensEffectsTemplate.py
+++ scripts/mtoa/ui/ae/aiImagerLensEffectsTemplate.py
@@ -4,32 +4,25 @@ from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
 from mtoa.ui.ae.aiImagersBaseTemplate import ImagerBaseUI
 import maya.cmds as cmds
 
+
 class AEaiImagerLensEffectsTemplate(ShaderAETemplate):
-
-
-    def createNewLensEffectUI(self,nodeAttr):
-        currentWidget = cmds.setParent(query=True)
-        self.ui = ImagerLensEffectUI(parent = currentWidget,nodeName = self.nodeName)
-   
-
-    def replaceLensEffectUI(self,nodeAttr):
-        currentWidget = cmds.setParent(query=True)
-        if self.ui:
-            for item in cmds.layout( currentWidget, query=True, childArray=True) :
-                cmds.deleteUI(item)
-        self.ui = ImagerLensEffectUI(currentWidget, self.nodeName)
 
     def setup(self):
         self.beginScrollLayout()
-        self.ui = None
         self.baseLayout = self.beginLayout("Main", collapse=False)
-        self.addCustom('dummy', self.createNewLensEffectUI, self.replaceLensEffectUI)
+        currentWidget = cmds.setParent(query=True)
+        self.ui = ImagerLensEffectUI(parent=currentWidget, nodeName=self.nodeName, template=self)
         self.endLayout()
         maya.mel.eval('AEdependNodeTemplate '+self.nodeName)
         self.addExtraControls()
         self.endScrollLayout()
 
+
 class ImagerLensEffectUI(ImagerBaseUI):
-    def __init__(self, parent=None, nodeName = None):
-        super(ImagerLensEffectUI, self).__init__(parent,nodeName)
-        cmds.attrControlGrp( attribute=nodeName+'.vignetting')
+    def __init__(self, parent=None, nodeName=None, template=None):
+        super(ImagerLensEffectUI, self).__init__(parent, nodeName, template)
+
+    def setup(self):
+        super(ImagerLensEffectUI, self).setup()
+        self.addSeparator()
+        self.addControl('vignetting', label='Vignetting', annotation='Vignetting amount')
