8e3e4b2e02a7d1ad2beb0bb680c002e739b76ec2
Ashley Handscomb Retallack <ashley@solidangle.com>
2020-11-20 12:56:55 +0000

    #4394 made imager controls get added to editorTemplate when template exits, and add dimControl method to templates

--- scripts/mtoa/ui/ae/aiImagersBaseTemplate.py
+++ scripts/mtoa/ui/ae/aiImagersBaseTemplate.py
@@ -9,24 +9,15 @@ from mtoa.utils import prettify
 
 
 class ImagerBaseUI(object):
-    def __init__(self, parent=None, nodeName = None):
+    def __init__(self, parent=None, nodeName=None, template=None):
         self.parentLayout = cmds.rowColumnLayout( numberOfColumns=3, columnWidth=[(1, 100), (2, 100) , (3, 100) ])
         self._controls = []
         self._layoutStack = [parent]
         self._nodeName = nodeName
         self._attr = None
+        self._template = template
 
-        cmds.text(label = "")
-        cmds.text(label = "Enable")
-        checkbox = cmds.checkBox( label='', parent = self.parentLayout )
-        cmds.connectControl( checkbox, '%s.enable' % nodeName )
-        # self._controls.append((checkbox))
-        cmds.text(label = "")
-        cmds.text(label = "Layer Selection")
-        textField = cmds.textField()
-        cmds.connectControl( textField, '%s.layerSelection' % nodeName )
-        # self._controls.append(textField)
-        cmds.setParent('..')
+        self.setup()
 
     # queries
     @property
@@ -39,6 +30,23 @@ class ImagerBaseUI(object):
     def attr(self):
         return self._attr
 
+    def setup(self):
+
+        # cmds.text(label = "")
+        # cmds.text(label = "Enable")
+        # checkbox = cmds.checkBox( label='', parent = self.parentLayout )
+        # cmds.connectControl( checkbox, '%s.enable' % nodeName )
+        # # self._controls.append((checkbox))
+        # cmds.text(label = "")
+        # cmds.text(label = "Layer Selection")
+        # textField = cmds.textField()
+        # cmds.connectControl( textField, '%s.layerSelection' % nodeName )
+        # # self._controls.append(textField)
+        # cmds.setParent('..')
+        self.addControl("enable", label="Enable")
+        if cmds.attributeQuery('layerSelection', n=self._nodeName, exists=True):
+            self.addControl("layerSelection", label="Layer Selection")
+
     def nodeAttr(self, attr):
         return self.nodeName + '.' + attr
 
@@ -48,48 +56,89 @@ class ImagerBaseUI(object):
     def addControl(self, attr, label=None, changeCommand=None, annotation=None,
                    preventOverride=False, dynamic=False, enumeratedItem=None):
 
+        control = None
+
         if not label:
             label = prettify(attr)
             if label.startswith('Ai '):
                 label = label[3:]
         kwargs = {}
         kwargs['label'] = label
-        kwargs['attribute'] = self.nodeAttr(attr)
         if annotation:
             kwargs['annotation'] = annotation
         if changeCommand:
             kwargs['changeCommand'] = changeCommand
         if enumeratedItem:
             kwargs['enumeratedItem'] = enumeratedItem
-        parent = self._layoutStack[-1]
-        cmds.setParent(parent)
-        control = AttrControlGrp(**kwargs)
-        self._controls.append((attr, control.setAttribute, parent))
+        if preventOverride:
+            kwargs['preventOverride'] = preventOverride
+        if dynamic:
+            kwargs['dynamic'] = dynamic
+
+        if self._template:
+            self._template.addControl(attr, **kwargs)
+        else:
+            kwargs['attribute'] = self.nodeAttr(attr)
+            parent = self._layoutStack[-1]
+            cmds.setParent(parent)
+            control = AttrControlGrp(**kwargs)
+            self._controls.append((attr, control, control.setAttribute, parent))
+
+        return control
 
     def addCustom(self, attr, createFunc, updateFunc):
-        parent = self._layoutStack[-1]
-        cmds.setParent(parent)
-        col = cmds.columnLayout(adj=True)
-        createFunc(self.nodeAttr(attr))
-        cmds.setParent(parent)
-        self._controls.append((attr, updateFunc, col))
+        if self._template:
+            self._template.addCustom(attr, createFunc, updateFunc)
+        else:
+            parent = self._layoutStack[-1]
+            cmds.setParent(parent)
+            col = cmds.columnLayout(adj=True)
+            control = createFunc(self.nodeAttr(attr))
+            cmds.setParent(parent)
+            self._controls.append((attr, control, updateFunc, col))
 
     def addSeparator(self):
-        cmds.separator()
+
+        if self._template:
+            self._template.addSeparator()
+        else:
+            cmds.separator()
 
     def beginLayout(self, label, **kwargs):
         '''
         begin a frameLayout.
         accepts any keyword args valid for creating a frameLayout
         '''
-        kwargs['label'] = label
-        cmds.setParent(self._layoutStack[-1])
-        cmds.frameLayout(**kwargs)
-        self._layoutStack.append(cmds.columnLayout(adjustableColumn=True))
+        if self._template:
+            self._template.beginLayout(label, **kwargs)
+        else:
+            kwargs['label'] = label
+            cmds.setParent(self._layoutStack[-1])
+            cmds.frameLayout(**kwargs)
+            self._layoutStack.append(cmds.columnLayout(adjustableColumn=True))
 
     def endLayout(self):
         '''
         end the current frameLayout
         '''
-        self._layoutStack.pop()
-        cmds.setParent(self._layoutStack[-1])
+        if self._template:
+            self._template.endLayout()
+        else:
+            self._layoutStack.pop()
+            cmds.setParent(self._layoutStack[-1])
+
+    def dimControl(self, attr, state=True):
+
+        if self._template:
+            self._template.dimControl(attr, state)
+        else:
+            control = self.getControl(attr)
+            if control:
+                control.edit(enable=state)
+
+    def getControl(self, attr):
+        for ctrl in self._controls:
+            if ctrl[0] == attr:
+                return ctrl[1]
+
+        return None
