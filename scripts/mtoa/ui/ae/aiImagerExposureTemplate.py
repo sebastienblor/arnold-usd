98ed06bf69d344928af4198a5dac29aac763c191
Ashley Handscomb Retallack <ashley@solidangle.com>
2020-11-20 13:16:44 +0000

    #4394 updated templates for exposure and whitebalance imagers

--- scripts/mtoa/ui/ae/aiImagerExposureTemplate.py
+++ scripts/mtoa/ui/ae/aiImagerExposureTemplate.py
@@ -7,34 +7,25 @@ import maya.cmds as cmds
 from mtoa.ui.qt.Qt import QtWidgets, QtCore, QtGui
 from mtoa.ui.ae.aiImagersBaseTemplate import ImagerBaseUI
 
+
 class AEaiImagerExposureTemplate(ShaderAETemplate):
 
-    def createNewExposureUI(self,nodeAttr):
-        currentWidget = cmds.setParent(query=True)
-        self.ui = ImagerExposureUI(parent = currentWidget,nodeName = self.nodeName)
-   
-
-    def replaceExposureUI(self,nodeAttr):
-        currentWidget = cmds.setParent(query=True)
-        if self.ui:
-            for item in cmds.layout( currentWidget, query=True, childArray=True) :
-                cmds.deleteUI(item)
-        self.ui = ImagerExposureUI(currentWidget, self.nodeName)
-
-    
     def setup(self):
         self.beginScrollLayout()
-        self.ui = None
         self.baseLayout = self.beginLayout("Main", collapse=False)
-        self.addCustom('dummy', self.createNewExposureUI, self.replaceExposureUI)
+        currentWidget = cmds.setParent(query=True)
+        self.ui = ImagerExposureUI(parent=currentWidget, nodeName=self.nodeName, template=self)
         self.endLayout()
         maya.mel.eval('AEdependNodeTemplate '+self.nodeName)
         self.addExtraControls()
         self.endScrollLayout()
 
+
 class ImagerExposureUI(ImagerBaseUI):
-    def __init__(self, parent=None, nodeName = None):
-        super(ImagerExposureUI, self).__init__(parent,nodeName)
-        cmds.attrControlGrp( attribute=nodeName+'.exposure')
+    def __init__(self, parent=None, nodeName=None, template=None):
+        super(ImagerExposureUI, self).__init__(parent, nodeName, template)
 
-
+    def setup(self):
+        super(ImagerExposureUI, self).setup()
+        self.addSeparator()
+        self.addControl('exposure')
--- scripts/mtoa/ui/ae/aiImagerWhiteBalanceTemplate.py
+++ scripts/mtoa/ui/ae/aiImagerWhiteBalanceTemplate.py
@@ -7,41 +7,34 @@ from mtoa.ui.ae.aiImagersBaseTemplate import ImagerBaseUI
 
 class AEaiImagerWhiteBalanceTemplate(ShaderAETemplate):
 
-    def createNewWhiteBalanceUI(self,nodeAttr):
-        currentWidget = cmds.setParent(query=True)
-        self.ui = ImagerWhiteBalanceUI(parent = currentWidget,nodeName = self.nodeName)
-   
-
-    def replaceWhiteBalanceUI(self,nodeAttr):
-        currentWidget = cmds.setParent(query=True)
-        if self.ui:
-            for item in cmds.layout( currentWidget, query=True, childArray=True) :
-                cmds.deleteUI(item)
-        self.ui = ImagerWhiteBalanceUI(currentWidget, self.nodeName)
-
     def setup(self):
         self.beginScrollLayout()
-        self.ui = None
         self.baseLayout = self.beginLayout("Main", collapse=False)
-        self.addCustom('dummy', self.createNewWhiteBalanceUI, self.replaceWhiteBalanceUI)
+        currentWidget = cmds.setParent(query=True)
+        self.ui = ImagerWhiteBalanceUI(parent=currentWidget, nodeName=self.nodeName, template=self)
         self.endLayout()
         maya.mel.eval('AEdependNodeTemplate '+self.nodeName)
         self.addExtraControls()
         self.endScrollLayout()
 
+
 class ImagerWhiteBalanceUI(ImagerBaseUI):
-    
+
+    def __init__(self, parent=None, nodeName=None, template=None):
+        super(ImagerWhiteBalanceUI, self).__init__(parent, nodeName, template)
+
+    def setup(self):
+        super(ImagerWhiteBalanceUI, self).setup()
+        self.addControl('mode', label='Mode', changeCommand=lambda *args: self.updateParamsVisibility(self.nodeName), annotation='White balance mode. (illuminant, temperature, custom)')
+        self.addControl('illuminant', annotation='Temperature for black body mode.')
+        self.addControl('temperature', annotation='Standard illuminant name for illuminante mode.')
+        self.addControl('custom', annotation='Custom white balance color.')
+
+        self.updateParamsVisibility(self.nodeName)
+
     def updateParamsVisibility(self, nodeName):
         modeAttr = '%s.%s' % (nodeName, 'mode')
         modeValue = cmds.getAttr(modeAttr)
-        cmds.control(self.illuminant, enable = (modeValue == 0 ), edit = True)
-        cmds.control(self.temperature, enable = (modeValue == 1), edit = True)
-        cmds.control(self.custom, enable = (modeValue == 2), edit = True)
-
-    def __init__(self, parent=None, nodeName = None):
-        super(ImagerWhiteBalanceUI, self).__init__(parent,nodeName)
-        cmds.attrControlGrp(attribute=nodeName+'.mode', changeCommand=lambda *args:self.updateParamsVisibility(nodeName, *args), annotation='EWhite balance mode. (illuminant, temperature, custom)')
-        self.illuminant = cmds.attrControlGrp(attribute = nodeName+'.illuminant', annotation='Temperature for black body mode.')
-        self.temperature = cmds.attrControlGrp(attribute = nodeName+'.temperature', annotation='Standard illuminant name for illuminante mode.')
-        self.custom = cmds.attrControlGrp(attribute = nodeName+'.custom', annotation='Custom white balance color.')
-        self.updateParamsVisibility(nodeName)
+        self.dimControl('illuminant', state=modeValue == 0)
+        self.dimControl('temperature', state=modeValue == 1)
+        self.dimControl('custom', state=modeValue == 2)
