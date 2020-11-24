78be88d4771e15d8da148006ac27d6cc18463cfe
Krishnan Ramachandran <krishnan.chunangad.ramachandran@autodesk.com>
2020-11-24 12:02:17 +0000

    Denoiser and Light Mixer Template

--- scripts/mtoa/ui/ae/aiImagerDenoiserTemplate.py
+++ scripts/mtoa/ui/ae/aiImagerDenoiserTemplate.py
@@ -1,42 +1,38 @@
-## Fully commented out because enable/disable is not available in 
-## noice denoiser yet. 
+import maya.mel
+from mtoa.ui.ae.templates import TranslatorControl
+from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
+from mtoa.ui.ae.aiImagersBaseTemplate import ImagerBaseUI
+import maya.cmds as cmds
 
+class AEaiImagerDenoiserTemplate(ShaderAETemplate):
 
-# import maya.mel
-# from mtoa.ui.ae.templates import TranslatorControl
-# from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
-# from mtoa.ui.ae.aiImagersBaseTemplate import ImagerBaseUI
-# import maya.cmds as cmds
-
-# class AEaiImagerDenoiserTemplate(ShaderAETemplate):
-
-#     def createNewDenoiserUI(self,nodeAttr):
-#         currentWidget = cmds.setParent(query=True)
-#         self.ui = ImagerDenoiserUI(parent = currentWidget,nodeName = self.nodeName)
+    def createNewDenoiserUI(self,nodeAttr):
+        currentWidget = cmds.setParent(query=True)
+        self.ui = ImagerDenoiserUI(parent = currentWidget,nodeName = self.nodeName)
    
 
-#     def replaceDenoiserUI(self,nodeAttr):
-#         currentWidget = cmds.setParent(query=True)
-#         if self.ui:
-#             for item in cmds.layout( currentWidget, query=True, childArray=True) :
-#                 cmds.deleteUI(item)
-#         self.ui = ImagerDenoiserUI(currentWidget, self.nodeName)
+    def replaceDenoiserUI(self,nodeAttr):
+        currentWidget = cmds.setParent(query=True)
+        if self.ui:
+            for item in cmds.layout( currentWidget, query=True, childArray=True) :
+                cmds.deleteUI(item)
+        self.ui = ImagerDenoiserUI(currentWidget, self.nodeName)
 
-#     def setup(self):
-#         self.beginScrollLayout()
-#         self.ui = None
-#         self.baseLayout = self.beginLayout("Main", collapse=False)
-#         self.addCustom('dummy', self.createNewDenoiserUI, self.replaceDenoiserUI)
-#         self.endLayout()
-#         maya.mel.eval('AEdependNodeTemplate '+self.nodeName)
-#         self.addExtraControls()
-#         self.endScrollLayout()
+    def setup(self):
+        self.beginScrollLayout()
+        self.ui = None
+        self.baseLayout = self.beginLayout("Main", collapse=False)
+        self.addCustom('dummy', self.createNewDenoiserUI, self.replaceDenoiserUI)
+        self.endLayout()
+        maya.mel.eval('AEdependNodeTemplate '+self.nodeName)
+        self.addExtraControls()
+        self.endScrollLayout()
 
-# class ImagerDenoiserUI(ImagerBaseUI):
-#     def __init__(self, parent=None, nodeName = None):
-#         super(ImagerDenoiserUI, self).__init__(parent,nodeName)
-#         self.addControl('patchRadius')
-#         self.addControl('searchRadius')
-#         self.addControl('tileSize')
-#         self.addControl('variance')
+class ImagerDenoiserUI(ImagerBaseUI):
+    def __init__(self, parent=None, nodeName = None):
+        super(ImagerDenoiserUI, self).__init__(parent,nodeName)
+        self.addControl('patchRadius')
+        self.addControl('searchRadius')
+        self.addControl('tileSize')
+        self.addControl('variance')
         
---
+++ scripts/mtoa/ui/ae/aiImagerLightMixerTemplate.py
@@ -0,0 +1,30 @@
+import maya.mel
+from mtoa.ui.ae.templates import TranslatorControl
+from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
+from mtoa.ui.qt import toQtObject
+from mtoa.ui.qt import toMayaName
+import maya.cmds as cmds
+from mtoa.ui.qt.Qt import QtWidgets, QtCore, QtGui
+from mtoa.ui.ae.aiImagersBaseTemplate import ImagerBaseUI
+
+
+class AEaiImagerLightMixerTemplate(ShaderAETemplate):
+
+    def setup(self):
+        self.beginScrollLayout()
+        self.baseLayout = self.beginLayout("Main", collapse=False)
+        currentWidget = cmds.setParent(query=True)
+        self.ui = ImagerLightMixerUI(parent=currentWidget, nodeName=self.nodeName, template=self)
+        self.endLayout()
+        maya.mel.eval('AEdependNodeTemplate '+self.nodeName)
+        self.addExtraControls()
+        self.endScrollLayout()
+
+
+class ImagerLightMixerUI(ImagerBaseUI):
+    def __init__(self, parent=None, nodeName=None, template=None):
+        super(ImagerLightMixerUI, self).__init__(parent, nodeName, template)
+
+    def setup(self):
+        super(ImagerLightMixerUI, self).setup()
+        self.addSeparator()
