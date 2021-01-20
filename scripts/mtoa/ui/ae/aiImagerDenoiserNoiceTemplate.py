cbe70c3ee479f5c7dae51697a3857d0d989429c9
Sebastien Blaineau-Ortega <sebastien.blaineau.ortega@autodesk.com>
2021-01-20 16:28:19 +0100

    box filters are only needed for optix denoiser, not noice
    (cherry picked from commit 0e25de7b3fa525e1caee36a1c9b9b24e4577cf78)

--- scripts/mtoa/ui/ae/aiImagerDenoiserNoiceTemplate.py
+++ scripts/mtoa/ui/ae/aiImagerDenoiserNoiceTemplate.py
@@ -23,26 +23,6 @@ class ImagerDenoiserUI(ImagerBaseUI):
     def __init__(self, parent=None, nodeName = None, template=None):
         super(ImagerDenoiserUI, self).__init__(parent, nodeName,template)
 
-    def doChangeFilter(self):
-        if not cmds.objExists('defaultArnoldFilter'):
-            return
-        cmds.setAttr('defaultArnoldFilter.aiTranslator', 'box', type="string")
-        self.filterMsgReplace('message')
-
-    def filterMsgNew(self, nodeAttr):
-        cmds.setUITemplate('attributeEditorTemplate', pst=True)
-        cmds.rowColumnLayout( numberOfColumns=2, columnAlign=[(1, 'left'),(2, 'right')], columnAttach=[(1, 'left', 0), (2, 'both', 0)], columnWidth=[(1,145),(2,80)] )
-        self.filterTextCtrl = cmds.text(label='   A box filter is required for   \n    better denoising results  ')
-        self.filterButtonCtrl = cmds.button(label='Set Box Filter', al='right', w=85, h=25, command=lambda *args: self.doChangeFilter())
-        cmds.setUITemplate(ppt=True)
-        self.filterMsgReplace(nodeAttr)
-        
-    def filterMsgReplace(self, nodeAttr):
-        filter = cmds.getAttr('defaultArnoldFilter.aiTranslator', asString=True)
-        enabled = False if filter == 'box' else True
-        cmds.text(self.filterTextCtrl, edit=True, enable = enabled)
-        cmds.button(self.filterButtonCtrl, edit=True, enable = enabled)
-
     def setup(self):
         super(ImagerDenoiserUI, self).setup()
         self.beginLayout("Main", collapse=False)
@@ -51,9 +31,6 @@ class ImagerDenoiserUI(ImagerBaseUI):
         self.addControl('searchRadius', hideMapButton = True, annotation = "")
         self.addControl('tileSize', hideMapButton = True, annotation = "")
         self.addControl('variance', hideMapButton = True, annotation = "")
-        self.addSeparator()
-        self.addCustom("message", self.filterMsgNew, self.filterMsgReplace)
-
         self.endLayout()
 
 
