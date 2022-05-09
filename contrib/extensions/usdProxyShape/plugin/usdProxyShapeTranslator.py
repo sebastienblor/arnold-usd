74408c9507844b46e11352fe644617f12fbc89dc
Sebastien Blaineau-Ortega <sebastien.blaineau.ortega@autodesk.com>
2022-05-09 12:49:13 +0200

    Add python version of the shaderReader translators for USD import #1013
    (cherry picked from commit fb6183370f6a0a004737a0766486fb950871e45f)

--- contrib/extensions/usdProxyShape/plugin/usdProxyShapeTranslator.py
+++ contrib/extensions/usdProxyShape/plugin/usdProxyShapeTranslator.py
@@ -1,12 +1,13 @@
 
 ## we want to add this directory to the path so we can use the extra xgenArnoldUI  files
-from pxr import Usd, UsdUtils, Tf, Vt
+from pxr import Usd, UsdShade, UsdUtils, Tf, Vt
 import maya.cmds as cmds
 import mayaUsd.lib as mayaUsdLib
 import maya.api.OpenMaya as OpenMaya
 import mtoa.core
 import arnold as ai
 import mtoa.utils as mtoaUtils
+import ctypes
 
 global _stageCacheDict
 _stageCacheDict = {}
@@ -16,6 +17,13 @@ _stageNames = {}
 
 global _sessionName
 _sessionName = ''
+
+global _usdToMayaNodes
+_usdToMayaNodes = {}
+
+global _usdToMayaAttrs
+_usdToMayaAttrs = {}
+
 
 def UsdArnoldUnregisterListener(cacheId):
     global _stageCacheDict
@@ -296,6 +304,187 @@ def ArnoldExportJobContext():
     extraArgs['chaser']  = ['ArnoldUsdChaser']
     return extraArgs
 
+
+# Convert the arnold node/attribute names to their Maya counterpart,
+# where e.g. standard_surface becomes aiStandardSurface
+def MakeCamelCase(inputName, prefix= True):
+   
+    name = []
+    capitalize = False
+   
+    if prefix:
+      name = ['a', 'i']
+      capitalize = True;
+   
+    nameList = list(inputName)
+
+    for i in range(len(inputName)):
+        c = inputName[i]
+        if c == '_':
+            capitalize = True
+        else:
+            if capitalize:
+                name.append(c.upper())
+            else:
+                name.append(c)
+                        
+            capitalize = False
+    return ''.join(name)
+
+# Find in which classification a shader must be created.
+# This function will return a list of 3 booleans that will
+# control the shadingNode command arguments [asShader, asTexture, asUtility]
+def GetShaderClassification(mayaShader):
+    classifications = cmds.getClassification(mayaShader)
+    shaderClassification = ['rendernode/arnold/shader', 'shader']
+    textureClassification = ['rendernode/arnold/texture', 'texture']
+    utilityClassification = ['rendernode/arnold/utility', 'utility']
+
+    for classification in classifications:
+        clElems = classification.split(':')
+        for clElem in clElems:
+            for shadCl in shaderClassification:
+                if clElem.startswith(shadCl):
+                    return [True, False, False]
+
+            for texCl in textureClassification:
+                if clElem.startswith(texCl):
+                    return [False, True, False]
+
+            for utilCl in utilityClassification:
+                if clElem.startswith(utilCl):
+                    return [False, False, True]
+
+    return [True, False, False]
+
+
+# The ArnoldShaderReader will be invoked when an Arnold shader is imported in Maya.
+# In that case, we want to create the maya version of each of these arnold shaders
+class ArnoldShaderReader(mayaUsdLib.ShaderReader):
+
+    def Read(self, context):
+        global _usdToMayaNodes
+        global _usdToMayaAttrs
+
+        try:
+            # Get the input USD primitive and UsdShade schema
+            prim = self._GetArgs().GetUsdPrim()
+            shaderSchema = UsdShade.Shader(prim)
+            if (not shaderSchema):
+                return None
+
+            # shaderId will return the shader type (from the attribute info:id)
+            shaderId = shaderSchema.GetShaderId() or ''
+            if len(shaderId) == 0:
+                return None
+
+            # Now let's lookup our map and see if there's a maya shader
+            # corresponding to this arnold shader type
+            mayaShader = _usdToMayaNodes[shaderId] or ''
+            if len(mayaShader) == 0:
+                return None
+
+            # Compute a maya shader name, by extracting the first '/'
+            # and replace the following ones by underscores
+            nodeName = prim.GetPath().pathString
+            if nodeName[0] == '/':
+                nodeName = nodeName[1:]
+
+            nodeName = nodeName.replace('/', '_')
+            # Get the classification for this shader, and call the command shadingNode that will create
+            # the shader in Maya
+            shaderTags = GetShaderClassification(mayaShader)
+            shaderName = cmds.shadingNode(mayaShader, asShader=shaderTags[0], asTexture=shaderTags[1], asUtility=shaderTags[2], name=nodeName)
+            selectionList = OpenMaya.MSelectionList()
+            selectionList.add(shaderName)
+            if selectionList.length() == 0:
+                return None
+
+            shaderObj = selectionList.getDependNode(0)
+            shaderFn = OpenMaya.MFnDependencyNode(shaderObj)
+            context.RegisterNewMayaNode(prim.GetPath().pathString, shaderObj)            
+
+            # Now lookup the attributes defined in this shader, and set them 
+            # in the Maya shader
+            paramMap = _usdToMayaAttrs[shaderId]
+            for input in shaderSchema.GetInputs():
+                usdAttr = input.GetAttr()
+                usdAttrName = usdAttr.GetBaseName()
+                if usdAttrName in paramMap:
+                    mayaUsdLib.ReadUtil.SetMayaAttr('{}.{}'.format(shaderName, usdAttrName), usdAttr.Get(Usd.TimeCode.Default()))
+
+        except RuntimeError as err:
+            print(err)
+
+# Register each of the Arnold shaders, so that they can be imported to Maya
+def RegisterShaderReaders():
+
+    global _usdToMayaNodes
+    global _usdToMayaAttrs
+    activeUniverse = ai.AiUniverseIsActive()
+    if not activeUniverse:
+        ai.AiBegin()
+
+    pluginLocation = cmds.pluginInfo('mtoa', q=True, p=True)
+    ind = pluginLocation.rfind(".")
+    if ind > 0:
+        metadataFile = '{}.mtd'.format(pluginLocation[:ind])
+        # Load MtoA's metadatas
+        ai.AiMetaDataLoadFile(metadataFile)
+
+    # Now loop over all shader types known by arnold
+    nodeIter = ai.AiUniverseGetNodeEntryIterator(ai.AI_NODE_SHADER)
+    while not ai.AiNodeEntryIteratorFinished(nodeIter):
+        nentry = ai.AiNodeEntryIteratorGetNext(nodeIter)
+        entryName = ai.AiNodeEntryGetName(nentry)
+        
+        # The fallback for node names is to prefix the arnold node name by ai
+        # and to make it camel-case
+        mayaType = MakeCamelCase(entryName)
+        
+        # ignore this arnold shader if it has the maya.hide metadata
+        hide = ai.POINTER(ai.c_bool)(ai.c_bool(False))
+        if ai.AiMetaDataGetBool(nentry, None, 'maya.hide', hide) and hide.contents.value:
+            continue
+    
+        mayaNameStr = ai.AtStringStruct()
+        # check if an explicite maya node name is defined
+        if ai.AiMetaDataGetStr(nentry, None, 'maya.name', ai.byref(mayaNameStr)):
+            mayaType = str(mayaNameStr)
+
+        # prefix the shader type (info:id) by arnold namespace
+        entryName = 'arnold:{}'.format(entryName)
+        # Store the "node types" maps from usd to maya
+        _usdToMayaNodes[entryName] = mayaType
+        mayaUsdLib.ShaderReader.Register(ArnoldShaderReader, entryName)
+
+        _usdToMayaAttrs[entryName] = {}
+
+        # Iterate over this node entry's parameters
+        paramIt = ai.AiNodeEntryGetParamIterator(nentry)
+        while not ai.AiParamIteratorFinished(paramIt):
+            paramEntry = ai.AiParamIteratorGetNext(paramIt)
+            paramName = ai.AiParamGetName(paramEntry)
+            if paramName == 'name':
+                continue
+            # as a fallback attribute name, make it camel-case. We don't prefix
+            # with "ai" as these are arnold nodes that don't have this prefix in maya
+            paramNameStr = MakeCamelCase(paramName, False)
+            mayaAttrNameStr = ai.AtStringStruct()
+            # check if an explicit maya name is set for this attribute
+            if ai.AiMetaDataGetStr(nentry, paramName, 'maya.name', ai.byref(mayaNameStr)):
+                paramNameStr = str(mayaAttrNameStr)
+
+            _usdToMayaAttrs[entryName][paramName] = paramNameStr
+    
+        ai.AiParamIteratorDestroy(paramIt)
+    ai.AiNodeEntryIteratorDestroy(nodeIter)
+
+    # If the arnold universe was created above, it's time to delete it
+    if not activeUniverse:
+        ai.AiEnd()
+
+
 # This code should only be called once when the mayaUsd plugin is loaded.
 # We register all the custom translators, the chaser, and the job context
 if _registerTranslators == False:
@@ -328,3 +517,5 @@ if _registerTranslators == False:
         # Register the Job context, it will add an option "Arnold" to the "Plugin Configuration" 
         # drop down, in the MayaUsd export UI
         mayaUsdLib.JobContextRegistry.RegisterExportJobContext("Arnold", "Arnold", "Arnold", ArnoldExportJobContext)
+        # Register all the Arnold shaders for USD import
+        RegisterShaderReaders()
