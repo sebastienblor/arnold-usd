1e885bf709545ac83c25fa0f2e62d1e4de3f1168
ADS\blaines <sebastien.blaineau.ortega@autodesk.com>
2023-01-04 15:05:46 +0100

    Temporarily run maya -batch instead of mayabatch on windows MTOA-1249

--- tools/python/utils/regression_test.py
+++ tools/python/utils/regression_test.py
@@ -91,10 +91,12 @@ class Test:
       # Note that we won't be loading the scene because it seems that the script is called before scene load.
       # So it's the script itself that loads whatever scene it wants
       if os.path.exists(os.path.join(test_dir, 'test.mel')):
-         if system.is_windows:
-            mayabatch_cmd = 'mayabatch'
-         else:
-            mayabatch_cmd = 'maya -batch'
+         # For windows we used to run mayabatch, but this is causing 
+         # hangs when arnold is the current renderer (MTOA-1249).
+         # In the meantime we're switching to maya -batch in the testsuite
+         # if system.is_windows:
+         #   mayabatch_cmd = 'mayabatch'
+         mayabatch_cmd = 'maya -batch'
          if not ' ' in env['MAYA_ROOT']:
             mayabatch_cmd = os.path.join(env['MAYA_ROOT'], 'bin', mayabatch_cmd)
 
