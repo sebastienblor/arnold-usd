f2496e859736124581ec6760b7203d7bda780501
Ashley Handscomcb Retallack <ashley.handscomb.retallack@autodesk.com>
2023-01-30 13:04:58 +0000

    MTOA-1295 : reintroduce the arnoldScene MEL to txmanager

--- scripts/mtoa/txManager/lib.py
+++ scripts/mtoa/txManager/lib.py
@@ -128,6 +128,11 @@ class TxProcessor(QtCore.QObject):
         if not selected_textures:
             return self.test_progress()
              
+        # first we need to make sure the options & color manager node were converted to arnold
+        cmds.arnoldScene(mode='create')
+
+        ai.AiMsgSetConsoleFlags(ai.AI_LOG_INFO)
+
         render_colorspace = cmds.colorManagementPrefs(query=True, renderingSpaceName=True)
 
         cmEnable = cmds.colorManagementPrefs(query=True, cmEnabled=True)
@@ -238,6 +243,9 @@ class TxProcessor(QtCore.QObject):
 
         utils.executeDeferred(self.txManager.on_refresh)
 
+        # an arnold scene was created above, let's delete it now
+        cmds.arnoldScene(mode="destroy")
+    
         return True
 
 
