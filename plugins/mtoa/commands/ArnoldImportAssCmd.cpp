37b2e7eea3b6b3043b92d8aa307759692ee9569e
Jan Walter <jan.walter@autodesk.com>
2023-03-08 17:23:43 +0100

    MTOA-1300 : Import Arnold Shaders does not import uv_projection.matrix (#574)
    * MTOA-1300 : Check both sides of a connection (RGB->RGBA and RGBA->RGB).
    
    * MTOA-1300 : Copy matrix values from Arnold to Maya.

--- plugins/mtoa/commands/ArnoldImportAssCmd.cpp
+++ plugins/mtoa/commands/ArnoldImportAssCmd.cpp
@@ -460,6 +460,42 @@ void setAttributeValues(const AtNode* node,
           break;
       }
     case AI_TYPE_MATRIX:
+      {
+	  AtMatrix mat = AiNodeGetMatrix(node, paramName);
+	  attrValue += "-type \"matrix\" ";
+	  attrValue += (float) mat[0][0];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[0][1];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[0][2];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[0][3];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[1][0];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[1][1];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[1][2];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[1][3];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[2][0];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[2][1];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[2][2];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[2][3];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[3][0];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[3][1];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[3][2];
+	  attrValue += MString(" ");
+	  attrValue += (float) mat[3][3];
+      }
+      break;
     case AI_TYPE_CLOSURE:
     case AI_TYPE_USHORT:
     case AI_TYPE_HALF:
